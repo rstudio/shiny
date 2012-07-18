7dc6b4035aefab7071bce9ec1990d2ccbe4412bf
JJ Allaire <jj@rstudio.org>
2012-07-18 03:25:40 -0700

    add initial scaffolding for ui module

---
+++ R/ui.R
@@ -0,0 +1,23 @@
+
+
+# attribs, text, children
+
+# explicit named args (my attributes)
+# special named args (class, style, attribs)
+# un-named character string (content)
+# children (which you must explicitly render)
+# you return a function 
+
+processArgs <- function(args) {
+  
+}
+
+p <- function(...) {
+  
+}
+
+
+
+defineUI <- function(header, inputs, outputs, stylesheet) {
+  
+}

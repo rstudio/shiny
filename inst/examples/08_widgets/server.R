acd68b5de894c1c1d6238f68ab92ed450a88e2e5
JJ Allaire <jj@rstudio.org>
2012-07-25 10:10:03 -0700

    more widgets example

--- inst/examples/08_widgets/server.R
+++ inst/examples/08_widgets/server.R
@@ -1,7 +1,22 @@
 library(shiny)
+library(datasets)
 
 shinyServer(function(input, output) {
   
-
+  datasetInput <- function() {
+    switch(input$dataset,
+           "rock" = rock,
+           "pressure" = pressure,
+           "cars" = cars)
+  }
   
+  output$summary <- reactiveText(function() {
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  output$view <- reactiveTable(function() {
+    obs <- as.integer(input$obs)
+    head(datasetInput(), n = obs)
+  })
 })
--- inst/examples/08_widgets/ui.R
+++ inst/examples/08_widgets/ui.R
@@ -3,14 +3,31 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   headerPanel(
-    h1("More Widgets")
+    h1("More Widgets"),
+    HTML("This example demonstrates the use of the <em>HTML</em>",
+         "widget to embed HTML directly, as well as the use of the ",
+         "<em>helpText</em> and <em>submitButton</em> widgets.",
+         "<hr/>")
   ),
   
   sidebarPanel(
+    selectInput("dataset", "Choose a dataset:", 
+                choices = c("rock", "pressure", "cars")),
     
+    numericInput("obs", "Number of observations to view:", 10),
+    
+    helpText("Note: while the data view will show only the specified",
+             "number of observations, the summary will still be based",
+             "on the full dataset."),
+    
+    submitButton("Update View")
   ),
   
   mainPanel(
+    h4("Summary"),
+    verbatimTextOutput("summary"),
     
+    h4("Observations"),
+    tableOutput("view")
   )
 ))

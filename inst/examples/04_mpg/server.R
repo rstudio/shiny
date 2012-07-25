f958839af1490d059451878790fea820eacb95bc
JJ Allaire <jj@rstudio.org>
2012-07-25 11:16:04 -0700

    load mpgData at startup

--- inst/examples/04_mpg/server.R
+++ inst/examples/04_mpg/server.R
@@ -1,26 +1,25 @@
 library(shiny)
 library(datasets)
 
+# We tweak the "am" field to have nicer factor labels. Since this doesn't
+# rely on any user inputs we can do this once at startup and then use the
+# value throughout the lifetime of the application
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
 shinyServer(function(input, output) {
   
   formulaText <- reactive(function() {
     paste("mpg ~", input$variable)
   })
-  
-  mpgData <- reactive(function() {
-    data <- mtcars
-    data$cyl <- as.factor(data$cyl)
-    data$am <- factor(data$am, labels = c("Automatic", "Manual"))
-    data
-  })
-  
+   
   output$caption <- reactive(function() {
     formulaText()
   })
   
   output$plot <- reactivePlot(function() {
     boxplot(as.formula(formulaText()), 
-            data = mpgData(),
+            data = mpgData,
             outline = input$outliers)
   })
 })

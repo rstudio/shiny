5cfd546b2ac95a3b9ee89eb92090771766344835
JJ Allaire <jj@rstudio.org>
2012-07-26 09:18:14 -0700

    change indentation for tabset app

--- inst/examples/06_tabsets/ui.R
+++ inst/examples/06_tabsets/ui.R
@@ -1,38 +1,36 @@
 library(shiny)
 
 # Define UI for random distribution application 
-shinyUI(
-  pageWithSidebar(
+shinyUI(pageWithSidebar(
     
-    # Application title
-    headerPanel("Tabsets"),
+  # Application title
+  headerPanel("Tabsets"),
+  
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the br()
+  # element to introduce extra vertical spacing
+  sidebarPanel(
+    radioButtons("dist", "Distribution type:",
+                 list("Normal" = "norm",
+                      "Uniform" = "unif",
+                      "Log-normal" = "lnorm",
+                      "Exponential" = "exp")),
+    br(),
     
-    # Sidebar with controls to select the random distribution type
-    # and number of observations to generate. Note the use of the br()
-    # element to introduce extra vertical spacing
-    sidebarPanel(
-      radioButtons("dist", "Distribution type:",
-                   list("Normal" = "norm",
-                        "Uniform" = "unif",
-                        "Log-normal" = "lnorm",
-                        "Exponential" = "exp")),
-      br(),
-      
-      sliderInput("n", 
-                  "Number of observations:", 
-                   value = 500,
-                   min = 1, 
-                   max = 1000)
-    ),
-    
-    # Show a tabset that includes a plot, summary, and table view
-    # of the generated distribution
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Plot", plotOutput("plot")), 
-        tabPanel("Summary", verbatimTextOutput("summary")), 
-        tabPanel("Table", tableOutput("table"))
-      )
+    sliderInput("n", 
+                "Number of observations:", 
+                 value = 500,
+                 min = 1, 
+                 max = 1000)
+  ),
+  
+  # Show a tabset that includes a plot, summary, and table view
+  # of the generated distribution
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot", plotOutput("plot")), 
+      tabPanel("Summary", verbatimTextOutput("summary")), 
+      tabPanel("Table", tableOutput("table"))
     )
   )
-)
+))

6d9da1260a4031989f74bb3532ad0ce30773b4d5
JJ Allaire <jj@rstudio.org>
2012-07-27 09:53:32 -0700

    add comments to sliders example

--- inst/examples/05_sliders/server.R
+++ inst/examples/05_sliders/server.R
@@ -1,35 +1,33 @@
 library(shiny)
 
-
-
-
+# Define server logic for slider examples
 shinyServer(function(input, output) {
   
-
+  # Reactive function to compose a data frame containing all of the values
   sliderValues <- reactive(function() {
     
+    # Show values using R's default print format
     printValue <- function(value) {
       capture.output(print(value))
     }
     
+    # Compose data frame
     data.frame(
       Name = c("Integer", 
                "Decimal",
                "Range",
                "Custom Format",
                "Animation"),
-      
       Value = c(printValue(input$integer), 
                 printValue(input$decimal),
                 printValue(input$range),
                 printValue(input$format),
-                printValue(input$animation)),
-      
+                printValue(input$animation)), 
       stringsAsFactors=FALSE)
   }) 
   
+  # Show the values using an HTML table
   output$values <- reactiveTable(function() {
     sliderValues()
   })
-  
 })
--- inst/examples/05_sliders/ui.R
+++ inst/examples/05_sliders/ui.R
@@ -1,20 +1,38 @@
 library(shiny)
 
+# Define UI for slider demo application
 shinyUI(pageWithSidebar(
   
+  #  Application title
   headerPanel("Sliders"),
   
+  # Sidebar with sliders that demonstrate various available options
   sidebarPanel(
-    sliderInput("integer", "Integer:", 0, 1000, 500),
-    sliderInput("decimal", "Decimal:", 0, 1, 0.5, step= 0.1, round=2),
-    sliderInput("range", "Range:", 1, 1000, c(200,500)),
-    sliderInput("format", "Custom Format:", 0, 100000, 50000, step = 25000,
-                format="$#,##0", locale="us"),   
+    # Simple integer interval
+    sliderInput("integer", "Integer:", 
+                min=0, max=1000, value=500),
+    
+    # Decimal interval with step value
+    sliderInput("decimal", "Decimal:", 
+                min = 0, max = 1, value = 0.5, step= 0.1),
+   
+    # Specification of range within an interval
+    sliderInput("range", "Range:",
+                min = 1, max = 1000, value = c(200,500)),
+    
+    # Provide a custom currency format for value display
+    sliderInput("format", "Custom Format:", 
+                min = 0, max = 100000, value = 50000, step = 25000,
+                format="$#,##0", locale="us"),  
+    
+    # Animation with custom interval (in milliseconds) to control speed
+    # (also provide helpText to highlight the possiblity of animation)
     sliderInput("animation", "Animation:", 1, 2000, 1, step = 10,
                 animationInterval = 300),
     helpText("Use the Play button to animate values.")
   ),
   
+  # Show a table summarizing the values entered
   mainPanel(
     tableOutput("values")
   )

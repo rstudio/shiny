338463057c1d4025f5728a9042ae6570828853dc
JJ Allaire <jj@rstudio.org>
2012-07-27 09:05:23 -0700

    initial code for slider example

--- inst/examples/05_sliders/server.R
+++ inst/examples/05_sliders/server.R
@@ -1,7 +1,35 @@
 library(shiny)
 
+
+
+
 shinyServer(function(input, output) {
   
 
+  sliderValues <- reactive(function() {
+    
+    printValue <- function(value) {
+      capture.output(print(value))
+    }
+    
+    data.frame(
+      Name = c("Integer", 
+               "Decimal",
+               "Range",
+               "Custom Format",
+               "Animation"),
+      
+      Value = c(printValue(input$integer), 
+                printValue(input$decimal),
+                printValue(input$range),
+                printValue(input$format),
+                printValue(input$animation)),
+      
+      stringsAsFactors=FALSE)
+  }) 
+  
+  output$values <- reactiveTable(function() {
+    sliderValues()
+  })
   
 })
--- inst/examples/05_sliders/ui.R
+++ inst/examples/05_sliders/ui.R
@@ -5,10 +5,17 @@ shinyUI(pageWithSidebar(
   headerPanel("Sliders"),
   
   sidebarPanel(
-    
+    sliderInput("integer", "Integer:", 0, 1000, 500),
+    sliderInput("decimal", "Decimal:", 0, 1, 0.5, step= 0.1, round=2),
+    sliderInput("range", "Range:", 1, 1000, c(200,500)),
+    sliderInput("format", "Custom Format:", 0, 100000, 50000, step = 25000,
+                format="$#,##0", locale="us"),   
+    sliderInput("animation", "Animation:", 1, 2000, 1, step = 10,
+                animationInterval = 300),
+    helpText("Use the Play button to animate values.")
   ),
   
   mainPanel(
-    
+    tableOutput("values")
   )
 ))

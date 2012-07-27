da6771eaae655cab21c27e86ca25a27414bd3afc
Joe Cheng <joe@rstudio.org>
2012-07-27 01:37:24 -0700

    Back out accidentally-committed test code

--- inst/examples/01_hello/server.R
+++ inst/examples/01_hello/server.R
@@ -1,21 +1,7 @@
 library(shiny)
 
-tickFactor <- function (x) {
-  calcTickSize <- function(range) {
-    tickSize <- 10^(floor(log10(range*x)))
-    floor(range/tickSize)
-  }
-  
-  range <- 1:10000
-  df <- data.frame(range=range, ticks=calcTickSize(range))
-}
-
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
-  
-  tickCounts <- reactive(function() {
-    tickFactor(as.numeric(input$obs))
-  })
    
   # Function that generates a plot of the distribution. The function
   # is wrapped in a call to reactivePlot to indicate that:
@@ -25,12 +11,13 @@ shinyServer(function(input, output) {
   #  2) Its output type is a plot 
   #
   output$distPlot <- reactivePlot(function() {
-    plot(tickCounts())
+    
+    # convert string input to an integer
+    obs <- as.integer(input$obs)
+    
+    # generate an rnorm distribution and plot it
+    dist <- rnorm(obs)
+    hist(dist)
   })
-  output$min <- reactiveText(function() {
-    min(tickCounts()$ticks)
-  })
-  output$max <- reactiveText(function() {
-    max(tickCounts()$ticks)
-  })
+  
 })

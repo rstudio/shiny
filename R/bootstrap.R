4ff1c95083c131ecd5ce62df02e110d26b02abd5
Joe Cheng <joe@rstudio.org>
2012-07-27 17:46:15 -0700

    Slider and animation docs

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -198,9 +198,45 @@ submitButton <- function(text = "Apply Changes") {
   )
 }
 
+#' Slider Input Widget
+#' 
+#' Constructs a slider widget to select a numeric value from a range.
+#' 
+#' @param inputId Specifies the \code{input} slot that will be used to access 
+#'   the value.
+#' @param label A descriptive label to be displayed with the widget.
+#' @param min The minimum value (inclusive) that can be selected.
+#' @param max The maximum value (inclusive) that can be selected.
+#' @param value The initial value of the slider. A warning will be issued if the
+#'   value doesn't fit between \code{min} and \code{max}.
+#' @param step Specifies the interval between each selectable value on the 
+#'   slider (\code{NULL} means no restriction).
+#' @param round \code{TRUE} to round all values to the nearest integer; 
+#'   \code{FALSE} if no rounding is desired; or an integer to round to that 
+#'   number of digits (for example, 1 will round to the nearest 10, and -2 will 
+#'   round to the nearest .01). Any rounding will be applied after snapping to 
+#'   the nearest step.
+#' @param format Customize format values in slider labels. See 
+#'   \url{http://archive.plugins.jquery.com/project/numberformatter} for syntax 
+#'   details.
+#' @param locale The locale to be used when applying \code{format}. See details.
+#' @param ticks \code{FALSE} to hide tick marks, \code{TRUE} to show them
+#'   according to some simple heuristics.
+#'   
+#' @details
+#' 
+#' Valid values for \code{locale} are: \tabular{ll}{ Arab Emirates \tab "ae" \cr
+#' Australia \tab "au" \cr Austria \tab "at" \cr Brazil \tab "br" \cr Canada
+#' \tab "ca" \cr China \tab "cn" \cr Czech \tab "cz" \cr Denmark \tab "dk" \cr 
+#' Egypt \tab "eg" \cr Finland \tab "fi" \cr France  \tab "fr" \cr Germany \tab
+#' "de" \cr Greece \tab "gr" \cr Great Britain \tab "gb" \cr Hong Kong \tab "hk"
+#' \cr India \tab "in" \cr Israel \tab "il" \cr Japan \tab "jp" \cr Russia \tab
+#' "ru" \cr South Korea \tab "kr" \cr Spain \tab "es" \cr Sweden \tab "se" \cr 
+#' Switzerland \tab "ch" \cr Taiwan \tab "tw" \cr Thailand \tab "th" \cr United
+#' States \tab "us" \cr Vietnam \tab "vn" \cr }
+#' 
 #' @export
 sliderInput <- function(inputId, label, min, max, value, step = NULL,
-                        ...,
                         round=FALSE, format='#,##0.#####', locale='us',
                         ticks=TRUE, animate=FALSE) {
  
--- R/slider.R
+++ R/slider.R
@@ -3,9 +3,22 @@ hasDecimals <- function(value) {
   return (!identical(value, truncatedValue))
 }
 
+#' Animation Options
+#' 
+#' Creates an options object for customizing animations for \link{slider}.
+#' 
+#' @param interval The interval, in milliseconds, between each animation step.
+#' @param loop \code{TRUE} to automatically restart the animation when it 
+#'   reaches the end.
+#' @param playButton Specifies the appearance of the play button. Valid values 
+#'   are a one-element character vector (for a simple text label), an HTML tag 
+#'   or list of tags (using \code{\link{tag}} and friends), or raw HTML (using 
+#'   \code{\link{HTML}}).
+#' @param pauseButton Similar to \code{playButton}, but for the pause button.
+#'   
 #' @export
 animationOptions <- function(interval=1000,
-                             loop=F,
+                             loop=FALSE,
                              playButton=NULL,
                              pauseButton=NULL) {
   list(interval=interval,

e61f7405fd23ba69f1837ae56c27142110a4cf15
Joe Cheng <joe@rstudio.org>
2012-08-31 22:39:45 -0700

    Upload example app should accept text/plain files

--- inst/examples/09_upload/ui.R
+++ inst/examples/09_upload/ui.R
@@ -4,7 +4,7 @@ shinyUI(pageWithSidebar(
   headerPanel("CSV Viewer"),
   sidebarPanel(
     fileInput('file1', 'Choose CSV File',
-              accept=c('text/csv', 'text/comma-separated-values')),
+              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
     tags$hr(),
     checkboxInput('header', 'Header', TRUE),
     radioButtons('sep', 'Separator',

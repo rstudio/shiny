999df6e40fbd93d0919ddca7a0b051f1cccf0ec3
Winston Chang <winston@stdout.org>
2013-02-06 12:29:24 -0600

    httpResponse: make sure headers is a list. Fixes #102

--- R/shiny.R
+++ R/shiny.R
@@ -337,6 +337,8 @@ httpResponse <- function(status = 200,
                          content_type = "text/html; charset=UTF-8", 
                          content = "",
                          headers = list()) {
+  # Make sure it's a list, not a vector
+  headers <- as.list(headers)
   if (is.null(headers$`X-UA-Compatible`))
     headers$`X-UA-Compatible` <- "chrome=1"
   resp <- list(status = status, content_type = content_type, content = content,

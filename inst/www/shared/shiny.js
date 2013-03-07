f0109c558814cb39cfbe5f17416edbfe4803e56d
Winston Chang <winston@stdout.org>
2013-03-07 17:01:15 -0600

    Send URL in clientdata

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1561,6 +1561,16 @@
       inputs.setInput('.clientdata_pixelratio', pixelRatio());
     });
 
+    // Send initial URL
+    // The hash isn't sent because it can change, and watching for changes can
+    // require polling on some browsers. The JQuery hashchange plugin can be
+    // used if this capability is important.
+    initialValues['.clientdata_url_protocol'] = window.location.protocol;
+    initialValues['.clientdata_url_hostname'] = window.location.hostname;
+    initialValues['.clientdata_url_port']     = window.location.port;
+    initialValues['.clientdata_url_pathname'] = window.location.pathname;
+    initialValues['.clientdata_url_search']   = window.location.search;
+
     // We've collected all the initial values--start the server process!
     inputsNoResend.reset(initialValues);
     shinyapp.connect(initialValues);

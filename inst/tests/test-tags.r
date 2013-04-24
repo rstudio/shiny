619b4824f06be0fc0846c9e0728d5f36d63a1d08
Winston Chang <winston@stdout.org>
2013-04-24 11:29:27 -0500

    Add tests for withTags

--- inst/tests/test-tags.r
+++ inst/tests/test-tags.r
@@ -21,3 +21,30 @@ test_that("Basic tag writing works", {
     as.character(tagList(tags$br(), "one")),
     HTML("<br/>\none"))
 })
+
+
+test_that("withTags works", {
+  output_tags <- tags$div(class = "myclass",
+    tags$h3("header"),
+    tags$p("text here")
+  )
+  output_withhtml <- withTags(
+    div(class = "myclass",
+      h3("header"),
+      p("text here")
+    )
+  )
+  expect_identical(output_tags, output_withhtml)
+
+
+  # Check that current environment is searched
+  x <- 100
+  expect_identical(tags$p(x), withTags(p(x)))
+
+  # Just to make sure, run it in a function, which has its own environment
+  foo <- function() {
+    y <- 100
+    withTags(p(y))
+  }
+  expect_identical(tags$p(100), foo())
+})

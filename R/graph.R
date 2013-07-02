917434cb6b8fab8b8e026a9c3cb624999765d996
Joe Cheng <joe@rstudio.org>
2013-07-02 03:03:29 -0700

    Introduce shiny.reactlog function

--- R/graph.R
+++ R/graph.R
@@ -4,37 +4,45 @@ writeReactLog <- function(file=stdout()) {
 }
 
 .graphAppend <- function(logEntry) {
-  .graphEnv$log <- c(.graphEnv$log, list(logEntry))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphEnv$log <- c(.graphEnv$log, list(logEntry))
 }
 
 .graphDependsOn <- function(id, label) {
-  .graphAppend(list(action='dep', id=id, dependsOn=label))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(action='dep', id=id, dependsOn=label))
 }
 
 .graphDependsOnId <- function(id, dependee) {
-  .graphAppend(list(action='depId', id=id, dependsOn=dependee))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(action='depId', id=id, dependsOn=dependee))
 }
 
 .graphCreateContext <- function(id, label, type, prevId) {
-  .graphAppend(list(
-    action='ctx', id=id, label=paste(label, collapse='\n'), type=type, prevId=prevId
-  ))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(
+      action='ctx', id=id, label=paste(label, collapse='\n'), type=type, prevId=prevId
+    ))
 }
 
 .graphEnterContext <- function(id) {
-  .graphAppend(list(action='enter', id=id))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(action='enter', id=id))
 }
 
 .graphExitContext <- function(id) {
-  .graphAppend(list(action='exit', id=id))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(action='exit', id=id))
 }
 
 .graphValueChange <- function(label) {
-  .graphAppend(list(action='valueChange', id=label))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(action='valueChange', id=label))
 }
 
 .graphInvalidate <- function(id) {
-  .graphAppend(list(action='invalidate', id=id))
+  if (isTRUE(getOption('shiny.reactlog', FALSE)))
+    .graphAppend(list(action='invalidate', id=id))
 }
 
 .graphEnv <- new.env()

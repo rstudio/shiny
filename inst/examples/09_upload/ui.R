aef8837b5d59c830d3286b1d639f766a18c0e56e
Yihui Xie <xie@yihui.name>
2013-10-21 21:25:42 -0500

    add .csv to fileInput() to make it consistent with the tutorial (#280)

--- inst/examples/09_upload/ui.R
+++ inst/examples/09_upload/ui.R
@@ -4,7 +4,7 @@ shinyUI(pageWithSidebar(
   headerPanel("Uploading Files"),
   sidebarPanel(
     fileInput('file1', 'Choose CSV File',
-              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
     tags$hr(),
     checkboxInput('header', 'Header', TRUE),
     radioButtons('sep', 'Separator',

2b9553e4da72874e64e1a6fa51d7c23eeb10e56f
JJ Allaire <jj@rstudio.org>
2013-12-10 11:35:56 -0500

    refactor functions to use fixed and fluid explicitly and to introduce the notion of "layout" functions

--- R/bootstrap-layout.R
+++ R/bootstrap-layout.R
@@ -1,106 +1,206 @@
 
-#' Functions for creating top-level page containers
-#' 
-#' Functions for creating a top-level application page that uses either 
-#' fluid or fixed layout. Fluid pages scale their components in realtime to 
-#' fill all available browser width. Fixed pages scale components at predefined
-#' size thresholds and limit the total width to 940 pixels.
-#' 
-#' @param head Tag or list of tags to be inserted into the head of the document
-#' (for example, addition of required Javascript or CSS resources via
-#' \code{tags$script} or \code{tags$style})
-#'   
-#' @return A UI defintion that can be passed to the \link{shinyUI} function.
-#' 
-#' @details The \code{isPageFluid} function can be used within other UI 
-#' definition functions to determine whether the current top-level page 
-#' is using fluid or fixed layout.
-#' 
-#' See the documentation on the bootstrap \href{http://getbootstrap.com/2.3.2/scaffolding.html#fluidGridSystem}{fluid grid system} and 
-#' \href{http://getbootstrap.com/2.3.2/scaffolding.html#gridSystem}{fixed grid system} for more details on the differences between 
-#' fluid and fixed pages.
-#' 
-#' @seealso \code{\link{gridContainer}}, \code{\link{gridCol}}, \code{\link{gridRow}}
-#' 
-#' @rdname pageContainers
-#' 
-#' @export
-fluidPage <- function(..., head = list()) {
-  containerPage(fluid = TRUE, ..., head)
-}
-
-#' @rdname pageContainers
-#' @export
-fixedPage <- function(..., head = list()) {
-  containerPage(fluid = FALSE, ..., head)
-}
-
-#' @rdname pageContainers
-#' @export
-isPageFluid <- function() {
-  .globals$pageIsFluid
-}
-
-# Store global state on whether the page currently being built is fluid
-.globals$pageIsFluid <- FALSE
-
-# Helper function to build a container page (fluidPage or fixedPage)
-containerPage <- function(fluid, ..., head = list()) {
-  
-  # set the fluid status for the page then unset it when done
-  .globals$pageIsFluid <- fluid  
-  on.exit(.globals$pageIsFluid <- FALSE)
-  
-  # build page
-  containerDiv <- gridContainer()
-  for (containerRow in list(...)) 
-    containerDiv <- tagAppendChild(containerDiv, gridRow(containerRow))
-  bootstrapPage(containerDiv, head = head)
-}
-
-
 #' Functions for creating bootstrap grid layouts
 #' 
-#' @param fluid Whether to use fluid layout for the grid. Automatically 
-#' defaults to the appropriate value for the current page being built.
+#' Functions for creating a bootstrap fluid or fixed grid layout. Fluid layouts 
+#' scale their components in realtime to fill all available browser width. 
+#' Fixed layouts scale components at predefined size thresholds and limit the
+#' total width to 940 pixels.
 #' 
-#' @return A grid container or element that can be added to a UI definition.
+#' @param ... Elements to include within the grid
 #' 
-#' @details See the documentation on the bootstrap \href{http://getbootstrap.com/2.3.2/scaffolding.html#fluidGridSystem}{fluid grid system} and 
-#' \href{http://getbootstrap.com/2.3.2/scaffolding.html#gridSystem}{fixed grid system} for more details on using containers, rows, and columns to layout pages.
+#' @return A bootstrap grid layout that can be added to a UI definition.
 #' 
-#' @seealso \code{\link{fluidPage}}, \code{\link{fixedPage}}
+#' @details A bootstrap grid layout consists of containers that include rows 
+#' which in turn include columns. Rows exist for the purpose of making sure
+#' their elements appear on the same line (if the browser has adequate width).
+#' Columns exist for the purpose of defining how much horizontal space within
+#' a 12-unit wide grid it's elements should occupy.
+#' 
+#' To create a fluid grid layout use the \code{fluidContainer} function and 
+#' include instances of \code{fluidRow} within it. To create a fixed grid
+#' layout use the \code{fixedContainer} function includes instances of 
+#' \code{fixedRow} within it.
+#' 
+#' @note See the documentation on the bootstrap \href{http://getbootstrap.com/2.3.2/scaffolding.html#fluidGridSystem}{fluid grid system} and 
+#' \href{http://getbootstrap.com/2.3.2/scaffolding.html#gridSystem}{fixed grid system}
+#' for more details on the differences between fluid and fixed layouts.
+#' 
+#' @seealso \code{\link{bootstrapPage}}, \code{\link{column}}
+#' 
+#' @examples
+#' shinyUI(bootstrapPage(
+#'   fluidContainer(
+#'     fluidRow(
+#'       column(width = 4,
+#'         "4"
+#'       ),
+#'       column(width = 3, offset = 2,
+#'         "3 offset 2"
+#'       )
+#'     )
+#'  )
+#' ))
 #' 
 #' @rdname gridLayout
 #' @export
-gridContainer <- function(..., fluid = isPageFluid()) {
-  div(class = gridClass("container", fluid), ...)
+fluidContainer <- function(...) {
+  div(class = "container-fluid", ...)
 }
 
 #' @rdname gridLayout
 #' @export
-gridRow <- function(..., fluid = isPageFluid()) {
-  div(class = gridClass("row", fluid), ...)
+fluidRow <- function(...) {
+  div(class = "row-fluid", ...)
 }
 
 #' @rdname gridLayout
+#' @export
+fixedContainer <- function(...) {
+  div(class = "container", ...)
+}
+
+#' @rdname gridLayout
+#' @export
+fixedRow <- function(...) {
+  div(class = "row", ...)
+}
+
+
+#' Create a column within a UI definition
+#' 
+#' Create a column for use within a \code{\link{columnLayout}},
+#' \code{\link{fluidRow}}, or \code{\link{fixedRow}}
 #' 
 #' @param width The grid width of the column (must be between 1 and 12)
+#' @param ... Elements to include within the column
 #' @param offset The number of columns to offset this column from the 
 #' end of the previous column.
 #' 
+#' @return A column that can be included within a \code{columnLayout}, \code{\link{fluidRow}}, or \code{\link{fixedRow}}.
+#' 
+#' 
 #' @export
-gridCol <- function(width, ..., offset = NA) {
+column <- function(width, ..., offset = 0) {
+  
+  # TODO: validate that the columns width is from 1 to 12
+  
   colClass <- paste("span", width, sep="")
-  if (!is.na(offset))
+  if (offset > 0)
     colClass <- paste(colClass, " offset", offset, sep="")
   div(class = colClass, ...)
 }
 
-# Helper function to append -fluid to containers and rows if appropriate
-gridClass <- function(class, fluid = isPageFluid()) {
-  if (fluid)
-    paste(class, "-fluid", sep="")
-  else
-    class
+
+#' Create a panel containing an application title.
+#' 
+#' @param title An application title to display
+#' @param windowTitle The title that should be displayed by the browser window.
+#' 
+#' @details Calling this function has the side effect of including a 
+#' \code{title} tag within the head. 
+#'     
+#' @examples
+#' titlePanel("Hello Shiny!")
+#' 
+#' @export
+titlePanel <- function(title, windowTitle=title) {    
+  tagList(
+    tags$head(tags$title(windowTitle)),
+    fluidContainer(fluidRow(h2(title)))
+  )
 }
+
+#' Create a container panel
+#' 
+#' Create a container that includes one or more rows of content (each element
+#' passed to the container will appear on it's own line in the UI)
+#' 
+#' @param ... Elements to include within the container
+#' 
+#' @export
+containerPanel <- function(...) {
+  rows <- list(...)
+  container <- fluidContainer()
+  for (row in rows)
+    container <- tagAppendChild(container, fluidRow(row))
+  container
+}
+
+#' Layout a sidebar and main area
+#' 
+#' Create a layout with a sidebar and main area. The sidebar is displayed with
+#' a distinct background color and typically contains input controls. The
+#' main area occupies 2/3 of the horizontal width and typically contains 
+#' outputs. 
+#' 
+#' @param sidebarPanel The \link{sidebarPanel} containing input controls
+#' @param mainPanel The \link{mainPanel} containing outputs
+#' @param position The position of the sidebar relative to the main area
+#' ("left" or "right")
+#' 
+#' @examples
+#' # Define UI
+#' shinyUI(bootstrapPage(
+#'   
+#'   # Application title
+#'   titlePanel("Hello Shiny!"),
+#'   
+#'   sidebarLayout(
+#'   
+#'     # Sidebar with a slider input
+#'     sidebarPanel(
+#'       sliderInput("obs", 
+#'                   "Number of observations:", 
+#'                   min = 0, 
+#'                   max = 1000, 
+#'                   value = 500)
+#'     ),
+#'   
+#'     # Show a plot of the generated distribution
+#'     mainPanel(
+#'       plotOutput("distPlot")
+#'     )
+#'   )
+#' ))
+#'
+#' @export
+sidebarLayout <- function(sidebarPanel,
+                          mainPanel,
+                          position = c("left", "right")) {
+   
+  # TODO: validate that sidebarPanel and mainPanel were created by
+  # their respective functions (i.e. have the correct span)
+  
+  # determine the order 
+  position <- match.arg(position)
+  if (position == "left") {
+    firstPanel <- sidebarPanel
+    secondPanel <- mainPanel
+  } 
+  else if (position == "right") {
+    firstPanel <- mainPanel
+    secondPanel <- sidebarPanel
+  }
+  
+  # return as a column layout
+  columnLayout(firstPanel, secondPanel)
+}
+
+#' Layout a set of columns
+#' 
+#' Layout a set of columns created using the \code{\link{column}} function. The
+#' widths of the columns should total no more than 12 units.
+#' 
+#' @param ... Columns to include within the layout
+#' 
+#' @export
+columnLayout <- function(...) {
+  
+  # TODO: validate that all of the columns have a spanX in their class
+  
+  fluidContainer(
+    fluidRow(...)
+  )
+}
+
+

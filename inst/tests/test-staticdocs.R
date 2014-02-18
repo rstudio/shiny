73411c75dbe4fa4d1c467f9a3d1d9b091b15f7ed
Joe Cheng <joe@rstudio.org>
2014-02-18 12:52:59 -0800

    Fix staticdocs test on Linux

--- inst/tests/test-staticdocs.R
+++ inst/tests/test-staticdocs.R
@@ -1,26 +1,26 @@
 context("staticdocs")
 
-test_that("All man pages have an entry in staticdocs/index.R", {
+test_that("All man pages have an entry in staticdocs/index.r", {
   if (!all(file.exists(c('../../staticdocs', '../../man')))) {
     # This test works only when run against a package directory
     return()
   }
   # Known not to be indexed
   known_unindexed <- c("shiny-package")
-  
+
   indexed_topics <- local({
     result <- character(0)
     sd_section <- function(dummy1, dummy2, section_topics) {
       result <<- c(result, section_topics)
     }
-    source("../../staticdocs/index.R", local = TRUE)
+    source("../../staticdocs/index.r", local = TRUE)
     result
   })
-  
+
   all_topics <- sub("\\.Rd", "", list.files("../../man", pattern = "*.Rd"))
-  
+
   # This test ensures that every documented topic is included in
-  # staticdocs/index.R, unless explicitly waived by specifying it
+  # staticdocs/index.r, unless explicitly waived by specifying it
   # in the known_unindexed variable above.
   expect_equivalent(sort(all_topics), sort(c(known_unindexed, indexed_topics)))
 })

69c0414791735e9ce3f66017163de75103e6a3b6
Joe Cheng <joe@rstudio.org>
2014-03-11 09:08:20 -0700

    Make showcase mode work with .r files

--- R/showcase.R
+++ R/showcase.R
@@ -71,7 +71,7 @@ appMetadata <- function(desc) {
 # Returns tags containing the application's code in Bootstrap-style tabs in
 # showcase mode.
 showcaseCodeTabs <- function(codeLicense) {
-  rFiles <- list.files(pattern = "\\.R$")
+  rFiles <- list.files(pattern = "\\.[rR]$")
   with(tags, div(id="showcase-code-tabs",
     a(id="showcase-code-position-toggle",
       class="btn btn-default btn-small",
@@ -79,7 +79,7 @@ showcaseCodeTabs <- function(codeLicense) {
       i(class="fa fa-level-up", "show with app")),
     ul(class="nav nav-tabs",
        lapply(rFiles, function(rFile) {
-         li(class=if (rFile == "server.R") "active" else "",
+         li(class=if (tolower(rFile) == "server.r") "active" else "",
             a(href=paste("#", gsub(".", "_", rFile, fixed=TRUE),
                          "_code", sep=""),
               "data-toggle"="tab", rFile))
@@ -87,7 +87,7 @@ showcaseCodeTabs <- function(codeLicense) {
     div(class="tab-content", id="showcase-code-content",
         lapply(rFiles, function(rFile) {
           div(class=paste("tab-pane",
-                          if (rFile == "server.R") " active" else "",
+                          if (tolower(rFile) == "server.r") " active" else "",
                           sep=""),
               id=paste(gsub(".", "_", rFile, fixed=TRUE),
                        "_code", sep=""),

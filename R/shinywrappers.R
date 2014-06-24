1983f60ec6739f08c2b884b34418933514e8fb4a
Joe Cheng <joe@rstudio.com>
2014-06-24 06:09:36 -0700

    Revert "tweak after #492: capture.output() does withVisible() and print() if necessary"
    This reverts commit 451f950d0dd54ad57ec5b9b5ab465a2ac6d29a9b.
    
    Too-aggressive capture.output was causing browser() not to work in
    renderPlot().

--- R/shinywrappers.R
+++ R/shinywrappers.R
@@ -124,9 +124,13 @@ renderPlot <- function(expr, width='auto', height='auto', res=72, ...,
 
     coordmap <- NULL
     plotFunc <- function() {
-      # Actually perform the plotting: use capture.output() to suppress output
-      # to console, and print func() if it returns a visible value
-      capture.output(func())
+      # Actually perform the plotting
+      result <- withVisible(func())
+      if (result$visible) {
+        # Use capture.output to squelch printing to the actual console; we
+        # are only interested in plot output
+        capture.output(print(result$value))
+      }
 
       # Now capture some graphics device info before we close it
       usrCoords <- par('usr')

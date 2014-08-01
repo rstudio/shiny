c095085d8420a6963711a8d1fdacd70d05a66455
Yihui Xie <xie@yihui.name>
2014-08-01 16:36:41 -0500

    fixes #557: choicesWithNames() turns a character vector to a list, which is no longer appropriate as a column in data.frame()

--- R/update-input.R
+++ R/update-input.R
@@ -455,8 +455,8 @@ selectizeJSON <- function(data, req) {
   # convert a single vector to a data frame so it returns {label: , value: }
   # later in JSON; other objects return arbitrary JSON {x: , y: , foo: , ...}
   data <- if (is.atomic(data)) {
-    data <- choicesWithNames(data)
-    data.frame(label = names(data), value = data, stringsAsFactors = FALSE)
+    data.frame(label = names(choicesWithNames(data)), value = data,
+               stringsAsFactors = FALSE)
   } else as.data.frame(data, stringsAsFactors = FALSE)
 
   # start searching for keywords in all specified columns

1e1b3c8f5f2cd928845beec06c356cecb6fdaf75
Winston Chang <winston@stdout.org>
2014-08-28 17:31:53 -0500

    Faster stack implementation

--- R/stack.R
+++ R/stack.R
@@ -1,28 +1,69 @@
+# A Stack object backed by a list. The backing list will grow or shrink as
+# the stack changes in size.
 Stack <- R6Class(
   'Stack',
   portable = FALSE,
   public = list(
-    .stack = list(),
 
-    push = function(obj) {
-      .stack <<- c(.stack, list(obj))
+    initialize = function(init = 20L) {
+      # init is the initial size of the list. It is also used as the minimum
+      # size of the list as it shrinks.
+      private$stack <- vector("list", init)
+      private$init <- init
+    },
+
+    push = function(..., .list = NULL) {
+      args <- c(list(...), .list)
+      new_size <- count + length(args)
+
+      # Grow if needed; double in size
+      while (new_size > length(stack)) {
+        stack[length(stack) * 2] <<- list(NULL)
+      }
+      stack[count + seq_along(args)] <<- args
+      count <<- new_size
+
       invisible(self)
     },
+
     pop = function() {
-      len <- length(.stack) 
-      if (len == 0)
+      if (count == 0L)
         return(NULL)
-      obj <- .stack[[len]]
-      .stack <<- .stack[-len]
-      obj
+
+      value <- stack[[count]]
+      stack[count] <<- list(NULL)
+      count <<- count - 1L
+
+      # Shrink list if < 1/4 of the list is used, down to a minimum size of `init`
+      len <- length(stack)
+      if (len > init && count < len/4) {
+        new_len <- max(init, ceiling(len/2))
+        stack <<- stack[seq_len(new_len)]
+      }
+
+      value
     },
+
     peek = function() {
-      len <- length(.stack) 
-      if (len == 0) return(NULL)
-      .stack[[len]]
+      if (count == 0L)
+        return(NULL)
+      stack[[count]]
     },
+
     size = function() {
-      length(.stack)
+      count
+    },
+
+    # Return the entire queue as a list, where the first item in the list is the
+    # oldest item in the stack, and the last item is the most recently added.
+    as_list = function() {
+      stack[rev(seq_len(count))]
     }
+  ),
+
+  private = list(
+    stack = NULL,   # A list that holds the items
+    count = 0L,     # Current number of items in the stack
+    init = 20L      # Initial and minimum size of the stack
   )
 )
---
+++ inst/tests/test-stack.R
@@ -0,0 +1,40 @@
+context("Stack")
+
+test_that("Basic operations", {
+  s <- Stack$new()
+  expect_identical(s$size(), 0L)
+
+  s$push(5)$push(6)$push(NULL)$push(list(a=1,b=2))
+
+  expect_identical(s$pop(), list(a=1,b=2))
+  expect_identical(s$peek(), NULL)
+  expect_identical(s$pop(), NULL)
+  expect_identical(s$size(), 2L)
+
+  # as_list() returns in the reverse order that they were inserted
+  expect_identical(s$as_list(), list(6, 5))
+})
+
+
+test_that("Pushing multiple", {
+  s <- Stack$new()
+  s$push(1,2,3)
+  s$push(4,5, .list=list(6,list(7,8)))
+  s$push(9,10)
+  expect_identical(s$as_list(), list(10,9,list(7,8),6,5,4,3,2,1))
+  expect_identical(s$pop(), 10)
+  expect_identical(s$pop(), 9)
+  expect_identical(s$pop(), list(7,8))
+})
+
+
+test_that("Popping from empty stack", {
+  s <- Stack$new()
+  expect_null(s$pop())
+  expect_null(s$pop())
+  expect_null(s$peek())
+  expect_identical(s$size(), 0L)
+
+  s$push(5)$push(6)
+  expect_identical(s$as_list(), list(6, 5))
+})

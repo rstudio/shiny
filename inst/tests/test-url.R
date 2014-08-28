5012831e7bef6073fe05cb434bb394a7779329c6
Winston Chang <winston@stdout.org>
2014-08-28 15:59:50 -0500

    Add tests for nested query strings

--- inst/tests/test-url.R
+++ inst/tests/test-url.R
@@ -17,4 +17,26 @@ test_that("Query string parsing", {
 
   # Should be the same with or without leading question mark
   expect_identical(parseQueryString("?foo=1&bar=b"), parseQueryString("foo=1&bar=b"))
+
+
+  # Nested and non-nested query strings
+  expect_identical(
+    parseQueryString("a[i1][j1]=x&b[i1][j1]=y&b[i2][j1]=z"),
+    list(
+      "a[i1][j1]" = "x",
+      "b[i1][j1]" = "y",
+      "b[i2][j1]" = "z"
+    )
+  )
+
+  expect_identical(
+    parseQueryString("a[i1][j1]=x&b[i1][j1]=y&b[i2][j1]=z", nested = TRUE),
+    list(
+      a = list(i1 = list(j1 = "x")),
+      b = list(
+        i1 = list(j1 = "y"),
+        i2 = list(j1 = "z")
+      )
+    )
+  )
 })

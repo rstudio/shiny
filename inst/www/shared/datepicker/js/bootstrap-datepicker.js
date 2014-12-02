28718429e30ec5ed3df00f7870f963adebca31fa
Winston Chang <winston@stdout.org>
2014-12-02 09:52:03 -0600

    Fix arrows for date inputs. Fixes #663
    The date inputs used css classes like icon-arrow-right, which no longer works
    with font-awesome 4.x. This change uses glyphicon arrows instead, which are
    included with Bootstrap and therefore don't need a separate HTML dependency.

--- inst/www/shared/datepicker/js/bootstrap-datepicker.js
+++ inst/www/shared/datepicker/js/bootstrap-datepicker.js
@@ -65,7 +65,7 @@
 		if (this.isRTL){
 			this.picker.addClass('datepicker-rtl');
 			this.picker.find('.prev i, .next i')
-						.toggleClass('icon-arrow-left icon-arrow-right');
+						.toggleClass('glyphicon-arrow-left glyphicon-arrow-right');
 		}
 
 		this.autoclose = false;
@@ -1112,9 +1112,9 @@
 		},
 		headTemplate: '<thead>'+
 							'<tr>'+
-								'<th class="prev"><i class="icon-arrow-left"/></th>'+
+								'<th class="prev"><i class="glyphicon glyphicon-arrow-left"/></th>'+
 								'<th colspan="5" class="datepicker-switch"></th>'+
-								'<th class="next"><i class="icon-arrow-right"/></th>'+
+								'<th class="next"><i class="glyphicon glyphicon-arrow-right"/></th>'+
 							'</tr>'+
 						'</thead>',
 		contTemplate: '<tbody><tr><td colspan="7"></td></tr></tbody>',

00f409d21de02608ac9eaca02f29119608ef6a0d
bhsu <bhsu22+github@gmail.com>
2015-01-14 13:34:27 -0500

    explicit parameter check

--- R/run-url.R
+++ R/run-url.R
@@ -71,8 +71,11 @@ runUrl <- function(url, filetype = NULL, subdir = NULL, destDir = NULL, ...) {
     first <- as.character(unzip(filePath, list=TRUE)$Name)[1]
     unzip(filePath, exdir = fileDir)
   }
-  on.exit(unlink(fileDir, recursive = is.null(destDir)), add = TRUE)
 
+  if(is.null(destDir)){
+    on.exit(unlink(fileDir, recursive = TRUE), add = TRUE)
+  }
+  
   appdir <- file.path(fileDir, first)
   if (!file_test('-d', appdir)) appdir <- dirname(appdir)
 

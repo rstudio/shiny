976a7684462ca71525bd343a724cd2e4c93acc1c
Yihui Xie <xie@yihui.name>
2015-05-17 14:29:44 -0500

    white spaces

--- inst/examples/04_mpg/server.R
+++ inst/examples/04_mpg/server.R
@@ -12,22 +12,22 @@ mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 # Define server logic required to plot various variables against
 # mpg
 shinyServer(function(input, output) {
-  
+
   # Compute the formula text in a reactive expression since it is
   # shared by the output$caption and output$mpgPlot functions
   formulaText <- reactive({
     paste("mpg ~", input$variable)
   })
-   
+
   # Return the formula text for printing as a caption
   output$caption <- renderText({
     formulaText()
   })
-  
+
   # Generate a plot of the requested variable against mpg and
   # only include outliers if requested
   output$mpgPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
+    boxplot(as.formula(formulaText()),
             data = mpgData,
             outline = input$outliers)
   })

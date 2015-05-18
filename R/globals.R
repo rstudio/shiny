90c00bed2f452f7935383967989f437f984e0681
Winston Chang <winston@stdout.org>
2015-05-18 11:56:02 -0500

    Check htmlwidgets version

--- R/globals.R
+++ R/globals.R
@@ -7,3 +7,17 @@
   # the private seed during load.
   withPrivateSeed(reinitializeSeed())
 }
+
+.onAttach <- function(libname, pkgname) {
+  # Check for htmlwidgets version, if installed. As of Shiny 0.12.0 and
+  # htmlwidgets 0.4, both packages switched from RJSONIO to jsonlite. Because of
+  # this change, Shiny 0.12.0 will work only with htmlwidgets >= 0.4, and vice
+  # versa.
+  if (system.file(package = "htmlwidgets") != "" &&
+      utils::packageVersion("htmlwidgets") < "0.4") {
+    packageStartupMessage(
+      "This version of Shiny is designed to work with htmlwidgets >= 0.4. ",
+      "Please upgrade your version of htmlwidgets."
+    )
+  }
+}

6c6775376e5536f16453107bd91e5f7eebeea67c
Joe Cheng <joe@rstudio.com>
2015-09-23 18:28:08 -0700

    Add placeholder parameter to textInput

--- R/input-text.R
+++ R/input-text.R
@@ -7,6 +7,9 @@
 #' @param value Initial value.
 #' @param width The width of the input, e.g. \code{'400px'}, or \code{'100\%'};
 #'   see \code{\link{validateCssUnit}}.
+#' @param placeholder A character string giving the user a hint as to what can
+#'   be entered into the control. Internet Explorer 8 and 9 do not support this
+#'   option.
 #' @return A text input control that can be added to a UI definition.
 #'
 #' @family input elements
@@ -15,10 +18,13 @@
 #' @examples
 #' textInput("caption", "Caption:", "Data Summary")
 #' @export
-textInput <- function(inputId, label, value = "", width = NULL) {
+textInput <- function(inputId, label, value = "", width = NULL,
+  placeholder = NULL) {
+
   div(class = "form-group shiny-input-container",
     style = if (!is.null(width)) paste0("width: ", validateCssUnit(width), ";"),
     label %AND% tags$label(label, `for` = inputId),
-    tags$input(id = inputId, type="text", class="form-control", value=value)
+    tags$input(id = inputId, type="text", class="form-control", value=value,
+      placeholder = placeholder)
   )
 }

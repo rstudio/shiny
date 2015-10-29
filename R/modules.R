rstudio/shiny

commit 07f2792cf9f65e964d32daf6ae87567128bb9742

Change evil `=` to righteous `<-`

R/modules.R
@@ -3,8 +3,8 @@
 # that value is returned instead. (See Decorator pattern.)
 createSessionProxy <- function(parentSession, ...) {
   e <- new.env(parent = emptyenv())
-  e$parent = parentSession
-  e$overrides = list(...)
+  e$parent <= parentSession
+  e$overrides <- list(...)
 
   structure(
     e,

commit 7e25a1566ffd2737262a84c2b5149363155e20b4

Correct a regression within modules
Commit 07f2792cf9f65e964d32daf6ae87567128bb9742 introduced an error, replacing `e$parent = parentSession` with `e$parent <= parentSession`, while it should have been `e$parent <- parentSession`

R/modules.R
@@ -3,7 +3,7 @@
 # that value is returned instead. (See Decorator pattern.)
 createSessionProxy <- function(parentSession, ...) {
   e <- new.env(parent = emptyenv())
-  e$parent <= parentSession
+  e$parent <- parentSession
   e$overrides <- list(...)
 
   structure(
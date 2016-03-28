4462b6bd39e46e3d2b2e8bea3fb2abf4fdea6d60
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-03-28 13:00:11 +0100

    changed a warning to an error, following the "fail fast" principle

--- R/shinywrappers.R
+++ R/shinywrappers.R
@@ -54,9 +54,9 @@ useRenderFunction <- function(renderFunc, inline = FALSE) {
 
   for (arg in names(outputArgs)) {
     if (!arg %in% names(formals(outputFunction))) {
-      warning(paste0("Unused argument: in 'outputArgs', '",
-                     arg, "' is not an valid argument for ",
-                     "the output function"))
+      stop(paste0("Unused argument: in 'outputArgs', '",
+                  arg, "' is not an valid argument for ",
+                  "the output function"))
       outputArgs[[arg]] <- NULL
     }
   }

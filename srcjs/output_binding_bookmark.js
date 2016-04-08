8a7c5c18d04951afa0ac4c340e539c17e9316bd1
Winston Chang <winston@stdout.org>
2016-04-08 14:59:24 -0500

    Add tooltip on copy

--- srcjs/output_binding_bookmark.js
+++ srcjs/output_binding_bookmark.js
@@ -1,20 +1,75 @@
-var bookmarkOutputBinding = new OutputBinding();
-$.extend(bookmarkOutputBinding, {
-  find: function(scope) {
+(function() {
+  var bookmarkOutputBinding = new OutputBinding();
+
+  function find(scope) {
     return $(scope).find('.shiny-bookmark-output');
-  },
-  renderValue: function(el, data) {
+  }
+
+  function renderValue(el, data) {
     const $el = $(el);
     const $button = $el.find('button');
 
     // Initialize clipboardjs for this element, one time only
     if (!$button.data('clipboard-initialized')) {
-      new Clipboard($button[0]);
+      const clipboard = new Clipboard($button[0]);
+      _initTooltip(clipboard, $button);
       $button.data('clipboard-initialized', true);
     }
 
     $el.find('input')[0].value = location.protocol + '//' + location.host +
       location.pathname + '?' + data;
   }
-});
-outputBindings.register(bookmarkOutputBinding, 'shiny.bookmarkOutput');
+
+  function _initTooltip(clipboard, $el) {
+    // When clipboard action occurs, show tooltip
+    clipboard.on('success', function(e) {
+      $el.tooltip({
+        title: 'Copied!',
+        placement: 'auto bottom',
+        trigger: 'manual'
+      });
+      $el.tooltip('show');
+      _scheduleTooltipHide($el);
+    });
+
+    // Backup tooltip for platforms that don't support copying to clipboard
+    clipboard.on('error', function(e) {
+      $el.tooltip({
+        title: () => _fallbackMessage(e.action),
+        placement: 'auto bottom',
+        trigger: 'manual'
+      });
+      $el.tooltip('show');
+      _scheduleTooltipHide($el);
+    });
+  }
+
+  function _fallbackMessage(action) {
+    var actionMsg = '';
+    var actionKey = (action === 'cut' ? 'X' : 'C');
+
+    if(/iPhone|iPad/i.test(navigator.userAgent)) {
+      actionMsg = 'Not supported';
+    } else if (/Mac/i.test(navigator.userAgent)) {
+      actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
+    } else {
+      actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
+    }
+
+    return actionMsg;
+  }
+
+  function _scheduleTooltipHide($el, delay = 2000) {
+    setTimeout(
+      () => $el.tooltip('hide'),
+      delay
+    );
+  }
+
+
+  $.extend(bookmarkOutputBinding, {
+    find,
+    renderValue
+  });
+  outputBindings.register(bookmarkOutputBinding, 'shiny.bookmarkOutput');
+})();

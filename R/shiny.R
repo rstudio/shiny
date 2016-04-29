aa2b6446842e52a689321a9803b6c58c6bd2309a
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-04-29 05:27:26 +0100

    updated documentation; added ... argument to onFlush() and onFlushed() in order to be able to pass in arguments to the func

--- R/shiny.R
+++ R/shiny.R
@@ -197,17 +197,19 @@ workerId <- local({
 #' \item{onEnded(callback)}{
 #'   Synonym for \code{onSessionEnded}.
 #' }
-#' \item{onFlush(func, once=TRUE)}{
+#' \item{onFlush(func, once=TRUE, ...)}{
 #'   Registers a function to be called before the next time (if \code{once=TRUE})
 #'   or every time (if \code{once=FALSE}) Shiny flushes the reactive system.
-#'   Returns a function that can be called with no arguments to cancel the
-#'   registration.
+#'   The \code{...} argument allows you to pass arguments that should be applied
+#'   to \code{func} when it is called. Returns a function that can be called
+#'   with no arguments to cancel the registration.
 #' }
-#' \item{onFlushed(func, once=TRUE)}{
+#' \item{onFlushed(func, once=TRUE, ...)}{
 #'   Registers a function to be called after the next time (if \code{once=TRUE})
 #'   or every time (if \code{once=FALSE}) Shiny flushes the reactive system.
-#'   Returns a function that can be called with no arguments to cancel the
-#'   registration.
+#'   The \code{...} argument allows you to pass arguments that should be applied
+#'   to \code{func} when it is called. Returns a function that can be called
+#'   with no arguments to cancel the registration.
 #' }
 #' \item{onSessionEnded(callback)}{
 #'   Registers a function to be called after the client has disconnected.
@@ -737,13 +739,14 @@ ShinySession <- R6Class(
       # Add to input message queue
       private$inputMessageQueue[[length(private$inputMessageQueue) + 1]] <- data
     },
-    onFlush = function(flushCallback, once = TRUE) {
+    onFlush = function(flushCallback, once = TRUE, ...) {
       if (!isTRUE(once)) {
         return(private$flushCallbacks$register(flushCallback))
       } else {
         dereg <- private$flushCallbacks$register(function() {
           dereg()
-          flushCallback()
+          if (is.null(formals(flushedCallback))) flushedCallback()
+          else flushedCallback(...)
         })
         return(dereg)
       }

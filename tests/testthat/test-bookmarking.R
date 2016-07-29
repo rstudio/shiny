a2b761094ba28e4e19d94dece104614ac971827e
Winston Chang <winston@stdout.org>
2016-07-29 16:07:18 -0500

    Fix bookmark values tests

--- tests/testthat/test-bookmarking.R
+++ tests/testthat/test-bookmarking.R
@@ -4,32 +4,32 @@ test_that("Inputs and values in query string", {
   # Normal format
   vals <- RestoreContext$new("?_inputs_&a=1&b=2&_values_&x=3")$asList()
   expect_identical(vals$input, list(a=1L, b=2L))
-  expect_identical(vals$values, list(x=3L))
+  expect_identical(as.list(vals$values), list(x=3L))
 
   # No leading '?', trailing '&', and values before inputs
   vals <- RestoreContext$new("_values_&x=3&_inputs_&a=1&b=2&")$asList()
   expect_identical(vals$input, list(a=1L, b=2L))
-  expect_identical(vals$values, list(x=3L))
+  expect_identical(as.list(vals$values), list(x=3L))
 
   # Just inputs, no values, and leading '&'
   vals <- RestoreContext$new("&_inputs_&a=1&b=2")$asList()
   expect_identical(vals$input, list(a=1L, b=2L))
-  expect_identical(vals$values, list())
+  expect_identical(as.list(vals$values), list())
 
   # No inputs, just values
   vals <- RestoreContext$new("?_values_&x=3")$asList()
   expect_identical(vals$input, list())
-  expect_identical(vals$values, list(x=3L))
+  expect_identical(as.list(vals$values), list(x=3L))
 
   # Empty query string
   vals <- RestoreContext$new("")$asList()
   expect_identical(vals$input, list())
-  expect_identical(vals$values, list())
+  expect_identical(as.list(vals$values), list())
 
   # Other items (not inputs and not values)
   vals <- RestoreContext$new("?c=3&d=4")$asList()
   expect_identical(vals$input, list())
-  expect_identical(vals$values, list())
+  expect_identical(as.list(vals$values), list())
 
   # Multiple instances of _inputs_ or _values_
   expect_warning(suppressMessages(RestoreContext$new("?_inputs_&a=1&_inputs_")))
@@ -43,12 +43,12 @@ test_that("Inputs and values in query string", {
   # blank values.
   expect_warning(suppressMessages(rc <- RestoreContext$new("?_inputs_&a=[x&b=1")))
   expect_identical(rc$input$asList(), list())
-  expect_identical(rc$values, list())
+  expect_identical(as.list(rc$values), list())
   expect_identical(rc$dir, NULL)
 
   # Ignore query string if it's a subapp
   rc <- RestoreContext$new("?w=&__subapp__=1")
   expect_identical(rc$input$asList(), list())
-  expect_identical(rc$values, list())
+  expect_identical(as.list(rc$values), list())
   expect_identical(rc$dir, NULL)
 })

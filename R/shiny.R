013599890f888388cd637c862e9839dc2a70f560
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-08-09 19:08:30 +0100

    Solve updateSelectInput bug in IE 11.0 (#1277)

--- R/shiny.R
+++ R/shiny.R
@@ -1348,10 +1348,11 @@ ShinySession <- R6Class(
     registerDataObj = function(name, data, filterFunc) {
       # abusing downloads at the moment
       self$downloads$set(name, list(data = data, filter = filterFunc))
-      return(sprintf('session/%s/dataobj/%s?w=%s',
+      return(sprintf('session/%s/dataobj/%s?w=%s&nonce=%s',
                      URLencode(self$token, TRUE),
                      URLencode(name, TRUE),
-                     workerId()))
+                     workerId(),
+                     URLencode(createUniqueId(8), TRUE)))
     },
     # This function suspends observers for hidden outputs and resumes observers
     # for un-hidden outputs.

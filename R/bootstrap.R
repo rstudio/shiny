rstudio/shiny

commit 7bb939ab7f5d8a9daa271ee03e99bbf90d366440

Restore bookmarked tabs. Fixes #1282 (#1296)

R/bootstrap.R
@@ -337,6 +337,7 @@ navbarPage <- function(title,
 
   # build the tabset
   tabs <- list(...)
+  selected <- restoreInput(id = id, default = selected)
   tabset <- buildTabset(tabs, "nav navbar-nav", NULL, id, selected)
 
   # built the container div dynamically to support optional collapsibility
@@ -633,6 +634,7 @@ tabsetPanel <- function(...,
   # build the tabset
   tabs <- list(...)
   type <- match.arg(type)
+  selected <- restoreInput(id = id, default = selected)
   tabset <- buildTabset(tabs, paste0("nav nav-", type), NULL, id, selected)
 
   # create the content
@@ -697,6 +699,7 @@ navlistPanel <- function(...,
 
   # build the tabset
   tabs <- list(...)
+  selected <- restoreInput(id = id, default = selected)
   tabset <- buildTabset(tabs,
                         "nav nav-pills nav-stacked",
                         textFilter,

commit bc90fe6f99c28ee731f801a4bd66051b4e3555e9

Don't error when creating tabsets without id
The bug was introduced by #1296.

R/bootstrap.R
@@ -335,9 +335,11 @@ navbarPage <- function(title,
   if (inverse)
     navbarClass <- paste(navbarClass, "navbar-inverse")
 
+  if (!is.null(id))
+    selected <- restoreInput(id = id, default = selected)
+
   # build the tabset
   tabs <- list(...)
-  selected <- restoreInput(id = id, default = selected)
   tabset <- buildTabset(tabs, "nav navbar-nav", NULL, id, selected)
 
   # built the container div dynamically to support optional collapsibility
@@ -631,10 +633,13 @@ tabsetPanel <- function(...,
                     version = "0.10.2.2")
   }
 
+  if (!is.null(id))
+    selected <- restoreInput(id = id, default = selected)
+
   # build the tabset
   tabs <- list(...)
   type <- match.arg(type)
-  selected <- restoreInput(id = id, default = selected)
+
   tabset <- buildTabset(tabs, paste0("nav nav-", type), NULL, id, selected)
 
   # create the content
@@ -697,9 +702,11 @@ navlistPanel <- function(...,
       tags$li(class="navbar-brand", text)
   }
 
+  if (!is.null(id))
+    selected <- restoreInput(id = id, default = selected)
+
   # build the tabset
   tabs <- list(...)
-  selected <- restoreInput(id = id, default = selected)
   tabset <- buildTabset(tabs,
                         "nav nav-pills nav-stacked",
                         textFilter,
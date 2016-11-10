28965b7356c7c7b33bfc13c93ce1b2efa64814a7
Jonathan <jonathan@rstudio.com>
2016-11-10 13:10:18 -0800

    Render HTML dependency <meta> tag contents correctly (#1463)
    * render HTML dependency <meta> tag contents correctly
    
    * use direct address rather than loop; update NEWS

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -3148,8 +3148,10 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     var $head = $("head").first();
 
     if (dep.meta) {
-      var metas = $.map(asArray(dep.meta), function (content, name) {
-        return $("<meta>").attr("name", name).attr("content", content);
+      var metas = $.map(asArray(dep.meta), function (obj, idx) {
+        // only one named pair is expected in obj as it's already been decomposed
+        var name = Object.keys(obj)[0];
+        return $("<meta>").attr("name", name).attr("content", obj[name]);
       });
       $head.append(metas);
     }

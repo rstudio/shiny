8214b3bbb62891d59caa243644822775982c4f7e
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-11-23 00:26:37 +0000

    update documentation based on Winton's feedback

--- R/input-submit.R
+++ R/input-submit.R
@@ -1,56 +1,27 @@
 #' Create a submit button
 #'
-#' Create a submit button for an input form. Forms that include a submit
+#' Create a submit button for an app. Apps that include a submit
 #' button do not automatically update their outputs when inputs change,
 #' rather they wait until the user explicitly clicks the submit button.
+#' The use of \code{submitButton} is generally discouraged in favor of
+#' the more versatile \code{\link{actionButton}} (see details below).
 #'
-#' @section Warning:
-#' Submit buttons are very particular shiny objects. One the weirdest
-#' things about them is they have no IDs, and there is no way to regulate
-#' which inputs they should (and which ones they should not) be applied
-#' to. This means they are very unwieldy for anything but the simplest
-#' apps (see the example below). For example, having \emph{two} submit
-#' buttons in the same app will probably not do what you'd expect because
-#' submit buttons are not isolated in any way, so activating any of the
-#' two buttons will cause all inputs in the app to fire. Another problem
-#' with submit buttons is that dynamically created submit buttons (for
-#' example, with \code{\link{renderUI}} or \code{\link{insertUI}}) will
-#' not work.
+#' Submit buttons are unusual Shiny inputs, and we recommend using
+#' \code{\link{actionButton}} instead of \code{submitButton} when you
+#' want to delay a reaction.
+#' See \href{http://shiny.rstudio.com/articles/action-buttons.html}{this
+#' article} for more information (including a demo of how to "translate"
+#' code using a \code{submitButton} to code using an \code{actionButton}).
 #'
-#' For all of these reasons, \strong{our general reccomendation is to
-#' avoid submit buttons} and
-#' \href{http://shiny.rstudio.com/articles/action-buttons.html#pattern-2---delay-reactions}{
-#' use action buttons instead when you want to delay a reaction}. Any
-#' code that uses a submit button can be converted to code that uses an
-#' action button instead (while the reverse is almost never true). So,
-#' if you find your app getting increasingly complicated, opt for using
-#' an action button (or more since there is no problem with multiple
-#' action buttons in the same app). The link above shows an example of
-#' a delayed reaction using an action button. For another example, the
-#' code below converts the example in this page (at the bottom) to an
-#' equivalent app using an action button (using the same pattern
-#' described in the link above):
-#'
-#' \preformatted{
-#' shinyApp(
-#'   ui = basicPage(
-#'     numericInput("num", label = "Make changes", value = 1),
-#'     actionButton("update" ,"Update View", icon("refresh"),
-#'                  class = "btn btn-primary"),
-#'     helpText("When you click the button above, you should see",
-#'              "the output below update to reflect the value you",
-#'              "entered at the top:"),
-#'     verbatimTextOutput("value")
-#'   ),
-#'   server = function(input, output) {
-#'     currentValue <- eventReactive(input$update, {
-#'       input$num
-#'     }, ignoreNULL = FALSE)
-#'
-#'     output$value <- renderPrint({ currentValue() })
-#'   }
-#' )
-#' }
+#' In essence, the presence of a submit button stops all inputs from
+#' sending their values automatically to the server. This means, for
+#' instance, that if there are \emph{two} submit buttons in the same app,
+#' clicking either one will cause all inputs in the app to send their
+#' values to the server. This is probably not what you'd want, which is
+#' why submit button are unwieldy for all but the simplest apps. There
+#' are other problems with submit buttons: for example, dynamically
+#' created submit buttons (for example, with \code{\link{renderUI}}
+#' or \code{\link{insertUI}}) will not work.
 #'
 #' @param text Button caption
 #' @param icon Optional \code{\link{icon}} to appear on the button

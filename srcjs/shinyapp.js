9dcbd532e697e3c5ac664daef4d66ceeeba8c998
Winston Chang <winston@stdout.org>
2016-11-28 15:56:58 -0600

    Add getTestSnapshotBaseUrl function

--- srcjs/shinyapp.js
+++ srcjs/shinyapp.js
@@ -838,6 +838,26 @@ var ShinyApp = function() {
 
   exports.progressHandlers = progressHandlers;
 
+  // Returns a URL which can be queried to get values from inside the server
+  // function. This is enabled with `options(shiny.testmode=TRUE)`.
+  this.getTestSnapshotBaseUrl = function({ fullUrl = true } = {})
+  {
+    const loc = window.location;
+    let url = "";
+
+    if (fullUrl) {
+      // Strip off everything after last slash in path, like dirname() in R
+      url = loc.origin + loc.pathname.replace(/\/[^/]*$/, "");
+    }
+    url += "/session/" +
+      encodeURIComponent(this.config.sessionId) +
+      "/dataobj/shinytest?w=" +
+      encodeURIComponent(this.config.workerId) +
+      "&nonce=" + randomId();
+
+    return url;
+  };
+
 }).call(ShinyApp.prototype);
 
 

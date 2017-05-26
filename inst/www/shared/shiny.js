8fe6ecfb4b6d08e121b7a86df254aee26c4976d1
Alan Dipert <alan@dipert.org>
2017-05-26 14:12:03 -0700

    Grunt

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -4838,6 +4838,9 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         // invalidated reactives, but observers don't actually execute.
         self.shinyapp.makeRequest('uploadieFinish', [], function () {}, function () {});
         $(self.iframe).remove();
+        // Reset the file input's value to "". This allows the same file to be
+        // uploaded again. https://stackoverflow.com/a/22521275
+        $(self.fileEl).val("");
       };
       if (this.iframe.attachEvent) {
         this.iframe.attachEvent('onload', iframeDestroy);
@@ -4953,6 +4956,9 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         self.$setActive(false);
         self.onProgress(null, 1);
         self.$bar().text('Upload complete');
+        // Reset the file input's value to "". This allows the same file to be
+        // uploaded again. https://stackoverflow.com/a/22521275
+        $(evt.el).val("");
       }, function (error) {
         self.onError(error);
       });

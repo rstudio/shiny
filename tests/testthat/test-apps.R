4a2e1b65cb747c5b073b243dc848eab1cffc4d84
Winston Chang <winston@stdout.org>
2017-07-12 16:22:13 -0500

    Add shiny-test-apps submodule

---
+++ tests/testthat/test-apps.R
@@ -0,0 +1,12 @@
+context("apps")
+
+if (dir.exists("apps")) {
+  app_dirs <- Filter(dir.exists, dir("apps", full.names = TRUE))
+  if (length(app_dirs) > 0) {
+    for (app_dir in app_dirs) {
+      test_that(basename(app_dir), {
+        shinytest::expect_pass(shinytest::testApp(app_dir))
+      })
+    }
+  }
+}

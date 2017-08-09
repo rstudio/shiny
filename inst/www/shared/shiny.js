650075a9abe3f5cfa53339c280fd0f7de2f4f02a
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2017-08-10 00:45:25 +0100

    Fix appendTab for empty tabsetPanels (#1813)
    * fix appendTab for empty tabsetPanels; use spread operator to avoid having to resort to apply; upgrade grunt.
    
    * revert back to `Math.max.apply(null, existingTabIds) + 1;` there's no browser compatibility issues there

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1749,7 +1749,8 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       set the tab id (counter dummy) to "id" and the tabset id to "tsid")
       */
       function getTabIndex($tabset, tabsetId) {
-        var existingTabIds = [];
+        // The 0 is to ensure this works for empty tabsetPanels as well
+        var existingTabIds = [0];
         var leadingHref = "#tab-" + tabsetId + "-";
         // loop through all existing tabs, find the one with highest id
         // (since this is based on a numeric counter), and increment

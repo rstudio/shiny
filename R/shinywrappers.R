d5754515a65970e3f555ca60cb01212c25074f59
Joe Cheng <joe@rstudio.com>
2018-04-23 17:05:08 -0700

    Remove "experimental feature" tag from renderUI

--- R/shinywrappers.R
+++ R/shinywrappers.R
@@ -437,8 +437,7 @@ renderText <- function(expr, env=parent.frame(), quoted=FALSE,
 
 #' UI Output
 #'
-#' \bold{Experimental feature.} Makes a reactive version of a function that
-#' generates HTML using the Shiny UI library.
+#' Renders reactive HTML using the Shiny UI library.
 #'
 #' The corresponding HTML output tag should be \code{div} and have the CSS class
 #' name \code{shiny-html-output} (or use \code{\link{uiOutput}}).
@@ -452,7 +451,7 @@ renderText <- function(expr, env=parent.frame(), quoted=FALSE,
 #'   call to \code{\link{uiOutput}} when \code{renderUI} is used in an
 #'   interactive R Markdown document.
 #'
-#' @seealso conditionalPanel
+#' @seealso \code{\link{uiOutput}} 
 #' @export
 #' @examples
 #' ## Only run examples in interactive R sessions

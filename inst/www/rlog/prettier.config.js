7b39b7918385381ae366fbbdbc7517f2049e1d00
Barret Schloerke <schloerke@gmail.com>
2018-05-17 16:42:02 -0400

    added prettier config for rlog

---
+++ inst/www/rlog/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Task-specific options go here.
+  tabWidth: 2, // Specify the number of spaces per indentation-level.
+  useTabs: false, // Indent lines with tabs instead of spaces
+  printWidth: 80, // Specify the line length that the printer will wrap on.
+  semi: true, // Add a semicolon at the end of every statement.
+  singleQuote: false, // Use single quotes instead of double quotes.
+  trailingComma: "es5",  // Print trailing commas wherever possible when multi-line
+  bracketSpacing: true, // Print spaces between brackets in object literals
+  arrowParens: "avoid", // Include parentheses around a sole arrow function parameter.
+}

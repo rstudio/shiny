0386ed64096b101a9d6afc0c0b54ceec3efced23
Barret Schloerke <schloerke@gmail.com>
2018-05-29 09:34:06 -0400

    add index file for updateGraph to gather all exports for easy import

---
+++ inst/www/rlog/src/updateGraph/index.js
@@ -0,0 +1,23 @@
+// @flow
+
+import { atTick } from "./atTick";
+
+import {
+  nextEnterExitEmpty,
+  prevEnterExitEmpty,
+  firstEnterExitEmpty,
+  lastEnterExitEmpty,
+} from "./enterExit";
+export * from "./queueEmpty";
+export * from "./step";
+export * from "./tick";
+export * from "./searchString";
+export * from "./hoverStickyFilterSearch";
+
+export { atTick, atTick as updateGraph };
+export {
+  nextEnterExitEmpty,
+  prevEnterExitEmpty,
+  firstEnterExitEmpty,
+  lastEnterExitEmpty,
+};

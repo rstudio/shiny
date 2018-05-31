438b1c043ed3401f0cf7fa9a067c4b6b6f4572ec
Barret Schloerke <schloerke@gmail.com>
2018-05-31 13:57:56 -0400

    set app data as log

--- inst/www/rlog/src/log/initStep.js
+++ inst/www/rlog/src/log/initStep.js
@@ -6,6 +6,10 @@ import _ from "lodash";
 
 import type { LogType } from "./logStates";
 
+if (window.__APP_DATA__) {
+  window.log = window.__APP_DATA__;
+}
+
 _.map((window.log: LogType), function(entry, i: number) {
   entry.step = i;
 });

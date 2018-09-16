tidyverse/ggplot2

commit 01155ba2e9394e17066668eace50c13354e03b3a

Fix back-transformation of ranges in coords, without API cleanup (#2832)
* rename range() function of coord to backtransform_range()

* update news. closes #2149, #2812

* add range backtransformation function. closes #2820.

* fix typo in coord code

* add backtransform_range() to coord_map

* update summarise_layout to use backtransform_range() instead of range().

* Update coord documentation

* undo API cleanup

* update documentation

* rebase

* fix remaining issue from rebase

R/coord-.r
@@ -13,14 +13,22 @@
 #'   - `render_bg`: Renders background elements.
 #'   - `render_axis_h`: Renders the horizontal axes.
 #'   - `render_axis_v`: Renders the vertical axes.
-#'   - `range`: Returns the x and y ranges
+#'   - `range(panel_params)`: Extracts the panel range provided
+#'     in `panel_params` (created by `setup_panel_params()`, see below) and
+#'     back-transforms to data coordinates. This back-transformation is needed
+#'     for coords such as `coord_flip()`, `coord_polar()`, `coord_trans()` where
+#'     the range in the transformed coordinates differs from the range in the
+#'     untransformed coordinates.
 #'   - `transform`: Transforms x and y coordinates.
 #'   - `distance`: Calculates distance.
 #'   - `is_linear`: Returns `TRUE` if the coordinate system is
 #'     linear; `FALSE` otherwise.
 #'   - `is_free`: Returns `TRUE` if the coordinate system supports free
-#'     positional scales.
-#'   - `setup_panel_params(data)`:
+#'     positional scales; `FALSE` otherwise.
+#'   - `setup_panel_params(scale_x, scale_y, params)`: Determines the appropriate
+#'     x and y ranges for each panel, and also calculates anything else needed to
+#'     render the panel and axes, such as tick positions and labels for major
+#'     and minor ticks. Returns all this information in a named list.
 #'   - `setup_data(data, params)`: Allows the coordinate system to
 #'     manipulate the plot data. Should return list of data frames.
 #'   - `setup_layout(layout, params)`: Allows the coordinate
@@ -73,8 +81,15 @@ Coord <- ggproto("Coord",
     )
   },
 
+  # transform range given in transformed coordinates
+  # back into range in given in (possibly scale-transformed)
+  # data coordinates
   range = function(panel_params) {
-    return(list(x = panel_params$x.range, y = panel_params$y.range))
+    warning(
+      "range backtransformation not implemented in this coord; plot may be wrong.",
+      call. = FALSE
+      )
+    list(x = panel_params$x.range, y = panel_params$y.range)
   },
 
   setup_panel_params = function(scale_x, scale_y, params = list()) {

R/coord-cartesian-.r
@@ -83,6 +83,10 @@ CoordCartesian <- ggproto("CoordCartesian", Coord,
     dist_euclidean(x, y) / max_dist
   },
 
+  range = function(panel_params) {
+    list(x = panel_params$x.range, y = panel_params$y.range)
+  },
+
   transform = function(data, panel_params) {
     rescale_x <- function(data) rescale(data, from = panel_params$x.range)
     rescale_y <- function(data) rescale(data, from = panel_params$y.range)

R/coord-map.r
@@ -119,6 +119,11 @@ CoordMap <- ggproto("CoordMap", Coord,
     out
   },
 
+  range = function(panel_params) {
+    # range is stored in data coordinates and doesn't have to be back-transformed
+    list(x = panel_params$x.range, y = panel_params$y.range)
+  },
+
   distance = function(x, y, panel_params) {
     max_dist <- dist_central_angle(panel_params$x.range, panel_params$y.range)
     dist_central_angle(x, y) / max_dist

R/coord-transform.r
@@ -119,6 +119,13 @@ CoordTrans <- ggproto("CoordTrans", Coord,
     dist_euclidean(self$trans$x$transform(x), self$trans$y$transform(y)) / max_dist
   },
 
+  range = function(self, panel_params) {
+    list(
+      x = self$trans$x$inverse(panel_params$x.range),
+      y = self$trans$y$inverse(panel_params$y.range)
+    )
+  },
+
   transform = function(self, data, panel_params) {
     trans_x <- function(data) transform_value(self$trans$x, data, panel_params$x.range)
     trans_y <- function(data) transform_value(self$trans$y, data, panel_params$y.range)

R/sf.R
@@ -361,6 +361,10 @@ CoordSf <- ggproto("CoordSf", CoordCartesian,
     )
   },
 
+  range = function(panel_params) {
+    list(x = panel_params$x_range, y = panel_params$y_range)
+  },
+
   # CoordSf enforces a fixed aspect ratio -> axes cannot be changed freely under faceting
   is_free = function() FALSE,
 

man/ggplot2-ggproto.Rd
@@ -201,14 +201,22 @@ object, you typically will want to implement one or more of the following:
 \item \code{render_bg}: Renders background elements.
 \item \code{render_axis_h}: Renders the horizontal axes.
 \item \code{render_axis_v}: Renders the vertical axes.
-\item \code{range}: Returns the x and y ranges
+\item \code{range(panel_params)}: Extracts the panel range provided
+in \code{panel_params} (created by \code{setup_panel_params()}, see below) and
+back-transforms to data coordinates. This back-transformation is needed
+for coords such as \code{coord_flip()}, \code{coord_polar()}, \code{coord_trans()} where
+the range in the transformed coordinates differs from the range in the
+untransformed coordinates.
 \item \code{transform}: Transforms x and y coordinates.
 \item \code{distance}: Calculates distance.
 \item \code{is_linear}: Returns \code{TRUE} if the coordinate system is
 linear; \code{FALSE} otherwise.
 \item \code{is_free}: Returns \code{TRUE} if the coordinate system supports free
-positional scales.
-\item \code{setup_panel_params(data)}:
+positional scales; \code{FALSE} otherwise.
+\item \code{setup_panel_params(scale_x, scale_y, params)}: Determines the appropriate
+x and y ranges for each panel, and also calculates anything else needed to
+render the panel and axes, such as tick positions and labels for major
+and minor ticks. Returns all this information in a named list.
 \item \code{setup_data(data, params)}: Allows the coordinate system to
 manipulate the plot data. Should return list of data frames.
 \item \code{setup_layout(layout, params)}: Allows the coordinate

rstudio/shiny

commit 7a56396185996de7b47809dcd5dd22f7dfda51a3

Allow for different values of ggplot2 domain.  DUCT TAPE FIX for dev version of ggplot2
Originally caused by https://github.com/tidyverse/ggplot2/pull/2832

Need to wait for https://github.com/tidyverse/ggplot2/pull/2821 to be merged.

Expecting a bug fix release between 3.0.0 and when this bug will be fixed, so can not compare against pkg versions. :-/

tests/testthat/test-plot-coordmap.R
@@ -351,9 +351,19 @@ test_that("ggplot coordmap with various scales and coords", {
     sortList(m$panels[[1]]$log),
     sortList(list(x=10, y=2))
   )
+
   # Check domains
-  expect_equal(
-    sortList(m$panels[[1]]$domain),
-    sortList(list(left=-1, right=3, bottom=-2, top=4))
+  expect_true(
+    isTRUE(testthat::compare(
+      sortList(m$panels[[1]]$domain),
+      sortList(list(left=-1, right=3, bottom=-2, top=4))
+    )$equal) ||
+    isTRUE(testthat::compare(
+      sortList(m$panels[[1]]$domain),
+      # y transformations are being back transformed to the original coords
+      # Odd behavior caused by https://github.com/tidyverse/ggplot2/pull/2832
+      # Waiting on https://github.com/tidyverse/ggplot2/pull/2821
+      sortList(list(left=-1, right=3, bottom=0.25, top=16))
+    )$equal)
   )
 })
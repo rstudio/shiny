d3e2d57047ca0f1e6243738e654b6b0d5d9722e1
Winston Chang <winston@stdout.org>
2018-10-16 13:29:45 -0500

    Modify updateBootstrapDatepicker script

--- tools/updateBootstrapDatepicker.R
+++ tools/updateBootstrapDatepicker.R
@@ -1,47 +1,54 @@
 #!/usr/bin/env Rscript
 
 # This script copies resources from Bootstrap Datepicker to shiny's inst
-# directory. The bootstrap-datepicker/ project directory should be on the same
-# level as the shiny/ project directory.
+# directory.
 
-# It is necessary to run Grunt after running this script: This copies the
-# un-minified JS file over, and running Grunt minifies it and inlines the locale
-# files into the minified JS.
+# This script can be run from the command line or sourced from an R session.
+library(rprojroot)
 
-# This script can be sourced from RStudio, or run with Rscript.
+version <- "1.8.0"
 
-# Returns the file currently being sourced or run with Rscript
-thisFile <- function() {
-  cmdArgs <- commandArgs(trailingOnly = FALSE)
-  needle <- "--file="
-  match <- grep(needle, cmdArgs)
-  if (length(match) > 0) {
-    # Rscript
-    return(normalizePath(sub(needle, "", cmdArgs[match])))
-  } else {
-    # 'source'd via R console
-    return(normalizePath(sys.frames()[[1]]$ofile))
+# The git tag for the https://github.com/uxsolutions/bootstrap-datepicker repo.
+tag <- paste0("v", version)
+
+source_file <- file.path(tempdir(), paste0("libuv-", version, ".tar.gz"))
+
+url <- paste0("https://github.com/uxsolutions/bootstrap-datepicker/archive/", tag, ".tar.gz")
+download.file(url, source_file)
+
+untar(source_file, exdir = tempdir())
+source_dir <- file.path(tempdir(), paste0("bootstrap-datepicker-", version), "dist")
+
+dest_dir <- rprojroot::find_package_root_file("inst", "www", "shared", "datepicker")
+unlink(dest_dir, recursive = TRUE)
+
+dir.create(dest_dir)
+
+filenames <- c(
+  "js/bootstrap-datepicker.min.js",
+  "css/bootstrap-datepicker3.min.css",
+  file.path("locales",
+    dir(file.path(source_dir, "locales"), "\\.min\\.js$")
+  )
+)
+
+copy_files <- function(srcdir, destdir, filenames) {
+  # Create needed directories
+  dest_subdirs <- file.path(destdir, unique(dirname(filenames)))
+  for (dest_subdir in dest_subdirs) {
+    dir.create(dest_subdir, recursive = TRUE)
+  }
+
+  res <- file.copy(
+    from = paste0(srcdir,  "/", filenames),
+    to   = paste0(destdir, "/", filenames)
+  )
+
+  if (!all(res)) {
+    message("Problem copying ", sum(!res), " files: \n  ",
+      paste0(filenames[!res], collapse = "\n  ")
+    )
   }
 }
 
-srcdir <- normalizePath(file.path(dirname(thisFile()), "../../bootstrap-datepicker/dist"))
-destdir <- normalizePath(file.path(dirname(thisFile()), "../inst/www/shared/datepicker"))
-
-file.copy(
-  file.path(srcdir, "js", "bootstrap-datepicker.js"),
-  file.path(destdir, "js"),
-  overwrite = TRUE
-)
-
-file.copy(
-  dir(file.path(srcdir, "locales"), "\\.js$", full.names = TRUE),
-  file.path(destdir, "js", "locales"),
-  overwrite = TRUE
-)
-
-file.copy(
-  dir(file.path(srcdir, "css"), "^bootstrap-datepicker3(\\.min)?\\.css$",
-      full.names = TRUE),
-  file.path(destdir, "css"),
-  overwrite = TRUE
-)
+copy_files(source_dir, dest_dir, filenames)

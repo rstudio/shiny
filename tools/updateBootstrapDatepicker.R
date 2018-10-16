9b0c63c9e66fbd0654782863187443d173d72d58
Winston Chang <winston@stdout.org>
2018-10-16 13:58:31 -0500

    Update locales for dateInput

--- tools/updateBootstrapDatepicker.R
+++ tools/updateBootstrapDatepicker.R
@@ -1,10 +1,7 @@
 #!/usr/bin/env Rscript
 
 # This script copies resources from Bootstrap Datepicker to shiny's inst
-# directory.
-
-# This script can be run from the command line or sourced from an R session.
-library(rprojroot)
+# directory. It can be run from the command line or sourced from an R session.
 
 version <- "1.8.0"
 
@@ -24,12 +21,14 @@ unlink(dest_dir, recursive = TRUE)
 
 dir.create(dest_dir)
 
+locale_files <- dir(file.path(source_dir, "locales"), "\\.min\\.js$")
+# Remove errant file from the list of things to copy
+locale_files <- locale_files[locale_files != "bootstrap-datepicker-en-CA.min.js"]
+
 filenames <- c(
   "js/bootstrap-datepicker.min.js",
   "css/bootstrap-datepicker3.min.css",
-  file.path("locales",
-    dir(file.path(source_dir, "locales"), "\\.min\\.js$")
-  )
+  file.path("locales", locale_files)
 )
 
 copy_files <- function(srcdir, destdir, filenames) {
@@ -54,3 +53,8 @@ copy_files <- function(srcdir, destdir, filenames) {
 copy_files(source_dir, dest_dir, filenames)
 
 message("Make sure to update the version to ", version, " in datePickerDependency!")
+
+locales <- sub("bootstrap-datepicker\\.(.*)\\.min\\.js", '"\\1"', locale_files)
+message("Also update valid languages in documentation for dateInput: \n",
+  paste0(locales, collapse = ", ")
+)

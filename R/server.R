956c1cb1a70afa55e6ac2e6131698ffb03412655
Winston Chang <winston@stdout.org>
2018-11-13 11:45:33 -0600

    Use setStaticPath instead of setStaticPaths

--- R/server.R
+++ R/server.R
@@ -66,7 +66,7 @@ addResourcePath <- function(prefix, directoryPath) {
   # the corresponding httpuv server object.
   if (!is.null(getShinyOption("server")))
   {
-    getShinyOption("server")$setStaticPaths(setNames(normalizedPath, prefix))
+    getShinyOption("server")$setStaticPath(.list = setNames(normalizedPath, prefix))
   }
 
   # .globals$resourcePaths persists across runs of applications.

8ca3397c5dc359dcc0bfc7c6cd7c71e9f988a6d6
Alan Dipert <alan@dipert.org>
2019-03-26 17:39:41 -0700

    Improve bootstrap-datepicker update script

--- tools/updateBootstrapDatepicker.R
+++ tools/updateBootstrapDatepicker.R
@@ -1,47 +1,33 @@
 #!/usr/bin/env Rscript
+# Retrieves a particular version of bootstrap-datepicker:
+#  https://github.com/uxsolutions/bootstrap-datepicker
+# After retrieving, applies the series of patches contained
+# in tools/datepicker-patches to inst/www/shared/datepicker/
 
-# This script copies resources from Bootstrap Datepicker to shiny's inst
-# directory. The bootstrap-datepicker/ project directory should be on the same
-# level as the shiny/ project directory.
+library(rprojroot)
 
-# It is necessary to run Grunt after running this script: This copies the
-# un-minified JS file over, and running Grunt minifies it and inlines the locale
-# files into the minified JS.
+version   <- "1.6.4"
+patch_dir <- rprojroot::find_package_root_file("tools/datepicker-patches")
+dest_dir  <- rprojroot::find_package_root_file("inst/www/shared/datepicker")
 
-# This script can be sourced from RStudio, or run with Rscript.
-
-# Returns the file currently being sourced or run with Rscript
-thisFile <- function() {
-  cmdArgs <- commandArgs(trailingOnly = FALSE)
-  needle <- "--file="
-  match <- grep(needle, cmdArgs)
-  if (length(match) > 0) {
-    # Rscript
-    return(normalizePath(sub(needle, "", cmdArgs[match])))
-  } else {
-    # 'source'd via R console
-    return(normalizePath(sys.frames()[[1]]$ofile))
-  }
+fetch_datepicker <- function() {
+  tag       <- paste0("v", version)
+  dest_file <- file.path(tempdir(), paste0("bootstrap-datepicker-", version, ".zip"))
+  url       <- sprintf("https://github.com/uxsolutions/bootstrap-datepicker/releases/download/%s/bootstrap-datepicker-%s-dist.zip", tag, version)
+  download.file(url, dest_file)
+  unzip(
+    dest_file,
+    files = c(
+      "js/bootstrap-datepicker.js",
+      "css/bootstrap-datepicker3.css",
+      "css/bootstrap-datepicker3.min.css"
+    ),
+    exdir = dest_dir
+  )
 }
 
-srcdir <- normalizePath(file.path(dirname(thisFile()), "../../bootstrap-datepicker/dist"))
-destdir <- normalizePath(file.path(dirname(thisFile()), "../inst/www/shared/datepicker"))
+apply_patches <- function() {
+}
 
-file.copy(
-  file.path(srcdir, "js", "bootstrap-datepicker.js"),
-  file.path(destdir, "js"),
-  overwrite = TRUE
-)
-
-file.copy(
-  dir(file.path(srcdir, "locales"), "\\.js$", full.names = TRUE),
-  file.path(destdir, "js", "locales"),
-  overwrite = TRUE
-)
-
-file.copy(
-  dir(file.path(srcdir, "css"), "^bootstrap-datepicker3(\\.min)?\\.css$",
-      full.names = TRUE),
-  file.path(destdir, "css"),
-  overwrite = TRUE
-)
+fetch_datepicker()
+apply_patches()

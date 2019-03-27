20329feb7fadd433138ed30c85f3da87329a9ebc
Alan Dipert <alan@dipert.org>
2019-03-26 17:52:47 -0700

    Improve bootstrap-datepicker update tools, add docs

--- tools/updateBootstrapDatepicker.R
+++ tools/updateBootstrapDatepicker.R
@@ -1,33 +1,24 @@
 #!/usr/bin/env Rscript
 # Retrieves a particular version of bootstrap-datepicker:
 #  https://github.com/uxsolutions/bootstrap-datepicker
-# After retrieving, applies the series of patches contained
-# in tools/datepicker-patches to inst/www/shared/datepicker/
+# After retrieving, you can apply patches stored in
+# tools/datepicker-patches with applyDatepickerPatches.R
 
 library(rprojroot)
 
 version   <- "1.6.4"
-patch_dir <- rprojroot::find_package_root_file("tools/datepicker-patches")
 dest_dir  <- rprojroot::find_package_root_file("inst/www/shared/datepicker")
+tag       <- paste0("v", version)
+dest_file <- file.path(tempdir(), paste0("bootstrap-datepicker-", version, ".zip"))
+url       <- sprintf("https://github.com/uxsolutions/bootstrap-datepicker/releases/download/%s/bootstrap-datepicker-%s-dist.zip", tag, version)
 
-fetch_datepicker <- function() {
-  tag       <- paste0("v", version)
-  dest_file <- file.path(tempdir(), paste0("bootstrap-datepicker-", version, ".zip"))
-  url       <- sprintf("https://github.com/uxsolutions/bootstrap-datepicker/releases/download/%s/bootstrap-datepicker-%s-dist.zip", tag, version)
-  download.file(url, dest_file)
-  unzip(
-    dest_file,
-    files = c(
-      "js/bootstrap-datepicker.js",
-      "css/bootstrap-datepicker3.css",
-      "css/bootstrap-datepicker3.min.css"
-    ),
-    exdir = dest_dir
-  )
-}
-
-apply_patches <- function() {
-}
-
-fetch_datepicker()
-apply_patches()
+download.file(url, dest_file)
+unzip(
+  dest_file,
+  files = c(
+    "js/bootstrap-datepicker.js",
+    "css/bootstrap-datepicker3.css",
+    "css/bootstrap-datepicker3.min.css"
+  ),
+  exdir = dest_dir
+)

57f8f5133856e0b10a215b174a5e344d32dfac07
Joe Cheng <joe@rstudio.com>
2019-05-24 16:30:14 -0400

    Clear cookies on logout, introduce oauth_config func/obj

--- examples/oauth/app.R
+++ examples/oauth/app.R
@@ -8,8 +8,32 @@ options(shiny.port=8100)
 
 source("oauth.R")
 
+github_oauth_config <- oauth_config(
+  oauth_endpoint_uri = "https://github.com/login/oauth/authorize",
+  token_endpoint_uri = "https://github.com/login/oauth/access_token",
+  app_uri = "http://127.0.0.1:8100/",
+
+  # Store client_id and client_secret however you want--just hardcoded for this example
+  client_id = "700d40c400de637d9780",
+  client_secret = "e6383430779d9df9b253e7d6b1fb53308033873d",
+
+  scope = ""
+)
+
+google_oauth_config <- oauth_config(
+  oauth_endpoint_uri = "https://accounts.google.com/o/oauth2/v2/auth",
+  token_endpoint_uri = "https://www.googleapis.com/oauth2/v4/token",
+  app_uri = "http://127.0.0.1:8100/",
+
+  # Store client_id and client_secret however you want--just hardcoded for this example
+  client_id = "350280321053-7bq89pep4da46df2g66ddjnj6e3qrnie.apps.googleusercontent.com",
+  client_secret = "8_AHVNXyKyO3tBAZFAy-2y0B",
+
+  scope = "https://www.googleapis.com/auth/drive.metadata.readonly"
+)
+
+
 ui <- fluidPage(
-  "Logged in as",
   textOutput("username", inline = TRUE),
   p(
     oauth_login_ui("oauth_login")
@@ -20,59 +44,38 @@ server <- function(input, output, session) {
 
   ### GITHUB
 
-  # token <- callModule(oauth_login, id = "oauth_login",
-  #   oauth_endpoint_uri = "https://github.com/login/oauth/authorize",
-  #   token_endpoint_uri = "https://github.com/login/oauth/access_token",
-  #   app_uri = "http://127.0.0.1:8100/",
-  #
-  #   # Store client_id and client_secret however you want--just hardcoded for this example
-  #   client_id = "700d40c400de637d9780",
-  #   client_secret = "e6383430779d9df9b253e7d6b1fb53308033873d",
-  #
-  #   scope = ""
-  # )
+  token <- callModule(oauth_login, id = "oauth_login", github_oauth_config)
+
+  output$username <- renderText({
+    if (is.null(token())) {
+      "Not logged in"
+    } else {
+      resp <- httr::GET("https://api.github.com/user",
+        httr::add_headers("Authorization" = paste("token", token()))
+      )
+
+      paste0("Logged in as ", httr::content(resp)$login)
+    }
+  })
+
+
+
+  ## GOOGLE
+
+  # token <- callModule(oauth_login, id = "oauth_login", google_oauth_config)
   #
   # output$username <- renderText({
   #   if (is.null(token())) {
   #     # Not logged in
   #     "(nobody)"
   #   } else {
-  #     resp <- httr::GET("https://api.github.com/user",
-  #       httr::add_headers("Authorization" = paste("token", token()))
-  #     )
-  #
-  #     httr::content(resp)$login
+  #     req <- gargle::request_build(method = "GET", path = "oauth2/v3/tokeninfo",
+  #       params = list(access_token=token()),
+  #       base_url = "https://www.googleapis.com")
+  #     resp <- gargle::request_make(req)
+  #     gargle::response_process(resp)$email
   #   }
   # })
-
-
-
-  ## GOOGLE
-
-  token <- callModule(oauth_login, id = "oauth_login",
-    oauth_endpoint_uri = "https://accounts.google.com/o/oauth2/v2/auth",
-    token_endpoint_uri = "https://www.googleapis.com/oauth2/v4/token",
-    app_uri = "http://127.0.0.1:8100/",
-
-    # Store client_id and client_secret however you want--just hardcoded for this example
-    client_id = "350280321053-7bq89pep4da46df2g66ddjnj6e3qrnie.apps.googleusercontent.com",
-    client_secret = "8_AHVNXyKyO3tBAZFAy-2y0B",
-
-    scope = "https://www.googleapis.com/auth/drive.metadata.readonly"
-  )
-
-  output$username <- renderText({
-    if (is.null(token())) {
-      # Not logged in
-      "(nobody)"
-    } else {
-      req <- gargle::request_build(method = "GET", path = "oauth2/v3/tokeninfo",
-        params = list(access_token=token()),
-        base_url = "https://www.googleapis.com")
-      resp <- gargle::request_make(req)
-      gargle::response_process(resp)$email
-    }
-  })
 }
 
 shinyApp(ui, server, options = list(port = 8100))
--- examples/oauth/oauth.R
+++ examples/oauth/oauth.R
@@ -3,24 +3,42 @@
 # Include in your Shiny UI wherever you want OAuth login UI to appear
 oauth_login_ui <- function(id) {
   ns <- NS(id)
-  uiOutput(ns("container"))
+  tagList(
+    uiOutput(ns("container")),
+    htmltools::singleton(tags$head(clear_cookie_custom_handler))
+  )
 }
 
 # A simple Bootstrap OAuth login button
-oauth_button <- function(login_url) {
+oauth_login_button <- function(login_url) {
   #tags$a(href=login_url, target="_blank", class="btn btn-default", "Login")
   tags$a(href=sprintf("javascript:window.open('%s');", login_url), class = "btn btn-default", "Login")
 }
 
+oauth_logout_button <- function(input_id) {
+  actionLink(input_id, "Logout")
+}
+
+oauth_config <- function(oauth_endpoint_uri, token_endpoint_uri, app_uri,
+  client_id, client_secret, scope, login_ui = oauth_login_button,
+  logout_ui = oauth_logout_button) {
+
+  list(
+    oauth_endpoint_uri = oauth_endpoint_uri,
+    token_endpoint_uri = token_endpoint_uri,
+    app_uri = app_uri,
+    client_id = client_id,
+    client_secret = client_secret,
+    scope = scope,
+    login_ui = login_ui,
+    logout_ui = logout_ui
+  )
+}
 
 # Server module for initializing oauth
-oauth_login <- function(input, output, session,
-  oauth_endpoint_uri, token_endpoint_uri, app_uri,
-  client_id, client_secret, scope,
-  login_ui = oauth_button) {
+oauth_login <- function(input, output, session, oauth_config) {
 
-  # Force argument evaluation
-  list(oauth_endpoint_uri, token_endpoint_uri, app_uri, client_id, client_secret, scope, login_ui)
+  force(oauth_config)
 
   token <- reactiveVal(NULL)
 
@@ -33,16 +51,34 @@ oauth_login <- function(input, output, session,
     }
   }
 
-  redirect_uri <- sub("/?$", "/oauth_callback", app_uri)
+  redirect_uri <- sub("/?$", "/oauth_callback", oauth_config$app_uri)
 
-  state <- store_oauth_request_state(token, redirect_uri, token_endpoint_uri, client_id, client_secret, session)
+  state <- store_oauth_request_state(token,
+    redirect_uri,
+    oauth_config$token_endpoint_uri,
+    oauth_config$client_id,
+    oauth_config$client_secret,
+    session)
 
   output$container <- renderUI({
     if (is.null(token())) {
       # login button
-      url <- make_authorization_url(oauth_endpoint_uri, redirect_uri, client_id, client_secret, scope, state, session)
-      login_ui(url)
+      url <- make_authorization_url(oauth_config, redirect_uri, state, session)
+
+      oauth_config$login_ui(url)
+    } else {
+      oauth_config$logout_ui(session$ns("btn_logout"))
     }
+  })
+
+  observeEvent(input$btn_logout, {
+    xsrf_token <- shiny:::createUniqueId(16)
+    clear_cookie_xsrf$set(xsrf_token, TRUE)
+
+    session$sendCustomMessage("oauth-clear-cookie-handler", list(
+      xsrf_token = xsrf_token
+    ))
+    token(NULL)
   })
 
   return(token)
@@ -69,7 +105,7 @@ store_oauth_request_state <- function(rv, redirect_uri, token_endpoint_uri, clie
 }
 
 
-make_authorization_url <- function(oauth_endpoint_uri, redirect_uri, client_id, client_secret, scope, state, session = getDefaultReactiveDomain()) {
+make_authorization_url <- function(oauth_config, redirect_uri, state, session = getDefaultReactiveDomain()) {
   # TODO: Implement for real
   #
   # The req object is a Rook request. This is just an environment object that
@@ -78,14 +114,12 @@ make_authorization_url <- function(oauth_endpoint_uri, redirect_uri, client_id, 
   # https://github.com/jeffreyhorner/Rook#the-environment
   url_template <- "%s?client_id=%s&redirect_uri=%s&response_type=code&state=%s&access_type=offline&include_granted_scopes=true&scope=%s"
   auth_url <- sprintf(url_template,
-    oauth_endpoint_uri,
-    utils::URLencode(client_id, reserved = TRUE, repeated = TRUE),
+    oauth_config$oauth_endpoint_uri,
+    utils::URLencode(oauth_config$client_id, reserved = TRUE, repeated = TRUE),
     utils::URLencode(redirect_uri, reserved = TRUE, repeated = TRUE),
     utils::URLencode(state, reserved = TRUE, repeated = TRUE),
-    utils::URLencode(scope, reserved = TRUE, repeated = TRUE)
+    utils::URLencode(oauth_config$scope, reserved = TRUE, repeated = TRUE)
   )
-
-  addRouteHandler("/oauth_callback", oauth_callback_handler)
 
   auth_url
 }
@@ -106,8 +140,12 @@ oauth_callback_handler <- function(req) {
   state <- qs_info$state
   if (!is.null(err)) {
     # TODO: Report error to user
-    message(err)
-    return(NULL)
+    message(jsonlite::toJSON(qs_info, pretty = TRUE, auto_unbox = TRUE))
+    return(list(
+      status = 500L,
+      headers = list("Content-Type" = "text/plain"),
+      body = "Authorization failure"
+    ))
   } else if (!is.null(code) && !is.null(state)) {
     req_info <- oauth_request_state$get(state)
     if (is.null(req_info)) {
@@ -131,7 +169,6 @@ oauth_callback_handler <- function(req) {
       )
     )
     respObj <- httr::content(resp, as = "parsed")
-    # respObj <- jsonlite::fromJSON(rawToChar(resp$content))
 
     rv(respObj$access_token)
 
@@ -146,9 +183,61 @@ oauth_callback_handler <- function(req) {
         # TODO: path/samesite
         "Set-Cookie" = sprintf("shinyoauthaccesstoken=%s; HttpOnly; Path=/", respObj$access_token)
       ),
-      body = "#<html><head><script>window.close();</script></head><body>You can close this window now</body></html>"
+      body = as.character(
+        tags$html(
+          HTML("<head><script>window.close();</script></head>"),
+          tags$body(
+            "You can close this window now"
+          )
+        )
+      )
     ))
   } else {
     # TODO: Report malformed request
   }
 }
+
+addRouteHandler("/oauth_callback", oauth_callback_handler)
+
+
+clear_cookie_xsrf <- fastmap::fastmap()
+
+oauth_clear_cookie_handler <- function(req) {
+  if (req$REQUEST_METHOD != "POST") {
+    return(NULL)
+  }
+
+  xsrf_token <- req$rook.input$read_lines(1)
+  if (is.null(clear_cookie_xsrf$get(xsrf_token))) {
+    return(list(
+      status = 403L,
+      headers = list(
+        "Content-Type" = "text/plain"
+      ),
+      body = "Unrecognized XSRF token"
+    ))
+  }
+  clear_cookie_xsrf$remove(xsrf_token)
+
+  return(list(
+    status = 200L,
+    headers = list(
+      "Content-Type" = "text/plain",
+      "Set-Cookie" = "shinyoauthaccesstoken=; HttpOnly; Path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    ),
+    body = ""
+  ))
+}
+
+addRouteHandler("/oauth_clear_cookie", oauth_clear_cookie_handler)
+
+clear_cookie_custom_handler <- tags$script(
+  "
+    Shiny.addCustomMessageHandler('oauth-clear-cookie-handler', function(msg) {
+      var req = new XMLHttpRequest();
+      req.open('POST', 'oauth_clear_cookie');
+      req.setRequestHeader('Content-Type', 'text/plain');
+      req.send(msg.xsrf_token);
+    });
+  "
+)

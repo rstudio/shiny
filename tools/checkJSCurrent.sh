3bd4825d4bcc9b23868f9d2951293fde7eb5d2d3
trestletech <jeff.allen@trestletechnology.net>
2019-06-19 11:34:59 -0500

    Write errors to stderr.

--- tools/checkJSCurrent.sh
+++ tools/checkJSCurrent.sh
@@ -8,8 +8,8 @@ set -e
 if [ -n "$(git status --porcelain)" ]
 then
   git status --porcelain
-  echo "Please rebuild the JavaScript and commit the changes."
-  echo "The above files changed when we built the JavaScript assets. This most often occurs when a user makes changes to the JavaScript sources but doesn't rebuild and commit them."
+  >&2 echo "Please rebuild the JavaScript and commit the changes."
+  >&2 echo "The above files changed when we built the JavaScript assets. This most often occurs when a user makes changes to the JavaScript sources but doesn't rebuild and commit them."
   exit 1
 else
   echo "No difference detected; JavaScript build is current."

5fdca29448318ef6eb06764674cc7634d7197432
Jeff Allen <trestletech@users.noreply.github.com>
2019-06-25 15:56:57 +0000

    Clarify interaction between width/height and CSS/templates (#2504)
    * Clarify interaction between width/height and CSS/templates
    
    * Reword given that height is less likely to be specified in CSS.

--- R/input-textarea.R
+++ R/input-textarea.R
@@ -3,18 +3,19 @@
 #' Create a textarea input control for entry of unstructured text values.
 #'
 #' @inheritParams textInput
-#' @param height The height of the input, e.g. `'400px'`, or
-#'   `'100%'`; see [validateCssUnit()].
-#' @param cols Value of the visible character columns of the input, e.g.
-#'   `80`. If used with `width`, `width` will take precedence in
-#'   the browser's rendering.
-#' @param rows The value of the visible character rows of the input, e.g.
-#'   `6`. If used with `height`, `height` will take precedence in
-#'   the browser's rendering.
+#' @param height The height of the input, e.g. `'400px'`, or `'100%'`; see
+#'   [validateCssUnit()].
+#' @param cols Value of the visible character columns of the input, e.g. `80`.
+#'   This argument will only take effect if there is not a CSS `width` rule
+#'   defined for this element; such a rule could come from the `width` argument
+#'   of this function or from a containing page layout such as
+#'   [fluidPage()].
+#' @param rows The value of the visible character rows of the input, e.g. `6`.
+#'   If the `height` argument is specified, `height` will take precedence in the
+#'   browser's rendering.
 #' @param resize Which directions the textarea box can be resized. Can be one of
-#'   `"both"`, `"none"`, `"vertical"`, and `"horizontal"`.
-#'   The default, `NULL`, will use the client browser's default setting for
-#'   resizing textareas.
+#'   `"both"`, `"none"`, `"vertical"`, and `"horizontal"`. The default, `NULL`,
+#'   will use the client browser's default setting for resizing textareas.
 #' @return A textarea input control that can be added to a UI definition.
 #'
 #' @family input elements

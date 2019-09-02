1ec7f22b5f556975d4f8beae5c68cf93409fc30f
Hadley Wickham <h.wickham@gmail.com>
2019-09-02 09:03:46 -0500

    Alphabetise options

--- R/shiny-options.R
+++ R/shiny-options.R
@@ -34,16 +34,6 @@ getShinyOption <- function(name, default = NULL) {
 #' be set with (for example) `options(shiny.trace=TRUE)`.
 #'
 #' \describe{
-#'   \item{shiny.launch.browser}{A boolean which controls the default behavior
-#'     when an app is run. See [runApp()] for more information.}
-#'   \item{shiny.port}{A port number that Shiny will listen on. See
-#'     [runApp()] for more information.}
-#'   \item{shiny.trace}{Print messages sent between the R server and the web
-#'     browser client to the R console. This is useful for debugging. Possible
-#'     values are `"send"` (only print messages sent to the client),
-#'     `"recv"` (only print messages received by the server), `TRUE`
-#'     (print all messages), or `FALSE` (default; don't print any of these
-#'     messages).}
 #'   \item{shiny.autoreload}{If `TRUE` when a Shiny app is launched, the
 #'     app directory will be continually monitored for changes to files that
 #'     have the extensions: r, htm, html, js, css, png, jpg, jpeg, gif. If any
@@ -60,44 +50,34 @@ getShinyOption <- function(name, default = NULL) {
 #'     The default polling interval is 500 milliseconds. You can change this
 #'     by setting e.g. `options(shiny.autoreload.interval = 2000)` (every
 #'     two seconds).}
-#'   \item{shiny.reactlog}{If `TRUE`, enable logging of reactive events,
-#'     which can be viewed later with the [reactlogShow()] function.
-#'     This incurs a substantial performance penalty and should not be used in
-#'     production.}
-#'   \item{shiny.usecairo}{This is used to disable graphical rendering by the
-#'     Cairo package, if it is installed. See [plotPNG()] for more
-#'     information.}
-#'   \item{shiny.maxRequestSize}{This is a number which specifies the maximum
-#'     web request size, which serves as a size limit for file uploads. If
-#'     unset, the maximum request size defaults to 5MB.}
-#'   \item{shiny.suppressMissingContextError}{Normally, invoking a reactive
-#'     outside of a reactive context (or [isolate()]) results in
-#'     an error. If this is `TRUE`, don't error in these cases. This
-#'     should only be used for debugging or demonstrations of reactivity at the
-#'     console.}
+#'   \item{shiny.deprecation.messages}{This controls whether messages for
+#'     deprecated functions in Shiny will be printed. See
+#'     [shinyDeprecated()] for more information.}
+#'   \item{shiny.error}{This can be a function which is called when an error
+#'     occurs. For example, `options(shiny.error=recover)` will result a
+#'     the debugger prompt when an error occurs.}
+#'   \item{shiny.fullstacktrace}{Controls whether "pretty" or full stack traces
+#'     are dumped to the console when errors occur during Shiny app execution.
+#'     The default is `FALSE` (pretty stack traces).}
 #'   \item{shiny.host}{The IP address that Shiny should listen on. See
 #'     [runApp()] for more information.}
 #'   \item{shiny.json.digits}{The number of digits to use when converting
 #'     numbers to JSON format to send to the client web browser.}
+#'   \item{shiny.launch.browser}{A boolean which controls the default behavior
+#'     when an app is run. See [runApp()] for more information.}
+#'   \item{shiny.maxRequestSize}{This is a number which specifies the maximum
+#'     web request size, which serves as a size limit for file uploads. If
+#'     unset, the maximum request size defaults to 5MB.}
 #'   \item{shiny.minified}{If this is `TRUE` or unset (the default), then
 #'     Shiny will use minified JavaScript (`shiny.min.js`). If
 #'     `FALSE`, then Shiny will use the un-minified JavaScript
 #'     (`shiny.js`); this can be useful during development.}
-#'   \item{shiny.error}{This can be a function which is called when an error
-#'     occurs. For example, `options(shiny.error=recover)` will result a
-#'     the debugger prompt when an error occurs.}
-#'   \item{shiny.table.class}{CSS class names to use for tables.}
-#'   \item{shiny.deprecation.messages}{This controls whether messages for
-#'     deprecated functions in Shiny will be printed. See
-#'     [shinyDeprecated()] for more information.}
-#'   \item{shiny.fullstacktrace}{Controls whether "pretty" or full stack traces
-#'     are dumped to the console when errors occur during Shiny app execution.
-#'     The default is `FALSE` (pretty stack traces).}
-#'   \item{shiny.stacktraceoffset}{If `TRUE`, then Shiny's printed stack
-#'     traces will display srcrefs one line above their usual location. This is
-#'     an arguably more intuitive arrangement for casual R users, as the name
-#'     of a function appears next to the srcref where it is defined, rather than
-#'     where it is currently being called from.}
+#'   \item{shiny.port}{A port number that Shiny will listen on. See
+#'     [runApp()] for more information.}
+#'   \item{shiny.reactlog}{If `TRUE`, enable logging of reactive events,
+#'     which can be viewed later with the [reactlogShow()] function.
+#'     This incurs a substantial performance penalty and should not be used in
+#'     production.}
 #'   \item{shiny.sanitize.errors}{If `TRUE`, then normal errors (i.e.
 #'     errors not wrapped in `safeError`) won't show up in the app; a simple
 #'     generic error message is printed instead (the error and strack trace printed
@@ -106,9 +86,28 @@ getShinyOption <- function(name, default = NULL) {
 #'     particular error `e` to get displayed to the user, then set this option
 #'     to `TRUE` and use `stop(safeError(e))` for errors you want the
 #'     user to see.}
+#'   \item{shiny.stacktraceoffset}{If `TRUE`, then Shiny's printed stack
+#'     traces will display srcrefs one line above their usual location. This is
+#'     an arguably more intuitive arrangement for casual R users, as the name
+#'     of a function appears next to the srcref where it is defined, rather than
+#'     where it is currently being called from.}
+#'   \item{shiny.suppressMissingContextError}{Normally, invoking a reactive
+#'     outside of a reactive context (or [isolate()]) results in
+#'     an error. If this is `TRUE`, don't error in these cases. This
+#'     should only be used for debugging or demonstrations of reactivity at the
+#'     console.}
+#'   \item{shiny.table.class}{CSS class names to use for tables.}
 #'   \item{shiny.testmode}{If `TRUE`, then enable features for testing Shiny
-#'     applications. If `FALSE` (the default), do not enable those features.
-#'   }
+#'     applications. If `FALSE` (the default), do not enable those features.}
+#'   \item{shiny.trace}{Print messages sent between the R server and the web
+#'     browser client to the R console. This is useful for debugging. Possible
+#'     values are `"send"` (only print messages sent to the client),
+#'     `"recv"` (only print messages received by the server), `TRUE`
+#'     (print all messages), or `FALSE` (default; don't print any of these
+#'     messages).}
+#'   \item{shiny.usecairo}{This is used to disable graphical rendering by the
+#'     Cairo package, if it is installed. See [plotPNG()] for more
+#'     information.}
 #' }
 #' @param ... Options to set, with the form `name = value`.
 #' @aliases shiny-options

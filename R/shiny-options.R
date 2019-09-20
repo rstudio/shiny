2c493759281cf5184e78c871d73a57222b338fb2
Carson <cpsievert1@gmail.com>
2019-09-20 13:23:50 -0500

    Provide an option, shiny.jquery.version, to opt-in into jQuery 1.12.1

--- R/shiny-options.R
+++ R/shiny-options.R
@@ -63,6 +63,9 @@ getShinyOption <- function(name, default = NULL) {
 #'     be done 100\% correctly.}
 #'   \item{shiny.host (defaults to `"127.0.0.1"`)}{The IP address that Shiny should listen on. See
 #'     [runApp()] for more information.}
+#'   \item{shiny.jquery.version (defaults to `3`)}{The major version of jQuery to use.
+#'   Currently only values of `3` or `1` are supported. If `1`, then jQuery 1.12.1 is used. If `3`,
+#'   then jQuery 3.4.1 is used.}
 #'   \item{shiny.json.digits (defaults to `16`)}{The number of digits to use when converting
 #'     numbers to JSON format to send to the client web browser.}
 #'   \item{shiny.launch.browser (defaults to `interactive()`)}{A boolean which controls the default behavior

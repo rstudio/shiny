43118a11b7a258967b7c13cd5dff5a3c1014aca1
trestletech <jeff.allen@trestletechnology.net>
2019-10-03 14:51:45 -0500

    Working on vignette

--- R/test-module.R
+++ R/test-module.R
@@ -5,8 +5,10 @@
 #'   `output`, and `session`.
 #' @param initialState A list describing the initial values for `input`. If no
 #'   initial state is given, `input` will initialize as an empty list.
+#' @export
 testModule <- function(module, expr, args, initialState=NULL) {
-  # Insert `session$env <- environment()` at the top of the function
+  # Capture the environment from the module
+  # Inserts `session$env <- environment()` at the top of the function
   fn_body <- body(module)
   fn_body[seq(3, length(fn_body)+1)] <- fn_body[seq(2, length(fn_body))]
   fn_body[[2]] <- quote(session$env <- environment())
@@ -15,30 +17,24 @@ testModule <- function(module, expr, args, initialState=NULL) {
   # Substitute expr for later evaluation
   expr <- substitute(expr)
 
+  # Cast the initial state to reactive values
   if (!is.null(initialState)){
     inp <- do.call(reactiveValues, initialState)
   } else {
     inp <- reactiveValues()
   }
+
   out <- list()
+
+  # Create the mock session
   session <- new.env(parent=emptyenv())
   session$input <- inp
   session$output <- out
+
+  # Initialize the module
   module(session$input, session$output, session)
 
-  # for (i in 1:length(expr)){
-  #   e <- expr[[i]]
-  #   print(e)
-  #   isolate({
-  #     eval(e, session$env)
-  #   })
-  #
-  #   flushReact()
-  #   later::run_now()
-  #   timerCallbacks$executeElapsed()
-  #   #session$onFlushed()
-  # }
-
+  # Run the test expression in a reactive context and in the module's environment.
   isolate({
     eval(expr, session$env)
   })
@@ -49,6 +45,7 @@ testModule <- function(module, expr, args, initialState=NULL) {
 #' @param dir The directory root of the Shiny application. If `NULL`, this function
 #'   will work up the directory hierarchy --- starting with the current directory ---
 #'   looking for a directory that contains an `app.R` or `server.R` file.
+#' @export
 testServer <- function(expr, dir=NULL) {
   stop("NYI")
 }

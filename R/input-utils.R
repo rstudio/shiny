6a11c8fcb11c7a3394167b219af046b1f8866731
Joe Cheng <joe@rstudio.com>
2020-01-06 17:04:26 -0800

    Remove .col-form-label
    I must've copied this from a bs4 example without understanding
    what it's for; it's intended for horizontal form labels, which
    we don't do

--- R/input-utils.R
+++ R/input-utils.R
@@ -1,7 +1,7 @@
 shinyInputLabel <- function(inputId, label = NULL) {
   tags$label(
     label,
-    class = "control-label col-form-label",
+    class = "control-label",
     class = if (is.null(label)) "shiny-label-null",
     `for` = inputId
   )

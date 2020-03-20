f235abcf9c63d840c5bc93291bbb466ec7c3f4ee
Barret Schloerke <schloerke@gmail.com>
2020-03-20 10:42:56 -0400

    add local test for pkgdown

---
+++ tests/testthat/test-pkgdown.R
@@ -0,0 +1,16 @@
+
+context("pkgdown")
+
+
+test_that("pkgdown works", {
+
+  # only test pkgdown from within `devtools::test()`
+  ## `./tools` will not exist when shiny is installed
+  pkgdown_file <- "../../tools/documentation/checkPkgdown.R"
+  if (file.exists(pkgdown_file)) {
+    source(pkgdown_file)
+  }
+
+  expect_true(TRUE)
+
+})

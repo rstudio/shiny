6dd499c6f4a64d02253432fe901e8d4b14aaa7c2
Barret Schloerke <schloerke@gmail.com>
2020-04-15 15:44:20 -0400

    Add contexts and pass all tests

--- tests/test-modules/107-scatterplot/tests/testthat/test-server.R
+++ tests/test-modules/107-scatterplot/tests/testthat/test-server.R
@@ -1,5 +1,4 @@
-# Use testthat just for expectations
-library(testthat)
+context("app")
 
 testServer("../..", expr = {
   # TODO-barret remove path above
@@ -8,11 +7,9 @@ testServer("../..", expr = {
   expect_equal(nrow(dplyr::filter(df(), selected_)), 0)
   expect_equal(output$summary, "0 observation(s) selected")
 
-  plotVal <- output$`scatters-plot1`
-
   # Select a region
   session$setInputs(
-    brush =
+    `scatters-brush` =
       list(xmin = 0.84909732337501, xmax = 1.289072630224, ymin = 23.228930276968,
         ymax = 29.434482709514, coords_css = list(xmin = 105.5999755859,
             xmax = 176.5999755859, ymin = 172.2000007629, ymax = 236.2000007629),
@@ -29,7 +26,6 @@ testServer("../..", expr = {
   )
 
   # Check the value of the reactiveVal `count()`
-  expect_equal(nrow(dplyr::filter(df(), selected_)), 22)
-  expect_equal(output$summary, "22 observation(s) selected")
-  plotVal22 <- output$`scatters-plot1`
+  expect_equal(nrow(dplyr::filter(df(), selected_)), 23)
+  expect_equal(output$summary, "23 observation(s) selected")
 })

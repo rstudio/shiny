1c8a83d77c6dc35b76ca1cd1a06876ebfa360fba
Barret Schloerke <schloerke@gmail.com>
2020-04-16 15:59:06 -0400

    If no module is used with shinytest, do not test the module

--- inst/app_template/tests/shinytest/mytest.R
+++ inst/app_template/tests/shinytest/mytest.R
@@ -2,6 +2,11 @@ app <- ShinyDriver$new("../../")
 app$snapshotInit("mytest")
 
 app$snapshot()
+{{
+if (isTRUE(module)) {
+'
 app$setInputs(`mymodule1-button` = "click")
 app$setInputs(`mymodule1-button` = "click")
-app$snapshot()
+app$snapshot()'
+}
+}}

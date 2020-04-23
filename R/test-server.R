acdbe8ef5e34cdae8c6040a2fc9176c99c57f3fa
Barret Schloerke <schloerke@gmail.com>
2020-04-23 17:47:52 -0400

    use list instead of rlang::list2

--- R/test-server.R
+++ R/test-server.R
@@ -50,7 +50,7 @@ isModuleServer <- function(x) {
 #'   # Any additional arguments, below, are passed along to the module.
 #' })
 #' @export
-testServer <- function(app = NULL, expr, args = rlang::list2()) {
+testServer <- function(app = NULL, expr, args = list()) {
 
   require(shiny)
 

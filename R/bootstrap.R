d1b1b0609b7a2b27bdc6faae00076e2e9a16cd76
JooYoung Seo <sjysky@gmail.com>
2020-07-16 15:18:40 -0400

    Added license with some cosmetic changes

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -68,7 +68,7 @@ bootstrapLib <- function(theme = NULL) {
     script = c(
       "js/bootstrap.min.js",
       # Safely adding accessibility plugin for screen readers and keyboard users; no break for sighted aspects (see https://github.com/paypal/bootstrap-accessibility-plugin)
-      "js/bootstrap-accessibility.min.js",
+      "accessibility/js/bootstrap-accessibility.min.js",
       # These shims are necessary for IE 8 compatibility
       "shim/html5shiv.min.js",
       "shim/respond.min.js"
@@ -76,7 +76,7 @@ bootstrapLib <- function(theme = NULL) {
     stylesheet = if (is.null(theme)) c(
       "css/bootstrap.min.css",
       # Safely adding accessibility plugin for screen readers and keyboard users; no break for sighted aspects (see https://github.com/paypal/bootstrap-accessibility-plugin)
-      "css/bootstrap-accessibility.css"
+      "accessibility/css/bootstrap-accessibility.css"
     ),
     meta = list(viewport = "width=device-width, initial-scale=1")
   )
--- tools/updateBootstrapAccessibilityPlugin.R
+++ tools/updateBootstrapAccessibilityPlugin.R
@@ -7,12 +7,11 @@
 library(rprojroot)
 
 sha   <- "fbbf870eafc1ee5d4547fabbeea71778ddbe2166"
-dest_dir  <- rprojroot::find_package_root_file("inst/www/shared/bootstrap")
+dest_dir  <- rprojroot::find_package_root_file("inst/www/shared/bootstrap/accessibility")
 
-js_url       <- sprintf("https://raw.githubusercontent.com/paypal/bootstrap-accessibility-plugin/%s/plugins/js/bootstrap-accessibility.js", sha)
 min_js_url       <- sprintf("https://raw.githubusercontent.com/paypal/bootstrap-accessibility-plugin/%s/plugins/js/bootstrap-accessibility.min.js", sha)
 css_url       <- sprintf("https://raw.githubusercontent.com/paypal/bootstrap-accessibility-plugin/%s/plugins/css/bootstrap-accessibility.css", sha)
-url <- c(js_url, min_js_url, css_url)
+url <- c(min_js_url, css_url)
 
 # Downloading each required file:
 lapply(url, function(x) {download.file(x, file.path(tempdir(), basename(x)))})
@@ -20,7 +19,7 @@ lapply(url, function(x) {download.file(x, file.path(tempdir(), basename(x)))})
 # Copying js files:
 dir.create(file.path(dest_dir, "js"), recursive = TRUE)
 file.copy(
-  file.path(tempdir(), c("bootstrap-accessibility.js", "bootstrap-accessibility.min.js")),
+  file.path(tempdir(), "bootstrap-accessibility.min.js"),
   file.path(dest_dir, "js"),
   overwrite = TRUE
 )

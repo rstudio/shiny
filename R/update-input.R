50a6ffefc5cbe676ac7e2b6bb6926206f4b28511
colin <colin@thinkr.fr>
2020-08-27 23:46:09 +0200

    Created assert_slider_value(), a wrapper that checks if value is in the min max range for sliderInput & updateSliderInput

--- R/update-input.R
+++ R/update-input.R
@@ -444,14 +444,7 @@ updateSliderInput <- function(session, inputId, label = NULL, value = NULL,
     if (!is.null(value)) value <- to_ms(value)
   }
 
-  if (isTRUE(min(value) < min | max(value) > max)) {
-    warning(noBreaks. = TRUE,
-            sprintf(
-              "Trying to set a `value` outside of the [`min`, `max`] range (`value` = %s, `min` = %s, `max = %s). `value` will be set to `max`.",
-              paste(value, collapse = ","), min, max
-            )
-    )
-  }
+  assert_slider_value(value = value, min = min, max = max, fun = "updateSliderInput")
 
   message <- dropNulls(list(
     label = label,

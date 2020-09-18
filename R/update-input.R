621c1e441ff9e10cec528f8c9fa118035b2b43fc
colin <colin@thinkr.fr>
2020-09-18 22:55:14 +0200

    Copied the logic from radioButton to updateRadioButton
    this will close #3064

--- R/update-input.R
+++ R/update-input.R
@@ -569,6 +569,11 @@ updateRadioButtons <- function(session, inputId, label = NULL, choices = NULL,
     if (!is.null(choices)) selected <- choices[[1]]
     else if (!is.null(choiceValues)) selected <- choiceValues[[1]]
   }
+
+  # length of selected can't be more than one
+  if (length(selected) > 1)
+    stop("The 'selected' argument must be of length 1")
+
   updateInputOptions(session, inputId, label, choices, selected,
     inline, 'radio', choiceNames, choiceValues)
 }

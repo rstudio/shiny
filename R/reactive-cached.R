b78406870180225d53e8c7be9be1d0e63fd7616a
Winston Chang <winston@stdout.org>
2020-10-15 18:58:41 -0500

    More documentation for cachedReactive

--- R/reactive-cached.R
+++ R/reactive-cached.R
@@ -1,5 +1,98 @@
 #' Create a reactive expression with caching
 #'
+#' @description
+#'
+#' Ordinary [reactive()] expressions will cache their most recent value. This
+#' can make computation more efficient, because time-consuming code can execute
+#' once and the result can be used in multiple different places without needing
+#' to re-execute the code.
+#'
+#' With `cachedReactive()`, any number of previous values can be cached. The
+#' most important difference in using `cachedReactive()` is that you must
+#' provide `cacheKeyExpr`, which is an expression that generates as a _cache
+#' key_, in addition to `valueExpr`, which is an expression that generates the
+#' value returned by the `cachedReactive()`.
+#'
+#' For each possible value returned by `cacheKeyExpr`, there should be one
+#' possible value returned by `valueExpr`; a given value of `cacheKeyExpr`
+#' should correspond to multiple possible values of `valueExpr`.
+#'
+#' The way to use this is to have a `cacheKeyExpr` that is fast to compute, and
+#' a `valueExpr` that is expensive to compute. It evaluates the `cacheKeyExpr`,
+#' and then serializes and hashes the result. If the resulting hashed key is in
+#' the cache, then it simply retrieves and returns it; if not, then it executes
+#' `valueExpr`, stores the result in the cache, and returns it.
+#'
+#' The `cacheKeyExpr` should be fast to compute, and ideally it will return an
+#' object that is not too large, since the object must be serialized and hashed.
+#' Typically, `cacheKeyExpr` will use any reactive inputs that are used by
+#' `valueExpr`, but will do something that is fast and simple with them, like
+#' put them together in a list. It is also best to use non-reference objects,
+#' since the serialization of these objects may not capture relevant changes.
+#'
+#' @section Cache keys and reactivity:
+#'
+#' Because the value from `valueExpr` is cached, it is not necessarily
+#' re-executed, and therefore it can't be used to decide what objects to take
+#' reactive dependencies on. Instead, the `cacheKeyExpr` is used to figure out
+#' which objects to take reactive dependencies on. In short, the `cacheKeyExpr`
+#' is reactive, and `valueExpr` is not (it is run inside of [isolate()]).
+#'
+#' For example, if `cacheKeyExpr=input$x` and `valueExpr={input$x + input$y}`,
+#' then it will only take a reactive dependency on `input$x` -- it won't
+#' recompute `valueExpr` when `input$y` changes. Moreover, the cache won't use
+#' `input$y` as part of the key, and so it could return incorrect values in the
+#' future when it retrieves values from the cache. (See the examples below for
+#' an example of this.)
+#'
+#' A better cache key would be something like `list(input$x, input$y)`. This
+#' does two things: it ensures that a reactive dependency is taken on both `x`
+#' and `y`, and it also makes sure that both values are represented in the cache
+#' key.
+#'
+#' In general, `cacheKeyExpr` should use the same reactive inputs as
+#' `valueExpr`, but the computation should be simpler. If there are other
+#' (non-reactive) values that are consumed, such as external data sources, they
+#' should be used in the `cacheKeyExpr` as well. Note that if the data object is
+#' large, it can make sense to do some sort of reduction on it so that the
+#' serialization and hashing of the cache key is not too expensive.
+#'
+#' Remember that the `cacheKeyExpr` is _reactive_, so it is not re-executed
+#' every single time that someone accesses the `cachedReactive`. For example,
+#' suppose we have this `cachedReactive`:
+#'
+#' ```
+#' r <- cachedReactive(
+#'   cacheKeyExpr = list(input$x, input$y),
+#'   valueExpr = { input$x + input$y }
+#' )
+#' ```
+#'
+#' The first time someone calls `r()`, it executes both `cacheKeyExpr` and
+#' `valueExpr`. If someone calls `r()` again, then it does not need to
+#' re-execute `cacheKeyExpr`, because that `expr` has not been invalidated
+#' via a change to `input$x` or `input$y`; it simply returns the previous value.
+#' However, if `input$x` or `input$y` changes, then the reactive expression will
+#' be invalidated, and the next time that someone calls `r()`, `cacheKeyExpr`
+#' will need to be re-executed.
+#'
+#' @section Async with cached reactives:
+#'
+#' With a cached reactive expression, they key and/or value expression can be
+#' _asynchronous_. In other words, they can be promises -- not regular R
+#' promises, but rather objects provided by the promises package, which are
+#' similar to promises in JavaScript. (See [promises::promise()] for more
+#' information.)
+#'
+#' If `valueExpr` is a promise, then anything that consumes the `cachedReactive`
+#' must expect it to return a promise.
+#'
+#' Similarly, if `cacheKeyExpr` is a promise (in other words, it is
+#' asynchronous), then the entire `cachedReactive` must be asynchronous, since
+#' the key must be computed before `valueExpr` is evaluated (or the value is
+#' retrieved from the cache). Anything that consumes the `cachedReactive` must
+#' therefore expect it to return a promise.
+#'
 #' @inheritParams reactive
 #' @param cacheKeyExpr An expression that returns a value that will be hashed
 #'   and used as a cache key. This key should be a unique identifier for the
@@ -29,7 +122,7 @@
 #'   server = function(input, output) {
 #'     r <- cachedReactive(
 #'       # Use input$x and input$y for the cache key. This is a reactive
-#'       # expression
+#'       # expression.
 #'       list(input$x, input$y),
 #'       {
 #'         # The value expression is an expensive computation

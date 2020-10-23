1d8c97ad157b0421baff4d03dd8a56702dd0eca9
JooYoung Seo <sjysky@gmail.com>
2020-10-23 15:03:25 -0400

    add technical spec in comment for `aria-labelledby`
    Co-authored-by: Carson Sievert <cpsievert1@gmail.com>

--- R/input-checkboxgroup.R
+++ R/input-checkboxgroup.R
@@ -98,10 +98,10 @@ checkboxGroupInput <- function(inputId, label, choices = NULL, selected = NULL,
     style = if (!is.null(width)) paste0("width: ", validateCssUnit(width), ";"),
     class = divClass,
     shinyInputLabel(inputId, label),
-    # Improve accessibility
-    role = "group",
-    # The `aria-labelledby` is tied to id value in `shinyInputLabel()` to be announced to screen readers and in Braille:
+    # The `aria-labelledby` is tied to id value in `shinyInputLabel()` to be announced to screen readers and in Braille
+    # https://www.w3.org/TR/wai-aria-practices/examples/checkbox/checkbox-1/checkbox-1.html
     `aria-labelledby` = paste0(inputId, "-label"),
+      role = "group",
     options
   )
 }

2fd0ce1a0923ae7a625fa6810c1f9bd357218edb
Winston Chang <winston@stdout.org>
2020-11-03 13:35:08 -0600

    Add withEvent observer tests

---
+++ tests/testthat/test-with-event.R
@@ -0,0 +1,50 @@
+
+# Note that there are some tests for withEvent() and caching in
+# test-with-cache.R.
+
+test_that("withEvent and observers", {
+  trigger <- reactiveVal(1)
+  val <- reactiveVal(10)
+
+  vals <- numeric()
+  o <- withEvent(
+    trigger(),
+    x = observe({
+      vals <<- c(vals, val())
+    })
+  )
+
+  flushReact()
+  expect_identical(vals, 10)
+
+  # Changing val has no effect
+  val(20)
+  flushReact()
+  expect_identical(vals, 10)
+
+  # Changing trigger causes the observer to execute
+  trigger(2)
+  flushReact()
+  expect_identical(vals, c(10, 20))
+
+  trigger(3)
+  flushReact()
+  expect_identical(vals, c(10, 20, 20))
+})
+
+
+test_that("withEvent does not prevent observers from being GC'd", {
+  v <- reactiveVal(1)
+  o <- observe({ message(v()) })
+
+  finalized <- FALSE
+  reg.finalizer(o, function(e) { finalized <<- TRUE })
+
+  # o1 shouldn't keep a reference to o (and prevent it from getting GC'd).
+  o1 <- withEvent(o, v())
+  rm(o)
+
+  flushReact()
+  gc()
+  expect_true(finalized)
+})

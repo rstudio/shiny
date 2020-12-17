275387d07f56351d1ca703245ce5af657d6b4012
Barret Schloerke <schloerke@gmail.com>
2020-12-17 13:17:06 -0500

    Format document

--- R/devmode.R
+++ R/devmode.R
@@ -2,14 +2,19 @@
 #'
 #' @description \lifecycle{experimental}
 #'
-#' Developer mode enables a number of options to make a developer's life easier, like enabling non-minified JS and printing messages about deprecated functions and options.
+#'   Developer mode enables a number of options to make a developer's life
+#'   easier, like enabling non-minified JS and printing messages about
+#'   deprecated functions and options.
 #'
-#' Please see the function descriptions for more details.
+#'   Please see the function descriptions for more details.
 #'
 #' @keywords internal
-#' @describeIn devmode Function to set two options to enable/disable Shiny Developer Mode and Developer messages
-#' @param devmode Logical value which should be set to `TRUE` to enable Shiny Developer Mode
-#' @param verbose Logical value which should be set to `TRUE` display Shiny Developer messages
+#' @describeIn devmode Function to set two options to enable/disable Shiny
+#'   Developer Mode and Developer messages
+#' @param devmode Logical value which should be set to `TRUE` to enable Shiny
+#'   Developer Mode
+#' @param verbose Logical value which should be set to `TRUE` display Shiny
+#'   Developer messages
 #' @export
 #' @examples
 #' # Enable Shiny Developer mode
@@ -26,17 +31,29 @@ devmode <- function(
 }
 
 
-#' @describeIn devmode Determines if Shiny is in Developer Mode. If the `getOption("shiny.devmode")` is set to `TRUE` and not in testing inside `testthat`, then Shiny Developer Mode is enabled.
+#' @describeIn devmode Determines if Shiny is in Developer Mode. If the
+#'   `getOption("shiny.devmode")` is set to `TRUE` and not in testing inside
+#'   `testthat`, then Shiny Developer Mode is enabled.
 #' @section Avoiding direct dependency on shiny:
 #'
-#' The methods explained in this help file act independently from the rest of Shiny but are included to provide blue prints for your own packages. If your package already has (or is willing to take) a dependency on Shiny, we recommend using the exported Shiny methods for consistent behavior. Note that if you use exported Shiny methods, it will cause the Shiny package to load. This may be undesirable if your code will be used in (for example) R Markdown documents without `runtime: shiny`.
+#'   The methods explained in this help file act independently from the rest of
+#'   Shiny but are included to provide blue prints for your own packages. If
+#'   your package already has (or is willing to take) a dependency on Shiny, we
+#'   recommend using the exported Shiny methods for consistent behavior. Note
+#'   that if you use exported Shiny methods, it will cause the Shiny package to
+#'   load. This may be undesirable if your code will be used in (for example) R
+#'   Markdown documents without `runtime: shiny`.
 #'
-#' If your package can **not** take a dependency on Shiny, we recommending re-implementing these two functions:
+#'   If your package can **not** take a dependency on Shiny, we recommending
+#'   re-implementing these two functions:
 #'
 #' \enumerate{
 #' \item `in_devmode()`:
 #'
-#' This function should return `TRUE` if `getOption("shiny.devmode")` is set. In addition, we strongly recommend that it also checks to make sure `testthat` is not testing.
+#'   This function should return `TRUE` if `getOption("shiny.devmode")` is set.
+#'   In addition, we strongly recommend that it also checks to make sure
+#'   `testthat` is not testing.
+#'
 #' ```r
 #' in_devmode <- function() {
 #'   isTRUE(getOption("shiny.devmode", FALSE)) &&
@@ -46,7 +63,10 @@ devmode <- function(
 #'
 #' \item `get_devmode_option(name, default, devmode_default, devmode_message)`:
 #'
-#' This function is similar to `getOption(name, default)`, but when the option is not set, the default value changes depending on the Dev Mode.  `get_devmode_option()` should be implemented as follows:
+#' This function is similar to `getOption(name, default)`, but when the option
+#' is not set, the default value changes depending on the Dev Mode.
+#' `get_devmode_option()` should be implemented as follows:
+#'
 #' * If not in Dev Mode:
 #'   * Return `getOption(name, default)`.
 #' * If in Dev Mode:
@@ -57,7 +77,12 @@ devmode <- function(
 #'     * Notify the developer that the Dev Mode default value will be used.
 #'     * Return the Dev Mode default value.
 #'
-#' When notifying the developer that the default value has changed, we strongly recommend displaying a message (`devmode_message`) to `stderr()` once every 8 hours using [rlang::inform()]. This will keep the author up to date as to which Dev Mode options are being altered. To allow developers a chance to disable Dev Mode messages, the message should be skipped if `getOption("shiny.devmode.verbose", TRUE)` is not `TRUE`.
+#' When notifying the developer that the default value has changed, we strongly
+#' recommend displaying a message (`devmode_message`) to `stderr()` once every 8
+#' hours using [rlang::inform()]. This will keep the author up to date as to
+#' which Dev Mode options are being altered. To allow developers a chance to
+#' disable Dev Mode messages, the message should be skipped if
+#' `getOption("shiny.devmode.verbose", TRUE)` is not `TRUE`.
 #'
 #' ```r
 #' get_devmode_option <- function(name, default = NULL, devmode_default, devmode_message) {
@@ -89,7 +114,8 @@ devmode <- function(
 #' ```
 #' }
 #'
-#' The remaining functions in this file are used for author convenience and are not recommended for all reimplementation situations.
+#' The remaining functions in this file are used for author convenience and are
+#' not recommended for all reimplementation situations.
 #' @export
 #' @examples
 #' in_devmode() # TRUE/FALSE?
@@ -100,7 +126,8 @@ in_devmode <- function() {
     !identical(Sys.getenv("TESTTHAT"), "true")
 }
 
-#' @describeIn devmode Temporarily set Shiny Developer Mode and Developer message verbosity
+#' @describeIn devmode Temporarily set Shiny Developer Mode and Developer
+#'   message verbosity
 #' @param code Code to execute with the temporary Dev Mode options set
 #' @export
 #' @examples
@@ -122,11 +149,14 @@ with_devmode <- function(
 }
 
 
-#' @describeIn devmode If Shiny Developer Mode and verbosity are enabled, displays a message once every 8 hrs (by default)
+#' @describeIn devmode If Shiny Developer Mode and verbosity are enabled,
+#'   displays a message once every 8 hrs (by default)
 #' @inheritParams rlang::inform
 #' @param message Developer Mode message to be sent to [rlang::inform()]
-#' @param .frequency Frequency of the Developer Mode message used with [rlang::inform()]. Defaults to once every 8 hours.
-#' @param .frequency_id [rlang::inform()] message identifier. Defaults to `message`.
+#' @param .frequency Frequency of the Developer Mode message used with
+#'   [rlang::inform()]. Defaults to once every 8 hours.
+#' @param .frequency_id [rlang::inform()] message identifier. Defaults to
+#'   `message`.
 #' @param .file Output connection for [rlang::inform()]. Defaults to [stderr()]
 #' @param ... Parameters passed to [rlang::inform()]
 devmode_inform <- function(
@@ -162,9 +192,11 @@ devmode_inform <- function(
 #' @include map.R
 registered_devmode_options <- Map$new()
 
-#' @describeIn devmode Registers a Shiny Developer Mode option with an updated value and Developer message. This registration method allows package authors to write one message in a single location.
+#' @describeIn devmode Registers a Shiny Developer Mode option with an updated
+#'   value and Developer message. This registration method allows package
+#'   authors to write one message in a single location.
 #'
-#' For example, the following Shiny Developer Mode options are registered:
+#'   For example, the following Shiny Developer Mode options are registered:
 #'
 #' ```r
 #' # Reload the Shiny app when a sourced R file changes
@@ -201,8 +233,12 @@ registered_devmode_options <- Map$new()
 #' )
 #' ```
 #' @param name Name of option to look for in `options()`
-#' @param devmode_message Message to display once every 8 hours when utilizing the `devmode_default` value.  If `devmode_message` is missing, the registered `devmode_message` value be used.
-#' @param devmode_default Default value to return if `in_devmode()` returns `TRUE`. For `get_devmode_option()`, if `devmode_default` is missing, the registered `devmode_default` value will be used.
+#' @param devmode_message Message to display once every 8 hours when utilizing
+#'   the `devmode_default` value.  If `devmode_message` is missing, the
+#'   registered `devmode_message` value be used.
+#' @param devmode_default Default value to return if `in_devmode()` returns
+#'   `TRUE`. For `get_devmode_option()`, if `devmode_default` is missing, the
+#'   registered `devmode_default` value will be used.
 #' @examples
 #' # Ex: Within shiny, we register the option "shiny.minified"
 #' #   to default to `FALSE` when in Dev Mode
@@ -230,9 +266,16 @@ register_devmode_option <- function(
 }
 
 
-#' @describeIn devmode Provides a consistent way to change the expected [getOption()] behavior when Developer Mode is enabled. This method is very similar to [getOption()] where the globally set option takes precedence. See section "Avoiding direct dependency on shiny" for `get_devmode_option()` implementation details.
+#' @describeIn devmode Provides a consistent way to change the expected
+#'   [getOption()] behavior when Developer Mode is enabled. This method is very
+#'   similar to [getOption()] where the globally set option takes precedence.
+#'   See section "Avoiding direct dependency on shiny" for
+#'   `get_devmode_option()` implementation details.
 #'
-#' **Package developers:** Register your Dev Mode option using `register_devmode_option()` to avoid supplying the same `devmode_default` and `devmode_message` values throughout your package. (This requires a \pkg{shiny} dependency.)
+#'   **Package developers:** Register your Dev Mode option using
+#'   `register_devmode_option()` to avoid supplying the same `devmode_default`
+#'   and `devmode_message` values throughout your package. (This requires a
+#'   \pkg{shiny} dependency.)
 #' @export
 #' @examples
 #' # Used within `shiny::runApp(launch.browser)`

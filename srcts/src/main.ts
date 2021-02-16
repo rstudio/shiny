5392dcf1a98cd6bdb1ff211d8b7b744bd692df5f
Barret Schloerke <schloerke@gmail.com>
2021-02-16 10:25:29 -0500

    Set up `main.ts` with comments on what files should be imported

---
+++ srcts/src/main.ts
@@ -0,0 +1,174 @@
+
+import {
+  escapeHTML,
+  randomId,
+  strToBool,
+  getStyle,
+  padZeros,
+  roundSignif,
+  parseDate,
+  formatDateUTC,
+  makeResizeFilter,
+  pixelRatio,
+  scopeExprToFunc,
+  asArray,
+  mergeSort,
+  $escape,
+  mapValues,
+  isnan,
+  _equal,
+  equal,
+  compareVersion,
+  updateLabel,
+  getComputedLinkColor,
+  blob
+} from "./utils"
+
+import {isQt, isIE, IEVersion} from "./external/browser"
+
+import {FileProcessor} from "./file/FileProcessor"
+
+
+function main() {
+
+// "_start.js"
+// √
+
+
+// "utils.js"
+// √
+
+
+// "browser.js"
+// √
+
+
+// "input_rate.js"
+
+
+
+// "shinyapp.js"
+
+
+
+// "notifications.js"
+
+
+
+// "modal.js"
+
+
+
+// "file_processor.js"
+// √
+
+
+// "binding_registry.js"
+
+
+
+// "output_binding.js"
+
+
+
+// "output_binding_text.js"
+
+
+
+// "output_binding_image.js"
+
+
+
+// "output_binding_html.js"
+
+
+
+// "output_binding_downloadlink.js"
+
+
+
+// "output_binding_datatable.js"
+
+
+
+// "output_binding_adapter.js"
+
+
+
+// "input_binding.js"
+
+
+
+// "input_binding_text.js"
+
+
+
+// "input_binding_textarea.js"
+
+
+
+// "input_binding_password.js"
+
+
+
+// "input_binding_number.js"
+
+
+
+// "input_binding_checkbox.js"
+
+
+
+// "input_binding_slider.js"
+
+
+
+// "input_binding_date.js"
+
+
+
+// "input_binding_daterange.js"
+
+
+
+// "input_binding_select.js"
+
+
+
+// "input_binding_radio.js"
+
+
+
+// "input_binding_checkboxgroup.js"
+
+
+
+// "input_binding_actionbutton.js"
+
+
+
+// "input_binding_tabinput.js"
+
+
+
+// "input_binding_fileinput.js"
+
+
+
+// "init_shiny.js"
+
+
+
+// "reactlog.js"
+
+
+
+// "_end.js"
+// √
+
+
+
+
+}
+
+export {main}

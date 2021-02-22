d98dd2bb99f518e6882e183f03b69935d188abd5
Barret Schloerke <schloerke@gmail.com>
2021-02-22 18:20:00 -0500

    Remove global definition files as jquery is not globally defined anymore

--- srcts/src/window/extend/JQuery.d.ts
+++
@@ -1,9 +0,0 @@
-// Why this file is just an extension of JQuery with no import/exports
-// https://stackoverflow.com/a/30961346/591574
-
-// Model:
-// https://stackoverflow.com/a/47091565/591574
-interface JQuery {
-  //custom jquery plugin, had no typings
-  bsDatepicker(method: string): string | number | Date | null;
-}
--- srcts/src/window/globals.ts
+++ srcts/src/window/globals.ts
@@ -1,4 +1,15 @@
 /* eslint "@typescript-eslint/ban-ts-comment": 0 */
+
+// Why this file is just an extension of JQuery with no import/exports
+// https://stackoverflow.com/a/30961346/591574
+
+// Model:
+// https://stackoverflow.com/a/47091565/591574
+// interface JQueryStatic {
+//   //custom jquery plugin, had no typings
+//   bsDatepicker(method: string): string | number | Date | null;
+// }
+
 // @ts-ignore
 const WindowShiny = (window.Shiny = window.Shiny || {});
 // @ts-ignore

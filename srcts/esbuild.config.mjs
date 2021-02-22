0ae108006d347f089574c4f6ad351fcea39077ec
Barret Schloerke <schloerke@gmail.com>
2021-02-22 14:29:17 -0500

    Use script for esbuild

--- srcts/esbuild.config.mjs
+++ srcts/esbuild.config.mjs
@@ -2,37 +2,35 @@ import esbuild from "esbuild";
 import babel from "esbuild-plugin-babel";
 import readcontrol from "readcontrol";
 
-let watch = (process.argv.length >= 3 && process.argv[2] == "--watch");
+import process from "process";
 
-let outdir = "../inst/www/shared/"
+let watch = process.argv.length >= 3 && process.argv[2] == "--watch";
+
+let outdir = "../inst/www/shared/";
 let opts = {
-    entryPoints: [
-      "src/index.ts"
-    ],
-    bundle: true,
-    watch: watch,
-    plugins: [
-      babel()
-    ],
-    target: "es5",
-    define: {
-      SHINY_VERSION: `"${readcontrol.readSync("../DESCRIPTION").version}"`
-    }
-  }
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  watch: watch,
+  plugins: [babel()],
+  target: "es5",
+  sourcemap: true,
+  define: {
+    SHINY_VERSION: `"${readcontrol.readSync("../DESCRIPTION").version}"`,
+  },
+};
 
-console.log("Building shiny.js")
+console.log("Building shiny.js");
 esbuild
-.build({
-  ...opts,
-  outfile: outdir + "shiny.js"
-})
-.then(() => {
-    console.log("Building shiny.min.js")
-    esbuild
-      .build({
-        ...opts,
-        outfile: outdir + "shiny.min.js",
-        minify: true
-      })
+  .build({
+    ...opts,
+    outfile: outdir + "shiny.js",
   })
-  .catch(() => process.exit(1))
+  .then(function () {
+    console.log("Building shiny.min.js");
+    esbuild.build({
+      ...opts,
+      outfile: outdir + "shiny.min.js",
+      minify: false,
+    });
+  })
+  .catch(() => process.exit(1));

f9d82f17af47e8ac195b34f62f1a5a14314bd058
Carson <cpsievert1@gmail.com>
2021-03-05 13:10:32 -0600

    Towards some better styles

--- R/treelistPanel.R
+++ R/treelistPanel.R
@@ -6,39 +6,19 @@ treelistPanel <- function(..., id = NULL,
   if (!is.null(id))
     selected <- restoreInput(id = id, default = selected)
 
-  # build the tabset
-  # TODO: would allowing strings make sense?
   tabset <- buildTreePanel(..., ulClass = "treelist-nav", id = id, selected = selected)
 
   row <- if (fluid) fluidRow else fixedRow
 
+  # TODO:
+  # 1. consider using flexbox instead for layout
+  # 2. how to integrate with a sidebar?
   row(
     class = "treelist-container",
     column(widths[[1]], tabset$navList),
     column(widths[[2]], tabset$content),
     bslib::bs_dependency_defer(treelistCssDependency)
   )
-}
-
-treelistCssDependency <- function(theme) {
-  name <- "treelistPanel"
-  version <- packageVersion("shiny")
-  if (!is_bs_theme(theme)) {
-    htmlDependency(
-      name = name, version = version,
-      src = c(href = "treelist"),
-      stylesheet = "treelist.css"
-    )
-  } else {
-    scss <- system.file(package = "shiny", "www/shared/treelist/treelist.scss")
-    bslib::bs_dependency(
-      sass::sass_file(scss),
-      theme = theme,
-      name = name,
-      version = version,
-      cache_key_extra = version
-    )
-  }
 }
 
 # Algorithm inspired by buildTabset() but we need different HTML/CSS,
@@ -51,7 +31,8 @@ buildTreePanel <- function(..., ulClass, id = NULL, selected = NULL, foundSelect
   foundSelected <- res$foundSelected
 
   # add input class if we have an id
-  if (!is.null(id)) ulClass <- paste(ulClass, "shiny-tab-input")
+  # TODO: write another binding?
+  #if (!is.null(id)) ulClass <- paste(ulClass, "shiny-tab-input")
 
   if (anyNamed(tabs)) {
     nms <- names(tabs)
@@ -92,47 +73,69 @@ buildTreeItem <- function(index, tabsetId, foundSelected, tabs = NULL, divTag = 
       warning("Configurable icons are not yet supported in treelistPanel().")
     }
     tabset <- buildTreePanel(
-      !!!divTag$tabs, ulClass = "treelist-nav", foundSelected = foundSelected
+      !!!divTag$tabs, ulClass = "treelist-nav", #TODO: do we want this class on every panel?
+      foundSelected = foundSelected
     )
     # Sort of like .dropdown in the tabsetPanel() case,
     # but utilizes collapsing (which is recursive) instead of dropdown
     active <- containsSelectedTab(divTag$tabs)
     menuId <- paste0("collapse-", p_randomInt(1000, 10000))
     liTag <- tags$li(
-      tags$button(
-        class = "btn",
+      class = "treelist-item",
+      tags$a(
+        class = if (!active) "collapsed",
         "data-toggle" = "collapse",
         "data-value" = divTag$menuName,
         "data-target" = paste0("#", menuId),
+        role = "button",
+        getIcon(iconClass = divTag$iconClass),
         divTag$title
       ),
       div(
         class = "collapse",
-        class = if (active) "show",
+        class = if (active) "show in",
         id = menuId,
         tabset$navList
       )
     )
-
-    # list of tab content divs from the child tabset
-    divTag <- tabset$content$children
-    return(list(liTag = liTag, divTag = divTag))
+    return(list(liTag = liTag, divTag = tabset$content$children))
   }
 
   if (isTabPanel(divTag)) {
-    x <- buildNavItem(divTag, tabsetId, index)
-    liTag <- x$liTag
-    # We don't want BS4's nav-item/nav-link classes
-    x$liTag <- tagFunction(function() {
-      liTag <- if (inherits(liTag, "shiny.tag.function")) liTag() else liTag
-      if (!tagHasClass(liTag, "nav-item")) return(liTag)
-      liTag$attribs$class <- NULL
-      liTag$children[[1]]$attribs$class <- NULL
-      liTag
-    })
-
-    return(list(liTag = x$liTag, divTag = x$divTag))
+    id <- paste("tab", tabsetId, index, sep = "-")
+    title <- tagGetAttribute(divTag, "title")
+    value <- tagGetAttribute(divTag, "data-value")
+    icon <- getIcon(iconClass = tagGetAttribute(divTag, "data-icon-class"))
+    active <- isTabSelected(divTag)
+    liTag <- bs3NavItem(id, title, value, icon, active)
+    liTag <- tagAppendAttributes(liTag, class = "treelist-item")
+    divTag <- tagAppendAttributes(divTag, class = if (active) "active")
+    divTag$attribs$id <- id
+    divTag$attribs$title <- NULL
+    return(list(liTag = liTag, divTag = divTag))
   }
 
   abort("treelistPanel() items must be tabPanel()s and/or tabPanelMenu()s")
+}
+
+
+treelistCssDependency <- function(theme) {
+  name <- "treelistPanel"
+  version <- packageVersion("shiny")
+  if (!is_bs_theme(theme)) {
+    htmlDependency(
+      name = name, version = version,
+      src = c(href = "shared/treelist"),
+      stylesheet = "treelist.css"
+    )
+  } else {
+    scss <- system.file(package = "shiny", "www/shared/treelist/treelist.scss")
+    bslib::bs_dependency(
+      sass::sass_file(scss),
+      theme = theme,
+      name = name,
+      version = version,
+      cache_key_extra = version
+    )
+  }
 }

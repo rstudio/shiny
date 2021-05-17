a81ccab4b2e733f95f1adfcdbd3f3b55e11dfd8f
Barret Schloerke <schloerke@gmail.com>
2021-05-17 16:55:37 -0400

    Move reactlog

--- srcts/src/main.ts
+++ srcts/src/main.ts
@@ -2611,64 +2611,7 @@ function main(): void {
   });
 
   // "reactlog.js"
-  $(document).on("keydown", function (e) {
-    if (e.which !== 114 || (!e.ctrlKey && !e.metaKey) || e.shiftKey || e.altKey)
-      return;
-    const url =
-      "reactlog?w=" +
-      window.escape(Shiny.shinyapp.config.workerId) +
-      "&s=" +
-      window.escape(Shiny.shinyapp.config.sessionId);
-
-    window.open(url);
-    e.preventDefault();
-  });
-
-  $(document).on("keydown", function (e) {
-    if (
-      // if not one of the key combos below
-      !(
-        // cmd/ctrl + fn + f4
-        (
-          (e.which === 115 &&
-            (e.ctrlKey || e.metaKey) &&
-            !e.shiftKey &&
-            !e.altKey) ||
-          // cmd/ctrl + shift + fn + f3
-          (e.which === 114 &&
-            (e.ctrlKey || e.metaKey) &&
-            e.shiftKey &&
-            !e.altKey)
-        )
-      )
-    ) {
-      return;
-    }
-
-    const url =
-      "reactlog/mark?w=" +
-      window.escape(Shiny.shinyapp.config.workerId) +
-      "&s=" +
-      window.escape(Shiny.shinyapp.config.sessionId);
-
-    // send notification
-    $.get(url, function (result) {
-      if (result !== "marked") return;
-
-      const html =
-        '<span id="shiny-reactlog-mark-text">Marked time point in reactlog</span>';
-
-      Shiny.notifications.show({
-        html: html,
-        closeButton: true,
-      });
-    }).fail(function () {
-      // found returned error while marking, should open webpage
-      window.open(url);
-    });
-
-    e.preventDefault();
-  });
+  // √; ./shiny/reactlog.ts
 
   // "_end.js"
   // √

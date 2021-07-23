b9a2338aff559efc82f91dc053f8f8b09e677ff8
Barret Schloerke <schloerke@gmail.com>
2021-07-23 14:41:50 -0400

    add `handleEnvAndQuoted()` and `handleEnvAndQuotedInternal()` and `handleEnvAndQuoted3Internal()`

--- R/utils-lang.R
+++ R/utils-lang.R
@@ -68,21 +68,58 @@ formalsAndBody <- function(x) {
 #'
 #' @export
 quoToFunction <- function(q,
-                          label = deparse(sys.call(-1)[[1]]),
+                          label = sys.call(-1)[[1]],
                           ..stacktraceon = FALSE)
 {
   q <- as_quosure(q)
-  func <- as_function(q)
-  # as_function returns a function that takes `...`. We want one that takes no
+  func <- quoToSimpleFunction(q)
+  # cat("quoToFunction()\n")
+  # str(list(
+  #   self = environment(),
+  #   q = q,
+  #   func_env = environment(func)
+  # ))
+  # browser()
+  wrapFunctionLabel(func, updateFunctionLabel(label), ..stacktraceon = ..stacktraceon)
+}
+updateFunctionLabel <- function(label) {
+  # browser()
+  if (all(is.language(label))) {
+    # Prevent immediately invoked functions like as.language(a()())
+    if (is.language(label) && length(label) > 1) {
+      return("wrappedFunction")
+    }
+    label <- deparse(label, width.cutoff = 500L)
+  }
+  label <- as.character(label)
+  # Prevent function calls that are over one line; (Assignments are hard to perform)
+    # Prevent immediately invoked functions like "a()()"
+  if (length(label) > 1 || grepl("(", label, fixed = TRUE)) {
+    return("wrappedFunction")
+  }
+  if (label == "NULL") {
+    return("wrappedFunction")
+  }
+  label
+}
+
+quoToSimpleFunction <- function(q) {
+  return(
+    new_function(list(), get_expr(q), get_env(q))
+  )
+  # Should not use `new_function(list(), get_expr(q), get_env(q))` as extra logic
+  # is done by rlang to convert the quosure to a function within `as_function(q)`
+  fun <- as_function(q)
+  # as_function returns a function that takes `...`. We need one that takes no
   # args.
-  formals(func) <- list()
-  wrapFunctionLabel(func, label, ..stacktraceon = ..stacktraceon)
+  formals(fun) <- list()
+  fun
 }
 
 
 #' Convert expressions and quosures to functions
 #'
-#' `getQuosure()` and `quoToFunction()` are meant to be used together in a
+#' `handleEnvAndQuoted()` and `quoToFunction()` are meant to be used together in a
 #' `render` function, to capture user expressions or quosures and convert them
 #' to functions. They are meant to replace the older functions
 #' [installExprFunction()] and [exprToFunction()] (although those will continue
@@ -167,7 +204,152 @@ quoToFunction <- function(q,
 #'
 #' @rdname quoToFunction
 #' @export
+handleEnvAndQuoted <- function(q, x, env, quoted) {
+  ## To avoid possible boilerplate with `deprecated()`...
+  env_is_present <-
+    if (!is_present(env)) {
+      # If `env` is `deprecated()`, set to the parent frame of the caller
+      env <- parent.frame(2)
+
+      FALSE # env_is_present
+    } else {
+      # check of parent frame had a missing _`env`_ param
+      eval(substitute(!missing(env)), parent.frame())
+    }
+  quoted_is_present <-
+    if (!is_present(quoted)) {
+      # If `quoted` is deprecated(), set to `FALSE`
+      quoted <- FALSE
+
+      FALSE # quoted_is_present
+    } else {
+      # check of parent frame had a missing _`quoted`_ param
+      eval(substitute(!missing(quoted)), parent.frame())
+    }
+  ##
+  x_is_quosure <- is_quosure(eval(substitute(substitute(x)), parent.frame()))
+
+  handleEnvAndQuoted_(
+    q = q, env = env, quoted = quoted,
+    env_is_present = env_is_present,
+    quoted_is_present = quoted_is_present,
+    x_is_quosure = x_is_quosure,
+    verbose = TRUE
+  )
+}
+# Same as `handleEnvAndQuoted()`, but quiet
+# Under assumption that `env = deprecated()` and `quoted = deprecated()`
+handleEnvAndQuotedInternal <- function(q, x, env, quoted) {
+  ## Can leverage the fact that we know all `env` and `quoted` args are set to `deprecated()`
+  env_is_present <- is_present(env) # eval(substitute(!missing(env)), parent.frame())
+  quoted_is_present <- is_present(quoted) # eval(substitute(!missing(quoted)), parent.frame())
+  x_is_quosure <- is_quosure(eval(substitute(substitute(x)), parent.frame()))
+  if (env_is_present) env <- parent.frame(2)
+  if (quoted_is_present) quoted <- FALSE
+
+  handleEnvAndQuoted_(
+    q = q,
+    env = env,
+    quoted = quoted,
+    env_is_present = env_is_present,
+    quoted_is_present = quoted_is_present,
+    x_is_quosure = x_is_quosure,
+    verbose = FALSE
+  )
+}
+# Reaches up two calls to check if env / quoted were provided.
+# Can not leverage `is_present()` logic like in `handleEnvAndQuotedInternal()`
+handleEnvAndQuoted3Internal <- function(q, x, env, quoted) {
+
+  ## To avoid possible boilerplate with `deprecated()`...
+  env_is_present <-
+    if (!is_present(env)) {
+      # If `env` is `deprecated()`, set to the parent frame of the caller
+      env <- parent.frame(3)
+
+      FALSE # env_is_present
+    } else {
+      # check of parent frame of the caller had a missing _`env`_ param
+      eval(eval(substitute(substitute(!missing(env))), parent.frame()), parent.frame(2))
+    }
+  quoted_is_present <-
+    if (!is_present(quoted)) {
+      # If `quoted` is deprecated(), set to `FALSE`
+      quoted <- FALSE
+
+      FALSE # quoted_is_present
+    } else {
+      # check of parent frame had a missing _`quoted`_ param
+      eval(eval(substitute(substitute(!missing(quoted))), parent.frame()), parent.frame(2))
+    }
+  ##
+  x_is_quosure <- is_quosure(eval(eval(substitute(substitute(x)), parent.frame()), parent.frame(2)))
+
+  # cat("handleEnvAndQuoted3Internal(); environment: \n")
+  # str(list(
+  #   q = q,
+  #   env = env,
+  #   quoted = quoted,
+  #   self = environment(),
+  #   parent = parent.frame(1),
+  #   grandparent = parent.frame(2),
+  #   grandgrandparent = parent.frame(3),
+  #   grandgrandgrandparent = parent.frame(4)
+  # ))
+
+  handleEnvAndQuoted_(
+    q = q, env = env, quoted = quoted,
+    env_is_present = env_is_present,
+    quoted_is_present = quoted_is_present,
+    x_is_quosure = x_is_quosure,
+    verbose = FALSE
+  )
+}
+handleEnvAndQuoted_ <- function(
+  q, env, quoted,
+  env_is_present, quoted_is_present, x_is_quosure,
+  verbose = TRUE
+) {
+  # cat("handleEnvAndQuoted_()\n")
+  # str(list(
+  #   q = q,
+  #   env = env, quoted = quoted, env_is_present = env_is_present, quoted_is_present = quoted_is_present, x_is_quosure = x_is_quosure
+  # ))
+  # is_quoted <- quoted_is_present && isTRUE(quoted)
+  if (
+    env_is_present ||
+    quoted_is_present
+  ) {
+    if (verbose) deprecatedEnvQuotedMessage()
+    # browser()
+
+    # Can't have x be a quosure and use env/quoted.
+    if (x_is_quosure) {
+      stop(
+        "Can not use a `quosure()` with either the `env` or `quoted` parameters.\n",
+        "Please alter your quosure before the shiny function call and ",
+        "do not supply any `env` or `quoted` parameters.\n",
+        "To use your `quosure()` object directly, use `inject()` mixed with `!!`.\n",
+        "Ex: `inject(renderText(!!q))`"
+      )
+    }
+
+    # In this code path, x is NOT a literal quosure object
+    if (isTRUE(quoted)) {
+      q <- quo_set_expr(q, eval_tidy(q))
+    }
+    if (env_is_present) {
+      q <- quo_set_env(q, env)
+    }
+  }
+  q
+}
+
+
+# Internal helper method to reach up 1 parent frame.
 getQuosure <- function(x, env, quoted) {
+  stop("Removing getQuosure()")
+
   if (!any(c("env", "quoted") %in% names(match.call()))) {
     # This code path is used when `getQuosure(x)` is called.
     #
@@ -250,6 +432,7 @@ getQuosure <- function(x, env, quoted) {
 # 2 calls back to find the expression passed in, this function reaches 3 calls
 # back, and it is only used internally within Shiny.
 getQuosure3 <- function(x, env, quoted) {
+  stop("Removing getQuosure3()")
   if (!any(c("env", "quoted") %in% names(match.call(sys.function(-1), sys.call(-1))))) {
     # This code path is used when `getQuosure(x)` is called.
 
@@ -301,11 +484,16 @@ getQuosure3 <- function(x, env, quoted) {
 
 #' Convert an expression to a function
 #'
-#' This is to be called from another function, because it will attempt to get
-#' an unquoted expression from two calls back. Note: as of Shiny 1.7.0, it is
-#' recommended to use [getQuosure()] and [quoToFunction()] instead of
+#' @description
+#' `r lifecycle::badge("superseded")` Please see [`quoToFunction()`] for updated usage. (Shiny 1.7.0)
+#'
+#' Note: as of Shiny 1.7.0, it is
+#' recommended to use [`quoToFunction()`] (and if necessary, [`handleEnvAndQuoted()`]) instead of
 #' `exprToFunction()` and `installExprFunction()`. See the examples for
 #' information on how to migrate to `getQuosure()` and `quoToFunction()`.
+#'
+#' This is to be called from another function, because it will attempt to get
+#' an unquoted expression from two calls back.
 #'
 #' For `exprToFunction()`:
 #' If `expr` is a quoted expression, then this just converts it to a function.
@@ -406,10 +594,27 @@ getQuosure3 <- function(x, env, quoted) {
 #' isolate(tripleA())
 #' # "text, text, text"
 #' @export
-exprToFunction <- function(expr, env = parent.frame(), quoted = FALSE) {
-  expr <- getQuosure3(expr, env, quoted)
-  new_function(list(), get_expr(expr), get_env(expr))
+exprToFunction <- function(expr, env=parent.frame(), quoted=FALSE) {
+  if (!quoted) {
+    expr <- eval(substitute(substitute(expr)), parent.frame())
+  }
+
+  # expr is a quoted expression
+  new_function(list(), body=expr, env=env)
 }
+# exprToFunctionNew <- function(expr, env = parent.frame(), quoted = FALSE) {
+#   force(env)
+#   # shinyDeprecated("1.7.0", "exprToFunction()", "quoToFunction()", type = "superseded")
+#   q_old <- q <- enquo0(expr)
+#   q <- handleEnvAndQuoted3Internal(q, expr, env, quoted)
+#   cat("exprToFunction:\n"); str(list(
+#     q_old = q_old,
+#     q = q,
+#     env = env
+#   ))
+#   # browser()
+#   quoToFunction(q, label = functionLabel())
+# }
 
 #' @rdname exprToFunction
 #'
@@ -425,25 +630,50 @@ exprToFunction <- function(expr, env = parent.frame(), quoted = FALSE) {
 installExprFunction <- function(expr, name, eval.env = parent.frame(2),
                                 quoted = FALSE,
                                 assign.env = parent.frame(1),
-                                label = deparse(sys.call(-1)[[1]]),
+                                label = sys.call(-1)[[1]],
                                 wrappedWithLabel = TRUE,
                                 ..stacktraceon = FALSE) {
+  if (!quoted) {
+    quoted <- TRUE
+    expr <- eval(substitute(substitute(expr)), parent.frame())
+  }
 
-  expr <- getQuosure3(expr, eval.env, quoted)
-  func <- new_function(list(), get_expr(expr), get_env(expr))
-
+  func <- exprToFunction(expr, eval.env, quoted)
   if (length(label) > 1) {
     # Just in case the deparsed code is more complicated than we imagine. If we
     # have a label with length > 1 it causes warnings in wrapFunctionLabel.
     label <- paste0(label, collapse = "\n")
   }
   if (wrappedWithLabel) {
-    func <- wrapFunctionLabel(func, label, ..stacktraceon = ..stacktraceon)
+    func <- wrapFunctionLabel(func, updateFunctionLabel(label), ..stacktraceon = ..stacktraceon)
   } else {
     registerDebugHook(name, assign.env, label)
   }
   assign(name, func, envir = assign.env)
 }
+# installExprFunctionNew <- function(expr, name, eval.env = parent.frame(2),
+#                                 quoted = FALSE,
+#                                 assign.env = parent.frame(1),
+#                                 label = functionLabel(),
+#                                 wrappedWithLabel = TRUE,
+#                                 ..stacktraceon = FALSE) {
+#   # shinyDeprecated("1.7.0", "installExprFunction()", "quoToFunction()", type = "superseded")
+#   q <- enquo0(expr)
+#   q <- handleEnvAndQuoted3Internal(q, expr, eval.env, quoted)
+#   func <- quoToSimpleFunction(q)
+
+#   if (length(label) > 1) {
+#     # Just in case the deparsed code is more complicated than we imagine. If we
+#     # have a label with length > 1 it causes warnings in wrapFunctionLabel.
+#     label <- paste0(label, collapse = "\n")
+#   }
+#   if (wrappedWithLabel) {
+#     func <- wrapFunctionLabel(func, label, ..stacktraceon = ..stacktraceon)
+#   } else {
+#     registerDebugHook(name, assign.env, label)
+#   }
+#   assign(name, func, envir = assign.env)
+# }
 
 # Utility function for creating a debugging label, given an expression.
 # `expr` is a quoted expression.

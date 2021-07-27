020bfc174c7007cf69f529e70cc179ef9c08ade2
Barret Schloerke <schloerke@gmail.com>
2021-07-27 16:43:48 -0400

    If a man file had `\keywords{internal}` or is package docs, then do not expect the files to be listed in pkgdown entries

--- tools/documentation/checkPkgdown.R
+++ tools/documentation/checkPkgdown.R
@@ -15,7 +15,6 @@ local({
     unlist(lapply(yaml::yaml.load_file(pkgdown_file)$reference, function(x) x$contents))
   }
 
-
   indexed_topics <- get_indexed(pkgdown_file)
   all_topics     <- get_exported()
 
@@ -29,30 +28,48 @@ local({
       })
     })
   )
-  # TODO check for internal help pages and not warn about them
-  # TODO .. or check that pkgdown::build_reference_index() does not produce warnings
-  known_unindexed <- c("shiny-package", "stacktrace", "knitr_methods",
-                       "pageWithSidebar", "headerPanel", "shiny.appobj",
-                       "reexports", "makeReactiveBinding",
-                       "reactiveConsole", "registerThemeDependency",
-                       "memoryCache", "diskCache", "shinyDeprecated")
 
   ## This test ensures that every documented topic is included in
   ## staticdocs/index.r, unless explicitly waived by specifying it
-  ## in the known_unindexed variable above.
-  missing <- setdiff(all_topics, c(known_unindexed, indexed_topics))
-  ## Explicitly add reexports man files as they will be added at shiny-dev-center documentation build time
-  unknown <- setdiff(c(known_unindexed, indexed_topics), c(all_topics, reexports_man_file_names))
+  ## as `@keywords internal`
+  no_entry_topics <- setdiff(all_topics, indexed_topics)
+  internal_topics <- unlist(lapply(
+    all_topics,
+    function(topic) {
+      topic_txt <- readLines(rprojroot::find_package_root_file(paste0("man/", topic, ".Rd")))
+      if (
+        any(grepl("\\keyword{internal}", topic_txt, fixed = TRUE)) ||
+        any(grepl("\\docType{package}", topic_txt, fixed = TRUE))
+      ) {
+        # Return internal topic name
+        topic
+      } else {
+        NULL
+      }
+    }
+  ))
 
-  testthat::expect_equal(length(missing), 0,
-    info = paste("Functions missing from ./tools/documentation/pkgdown.yml:\n",
-      paste(" - ", missing, sep = "", collapse = "\n"),
-      "\nPlease update ./tools/documentation/pkgdown.yml or ",
-      "`known_unindexed` in ./tools/documentation/checkPkgdown.R",
+  # Make sure internal functions to NOT have an entry
+  displayed_internal_topics <- internal_topics[internal_topics %in% indexed_topics]
+  testthat::expect_equal(length(displayed_internal_topics), 0,
+    info = paste("Functions listed in ./tools/documentation/pkgdown.yml but have the keyword internal:\n",
+      paste(" - ", displayed_internal_topics, sep = "", collapse = "\n"),
+      "\nPlease update ./tools/documentation/pkgdown.yml or make it `#' @keywords internal`",
       sep = ""))
-  testthat::expect_equal(length(unknown), 0,
+
+  # Make sure there are no non-internal topics have an entry
+  missing_entry_topics <- setdiff(no_entry_topics, internal_topics)
+  testthat::expect_equal(length(missing_entry_topics), 0,
+    info = paste("Functions missing from ./tools/documentation/pkgdown.yml:\n",
+      paste(" - ", missing_entry_topics, sep = "", collapse = "\n"),
+      "\nPlease update ./tools/documentation/pkgdown.yml or remove `#' @keywords internal`",
+      sep = ""))
+
+  ## Explicitly add reexports man files as they will be added at shiny-dev-center documentation build time
+  unknown_topics <- setdiff(indexed_topics, c(all_topics, reexports_man_file_names))
+  testthat::expect_equal(length(unknown_topics), 0,
     info = paste("Unrecognized functions in ./tools/documentation/pkgdown.yml:\n",
-      paste(" - ", unknown, sep = "", collapse = "\n"),
+      paste(" - ", unknown_topics, sep = "", collapse = "\n"),
       "\nPlease update ./tools/documentation/pkgdown.yml",
       sep = ""))
   invisible(TRUE)

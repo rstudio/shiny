a8c14dab9623c984a66fcd4824d8d448afb151e7
Winston Chang <winston@stdout.org>
2021-10-04 11:07:42 -0500

    Bump version to 1.7.1.9000

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1,4 +1,4 @@
-/*! shiny 1.7.0 | (c) 2012-2021 RStudio, PBC. | License: GPL-3 | file LICENSE */
+/*! shiny 1.7.1.9000 | (c) 2012-2021 RStudio, PBC. | License: GPL-3 | file LICENSE */
 (function() {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
@@ -13265,7 +13265,7 @@
   var windowShiny2;
   function setShiny(windowShiny_) {
     windowShiny2 = windowShiny_;
-    windowShiny2.version = "1.7.0";
+    windowShiny2.version = "1.7.1.9000";
     var _initInputBindings = initInputBindings(), inputBindings = _initInputBindings.inputBindings, fileInputBinding2 = _initInputBindings.fileInputBinding;
     var _initOutputBindings = initOutputBindings(), outputBindings = _initOutputBindings.outputBindings;
     setFileInputBinding(fileInputBinding2);

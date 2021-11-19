c6fe10c3de966a11dc665cf8b3e0bf390b480c06
Barret Schloerke <schloerke@gmail.com>
2021-11-19 17:33:59 -0500

    try to trigger a new version to be used in TS builds

--- .github/shiny-workflows/routine.sh
+++ .github/shiny-workflows/routine.sh
@@ -6,7 +6,7 @@ Rscript ./tools/updatePackageJsonVersion.R
 if [ -n "$(git status --porcelain package.json)" ]
 then
   yarn build
-  git commit ./inst package.json -m 'Sync package version (GitHub Actions)' || echo "No package version to commit"
+  git add ./inst package.json && git commit -m 'Sync package version (GitHub Actions)' || echo "No package version to commit"
 else
   echo "No package version difference detected; package.json is current."
 fi

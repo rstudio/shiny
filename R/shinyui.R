a9ba867e73886168d857b2a8e07c2ff156b610cb
Neutron3529 <qweytr_1@163.com>
2022-05-19 23:56:44 +0800

    make mathjax configurable
    Provide two option, `MathJax_url` and `MathJax_config`, to enable the modification of the default mathjax.js.
    Since the response of `mathjax.rstudio.com` could be slower in some regions, it is better to provide a default value and a configure technique to enable the modification of the default mathjax url.
    
    change the names of the two newly added options
    
    update the doc of newly added shiny.mathjax.*

--- R/shinyui.R
+++ R/shinyui.R
@@ -14,7 +14,7 @@ NULL
 #' # now we can just write "static" content without withMathJax()
 #' div("more math here $$\\sqrt{2}$$")
 withMathJax <- function(...) {
-  path <- 'https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+  path <- paste(getOption('shiny.mathjax.url','https://mathjax.rstudio.com/latest/MathJax.js'),getOption('shiny.mathjax.config','config=TeX-AMS-MML_HTMLorMML'),sep='?')
   tagList(
     tags$head(
       singleton(tags$script(src = path, type = 'text/javascript'))

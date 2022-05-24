44f38e41b228a9b8d908503f7da79fbdb76233c4
Barret Schloerke <schloerke@gmail.com>
2022-05-24 17:46:37 -0400

    Get better narrowing for typing for `hasOwnProperty(obj, key)`

--- srcts/src/utils/object.ts
+++ srcts/src/utils/object.ts
@@ -1,5 +1,10 @@
-function hasOwnProperty(x: { [key: string]: unknown }, y: string): boolean {
-  return Object.prototype.hasOwnProperty.call(x, y);
+// Inspriation from https://fettblog.eu/typescript-hasownproperty/
+// But mixing with "NonNullable key of Obj" instead of "key to unknown values"
+function hasOwnProperty<Prop extends keyof X, X extends { [key: string]: any }>(
+  obj: X,
+  prop: Prop
+): obj is X & { [key in NonNullable<Prop>]: X[key] } {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
 }
 
 export { hasOwnProperty };

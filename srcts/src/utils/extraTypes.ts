cad83d5a912dd502f7066e934e5650058ab540b3
Barret Schloerke <schloerke@gmail.com>
2022-05-24 17:44:29 -0400

    Update extraTypes.ts

--- srcts/src/utils/extraTypes.ts
+++ srcts/src/utils/extraTypes.ts
@@ -1,4 +1,8 @@
 type AnyFunction = (...args: any[]) => any;
 type AnyVoidFunction = (...args: any[]) => void;
+type MapValuesUnion<T> = T[keyof T];
+type MapWithResult<X, R> = {
+  [Property in keyof X]: R;
+};
 
-export type { AnyFunction, AnyVoidFunction };
+export type { AnyFunction, AnyVoidFunction, MapValuesUnion, MapWithResult };

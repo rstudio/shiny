26f032d7f84a4adbc5d087bfafbe1ec102f2ebc2
Winston Chang <winston@stdout.org>
2022-06-10 09:52:46 -0500

    Code formatting

--- R/shinyui.R
+++ R/shinyui.R
@@ -14,7 +14,11 @@ NULL
 #' # now we can just write "static" content without withMathJax()
 #' div("more math here $$\\sqrt{2}$$")
 withMathJax <- function(...) {
-  path <- paste(getOption('shiny.mathjax.url','https://mathjax.rstudio.com/latest/MathJax.js'),getOption('shiny.mathjax.config','config=TeX-AMS-MML_HTMLorMML'),sep='?')
+  path <- paste0(
+    getOption("shiny.mathjax.url", "https://mathjax.rstudio.com/latest/MathJax.js"),
+    "?",
+    getOption("shiny.mathjax.config", "config=TeX-AMS-MML_HTMLorMML")
+  )
   tagList(
     tags$head(
       singleton(tags$script(src = path, type = 'text/javascript'))

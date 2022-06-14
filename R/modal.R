905b210e5951f1251cf51f3928e0c10afa943dd1
gsmolinski <g.smolinski1@gmail.com>
2022-06-14 21:16:29 +0200

    closes issue #3631 - documenting that 'xl' modal dialog will be changed to 'm' in Bootstrap 3

--- R/modal.R
+++ R/modal.R
@@ -43,7 +43,8 @@ removeModal <- function(session = getDefaultReactiveDomain()) {
 #' @param title An optional title for the dialog.
 #' @param footer UI for footer. Use `NULL` for no footer.
 #' @param size One of `"s"` for small, `"m"` (the default) for medium,
-#'   or `"l"` for large.
+#'   `"l"` for large or `"xl"` for extra large (will be changed to `"m"`
+#'   in Bootstrap 3).
 #' @param easyClose If `TRUE`, the modal dialog can be dismissed by
 #'   clicking outside the dialog box, or be pressing the Escape key. If
 #'   `FALSE` (the default), the modal dialog can't be dismissed in those

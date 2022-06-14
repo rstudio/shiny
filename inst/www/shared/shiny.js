41675aadf51de002b27c20f03a45c01a4e90c1a0
dvg-p4 <dgealow@paradigm4.com>
2022-06-14 12:34:42 -0400

    propogate nonstandard version number

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1,4 +1,4 @@
-/*! shiny 1.7.1.9004 | (c) 2012-2022 RStudio, PBC. | License: GPL-3 | file LICENSE */
+/*! shiny 1.7.1.5550 | (c) 2012-2022 RStudio, PBC. | License: GPL-3 | file LICENSE */
 (function() {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
@@ -12993,7 +12993,7 @@
   var windowShiny2;
   function setShiny(windowShiny_) {
     windowShiny2 = windowShiny_;
-    windowShiny2.version = "1.7.1.9004";
+    windowShiny2.version = "1.7.1.5550";
     var _initInputBindings = initInputBindings(), inputBindings = _initInputBindings.inputBindings, fileInputBinding2 = _initInputBindings.fileInputBinding;
     var _initOutputBindings = initOutputBindings(), outputBindings = _initOutputBindings.outputBindings;
     setFileInputBinding(fileInputBinding2);

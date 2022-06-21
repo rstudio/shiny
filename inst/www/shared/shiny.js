ab4d2838d7ac6dd099b190bebc2a28070de94867
dvg-p4 <dgealow@paradigm4.com>
2022-06-21 11:21:12 -0400

    Bump version number

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1,4 +1,4 @@
-/*! shiny 1.7.1.5550 | (c) 2012-2022 RStudio, PBC. | License: GPL-3 | file LICENSE */
+/*! shiny 1.7.1.55501 | (c) 2012-2022 RStudio, PBC. | License: GPL-3 | file LICENSE */
 (function() {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
@@ -13161,7 +13161,7 @@
   var windowShiny2;
   function setShiny(windowShiny_) {
     windowShiny2 = windowShiny_;
-    windowShiny2.version = "1.7.1.5550";
+    windowShiny2.version = "1.7.1.55501";
     var _initInputBindings = initInputBindings(), inputBindings = _initInputBindings.inputBindings, fileInputBinding2 = _initInputBindings.fileInputBinding;
     var _initOutputBindings = initOutputBindings(), outputBindings = _initOutputBindings.outputBindings;
     setFileInputBinding(fileInputBinding2);

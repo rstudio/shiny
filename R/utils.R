78f3f1180e894bdced66faf076848dd26306a553
George Stagg <george.stagg@posit.co>
2023-04-26 15:15:40 +0100

    Reduce amount of call stack used in hybrid_chain
    Replaces `tryCatch` in `hybrid_chain` with `withCallingHandlers`. This improves
    Shiny on webR on Safari < 16.4 by reducing the amount of call stack used during
    deep nested calls to `hybrid_chain`.
    
    A `delayedAssign` is used so that if the error condition handler is invoked,
    the condition is handled or re-thrown in the context of `hybrid_chain`, rather
    than the context of the inner function deeper in the stack.
    
    The `finally` argument is reimplemented with an `on.exit()`.

--- R/utils.R
+++ R/utils.R
@@ -1515,7 +1515,19 @@ hybrid_chain <- function(expr, ..., catch = NULL, finally = NULL,
 
   do <- function() {
     runFinally <- TRUE
-    tryCatch(
+    on.exit({ if (runFinally && !is.null(finally)) finally() })
+
+    catch_e <- NULL
+    delayedAssign("do_catch",
+      if (!is.null(catch)) {
+        catch(catch_e)
+        return()
+      } else {
+        stop(catch_e)
+      }
+    )
+
+    withCallingHandlers(
       {
         captureStackTraces({
           result <- withVisible(force(expr))
@@ -1543,12 +1555,9 @@ hybrid_chain <- function(expr, ..., catch = NULL, finally = NULL,
         })
       },
       error = function(e) {
-        if (!is.null(catch))
-          catch(e)
-        else
-          stop(e)
-      },
-      finally = if (runFinally && !is.null(finally)) finally()
+        catch_e <<- e
+        do_catch
+      }
     )
   }
 

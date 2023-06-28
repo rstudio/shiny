c2aaf06a51b0b24ff6447067d31b463cb4931411
Garrick Aden-Buie <garrick@adenbuie.com>
2023-06-28 15:42:33 -0400

    Copy-edit note about multiple=TRUE

--- R/input-select.R
+++ R/input-select.R
@@ -186,17 +186,17 @@ needOptgroup <- function(choices) {
 #'   value when it is a single choice input and the empty string is not in the
 #'   `choices` argument. This is to keep compatibility with
 #'   `selectInput(..., selectize = FALSE)`.
-#'   
-#'   The selectize input created from `selectInput(..., selectize = TRUE, 
-#'   multiple = TRUE)` allows deletion of all selected options, which will 
-#'   then return NULL as its value. Because the default behavior of an 
-#'   `observeEvent()` is to ignore expressions that evaluate to NULL, a change 
-#'   from a single selected option to no selected options in such a 
-#'   `selectInput()` would not constitute an event that would trigger an 
-#'   `observeEvent()` by default. If this behavior is undesirable in your 
-#'   context, it may be useful to set the `ignoreNULL` (and perhaps also the 
-#'   `ignoreInit`) parameter of `observeEvent()` to TRUE; see [observeEvent()]
-#'   for details. 
+#'
+#'   When `multiple = TRUE` is used with `selectizeInput()`, users can select
+#'   multiple options as well as delete all selected options. When no options
+#'   are selected, the server will receive a value of `NULL`. Because the
+#'   default behavior of an `observeEvent()` is to ignore expressions that
+#'   evaluate to NULL, a change from a single selected option to no selected
+#'   options in such a `selectizeInput()` will not invalidate (cause to update)
+#'   an `observeEvent()` by default. If this behavior is undesirable in your
+#'   context, it may be useful to set `ignoreNULL = TRUE` in `observeEvent()`
+#'   (and possibly also `ignoreInit = TRUE` as well). See [observeEvent()] for
+#'   details.
 #' @export
 selectizeInput <- function(inputId, ..., options = NULL, width = NULL) {
   selectizeIt(

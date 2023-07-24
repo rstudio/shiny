60b5a7cdcebf5d7d3eb09a02bf7ff39b9e8d77eb
Garrick Aden-Buie <garrick@adenbuie.com>
2023-07-24 14:02:34 -0400

    feat: Optimize `ancestorBindSetting()`
    Avoid generic `.closest()` DOM traversals and instead call `.closest()` only when we know we'll need it and within a limited scope.

--- srcts/src/bindings/input/_filterBindingMatches.ts
+++ srcts/src/bindings/input/_filterBindingMatches.ts
@@ -2,15 +2,37 @@ function ancestorBindSetting(el: HTMLElement): string {
   if (!(el instanceof HTMLElement)) return "eager";
 
   if (el.dataset.shinyBindInputs) {
+    // The element itself has a bind setting, use that. It might be useful
+    // TODO: should we only consider descendants, not current elements?
     return el.dataset.shinyBindInputs;
   }
 
-  const ancestorBindSetting = el.closest("[data-shiny-bind-inputs]");
-  if (!ancestorBindSetting || !(ancestorBindSetting instanceof HTMLElement)) {
+  if (!document.querySelector("[data-shiny-bind-inputs]")) {
+    // No bind settings declared in this document, everything is "eager"
     return "eager";
   }
 
-  return ancestorBindSetting.dataset.shinyBindInputs;
+  // Knowing we have some areas of the document with bind settings, we then
+  // focus on those areas. We're trying to avoid many calls to .closest() and
+  // only falling back to DOM traversal when necessary and in a limited scope.
+  const setAreas = Array.from(
+    document.querySelectorAll("[data-shiny-bind-inputs]")
+  ) as HTMLElement[];
+
+  for (const area of setAreas) {
+    if (area.contains(el)) {
+      const ancestorBindSetting = el.closest(
+        "[data-shiny-bind-inputs]"
+      ) as HTMLElement;
+
+      if (!(ancestorBindSetting instanceof HTMLElement)) continue;
+      if (ancestorBindSetting.dataset.shinyBindInputs) {
+        return ancestorBindSetting.dataset.shinyBindInputs as string;
+      }
+    }
+  }
+
+  return "eager";
 }
 
 function filterBindingMatchesAllowedOnly(

504836bed336ee6f370bbbf5bb1575a13f543fe0
gadenbuie <gadenbuie@users.noreply.github.com>
2023-07-28 15:04:09 +0000

    `yarn build` (GitHub Actions)

--- srcts/types/src/bindings/input/_filterBindingMatches.d.ts
+++
@@ -1,3 +0,0 @@
-declare function filterBindingMatchesAllowedOnly(matches: JQuery<HTMLElement>): JQuery<HTMLElement>;
-declare function filterBindingMatchesIfStrict(matches: JQuery<HTMLElement>, inputClass: string): JQuery<HTMLElement>;
-export { filterBindingMatchesAllowedOnly, filterBindingMatchesIfStrict };

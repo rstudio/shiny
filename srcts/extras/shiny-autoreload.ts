6560c59c8f36885d633ea3af08cff918e2492acd
Joe Cheng <joe@posit.co>
2023-10-14 10:22:09 -0700

    Soften visually jarring greyout when autoreloading
    This change detects when Shiny is autoreloading, and instead of
    showing the typical disconnection treatment (dark grey), it
    initially shows nothing and then quickly fades to very light
    grey.
    
    The goal is for instant autoreloads to feel seamless, but longer
    autoreloads to make it clear when the autoreload has completed,
    and still not feel as jarring as a full disconnect.

--- srcts/extras/shiny-autoreload.ts
+++ srcts/extras/shiny-autoreload.ts
@@ -9,9 +9,47 @@ const defaultUrl = `${protocol}//${window.location.host}${defaultPath}`;
 const wsUrl = document.currentScript.dataset.wsUrl || defaultUrl;
 const ws = new WebSocket(wsUrl);
 
+enum Status {
+  // Trying to reach the autoreload server
+  Connecting = "connecting",
+  // Connected to the autoreload server, just waiting for it to tell us to
+  // reload
+  Ready = "ready",
+  // The server has asked us to reload and we're in the process of doing so
+  Reloading = "reloading",
+  // The connection to the server was broken. TODO: should we try reconnecting?
+  Closed = "closed",
+}
+
+// Put a class on <html> indicating the current state of the autoreload channel
+function setAutoreloadStatus(status: Status) {
+  for (const s of Object.values(Status)) {
+    if (status === s) {
+      document.documentElement.classList.add(`autoreload-${s}`);
+    } else {
+      document.documentElement.classList.remove(`autoreload-${s}`);
+    }
+  }
+}
+
+// Also unconditionally add the "autoreload" class to <html>, so it's easy to
+// tell whether autoreload is even enabled
+document.documentElement.classList.add("autoreload");
+
+setAutoreloadStatus(Status.Connecting);
+
+ws.onopen = function(event) {
+  setAutoreloadStatus(Status.Ready);
+};
+
+ws.onclose = function(event) {
+  setAutoreloadStatus(Status.Closed);
+};
+
 ws.onmessage = function (event) {
   if (event.data === "autoreload") {
     window.location.reload();
+    setAutoreloadStatus(Status.Reloading);
   }
 };
 

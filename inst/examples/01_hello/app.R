f85e8e7904fb982cbb89ace1be5f90411ba648a9
skaltman <sarakaialtman@gmail.com>
2024-01-03 14:36:13 -0800

    update 01_hello

--- inst/examples/01_hello/app.R
+++ inst/examples/01_hello/app.R
@@ -1,34 +1,23 @@
 library(shiny)
+library(bslib)
 
 # Define UI for app that draws a histogram ----
-ui <- fluidPage(
+ui <- page_sidebar(
 
   # App title ----
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
+  title = "Hello Shiny!",
+  # Sidebar panel for inputs ----
+  sidebar =
+    sidebar(
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 1,
                   max = 50,
                   value = 30)
-
     ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
-    )
-  )
+  # Output: Histogram ----
+  plotOutput(outputId = "distPlot")
 )
 
 # Define server logic required to draw a histogram ----

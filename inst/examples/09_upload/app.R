5fbbeb1c5633e4095ba9ea5aeb678efd942e8812
Daniel Chen <chendaniely@users.noreply.github.com>
2024-01-19 12:19:47 -0800

    update 09 upload app for bslib

--- inst/examples/09_upload/app.R
+++ inst/examples/09_upload/app.R
@@ -1,64 +1,55 @@
+library(bslib)
 library(shiny)
 
 # Define UI for data upload app ----
-ui <- fluidPage(
+ui <- page_sidebar(
+
+  # Main panel for displaying outputs ----
+
+  # Output: Data file ----
+  tableOutput("contents"),
+
+  # Sidebar panel for inputs ----
+  sidebar = sidebar(
+    # Input: Select a file ----
+    fileInput("file1", "Choose CSV File",
+              multiple = TRUE,
+              accept = c("text/csv",
+                       "text/comma-separated-values,text/plain",
+                       ".csv")),
+
+    # Horizontal line ----
+    tags$hr(),
+
+    # Input: Checkbox if file has header ----
+    checkboxInput("header", "Header", TRUE),
+
+    # Input: Select separator ----
+    radioButtons("sep", "Separator",
+                 choices = c(Comma = ",",
+                             Semicolon = ";",
+                             Tab = "\t"),
+                 selected = ","),
+
+    # Input: Select quotes ----
+    radioButtons("quote", "Quote",
+                 choices = c(None = "",
+                             "Double Quote" = '"',
+                             "Single Quote" = "'"),
+                 selected = '"'),
+
+    # Horizontal line ----
+    tags$hr(),
+
+    # Input: Select number of rows to display ----
+    radioButtons("disp", "Display",
+                 choices = c(Head = "head",
+                             All = "all"),
+                 selected = "head")
+  ),
 
   # App title ----
-  titlePanel("Uploading Files"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Select a file ----
-      fileInput("file1", "Choose CSV File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                         "text/comma-separated-values,text/plain",
-                         ".csv")),
-
-      # Horizontal line ----
-      tags$hr(),
-
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
-
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-
-      # Horizontal line ----
-      tags$hr(),
-
-      # Input: Select number of rows to display ----
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head")
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Data file ----
-      tableOutput("contents")
-
-    )
-
-  )
+  title = "Uploading Files"
 )
 
 # Define server logic to read selected file ----

9fdc76d5af84504601fd485bb42656d9e0ec3ac0
wch <wch@users.noreply.github.com>
2024-05-24 16:32:42 +0000

    `yarn build` (GitHub Actions)

--- inst/www/shared/shiny-testmode.js
+++ inst/www/shared/shiny-testmode.js
@@ -1,3 +1,3 @@
-/*! shiny 1.8.1.9001 | (c) 2012-2024 RStudio, PBC. | License: GPL-3 | file LICENSE */
-"use strict";(function(){var a=eval;window.addEventListener("message",function(i){var e=i.data;e.code&&a(e.code)});})();
+/*! shiny 1.8.1.9001 | (c) 2012-2024 Posit, PBC. | License: GPL-3 | file LICENSE */
+"use strict";(()=>{var t=eval;window.addEventListener("message",function(a){let e=a.data;e.code&&t(e.code)});})();
 //# sourceMappingURL=shiny-testmode.js.map

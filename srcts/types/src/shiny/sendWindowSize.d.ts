1018f7cc62c4dc5c9c7269a9065b56894c5fc733
karangattu <karangattu@users.noreply.github.com>
2024-10-29 01:29:43 +0000

    `yarn build` (GitHub Actions)

--- srcts/types/src/shiny/sendWindowSize.d.ts
+++
@@ -1,9 +0,0 @@
-import type { InputBatchSender } from "../inputPolicies";
-import { Debouncer } from "../time";
-declare class SendWindowSize {
-    regular: () => void;
-    transitioned: () => void;
-    setWindowSizeSend(inputBatchSender: InputBatchSender, doSendWindowSize: () => void): Debouncer<typeof doSendWindowSize>;
-}
-declare const sendWindowSizeFns: SendWindowSize;
-export { sendWindowSizeFns };

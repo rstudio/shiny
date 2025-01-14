67b5ffe7e42dfaacf8fa34beb6d30c2258c54b78
gadenbuie <gadenbuie@users.noreply.github.com>
2025-01-14 18:08:03 +0000

    `yarn build` (GitHub Actions)

--- inst/www/shared/shiny-testmode.js
+++ inst/www/shared/shiny-testmode.js
@@ -1,3 +1,3 @@
-/*! shiny 1.10.0.9000 | (c) 2012-2025 RStudio, PBC. | License: GPL-3 | file LICENSE */
+/*! shiny 1.10.0.9000 | (c) 2012-2025 Posit Software, PBC. | License: GPL-3 | file LICENSE */
 "use strict";(function(){var a=eval;window.addEventListener("message",function(i){var e=i.data;e.code&&a(e.code)});})();
 //# sourceMappingURL=shiny-testmode.js.map

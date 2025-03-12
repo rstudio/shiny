73e84884d48f8fb4bae40f5564abacecfb1fb4e0
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-03-12 16:09:35 +0000

    Updates ExtendedTask example with cancellation

--- R/extended-task.R
+++ R/extended-task.R
@@ -41,12 +41,13 @@
 #'   is, a function that quickly returns a promise) and allows even that very
 #'   session to immediately unblock and carry on with other user interactions.
 #'
-#' @examplesIf rlang::is_interactive() && rlang::is_installed("future")
-#'
+#' @examplesIf rlang::is_interactive() && rlang::is_installed("mirai")
 #' library(shiny)
 #' library(bslib)
-#' library(future)
-#' plan(multisession)
+#' library(mirai)
+#'
+#' daemons(1)
+#' onStop(function() daemons(0))
 #'
 #' ui <- page_fluid(
 #'   titlePanel("Extended Task Demo"),
@@ -55,18 +56,18 @@
 #'     "that takes a while to perform."
 #'   ),
 #'   input_task_button("recalculate", "Recalculate"),
+#'   actionButton("cancel", "Cancel"),
 #'   p(textOutput("result"))
 #' )
 #'
 #' server <- function(input, output) {
 #'   rand_task <- ExtendedTask$new(function() {
-#'     future(
+#'     mirai(
 #'       {
 #'         # Slow operation goes here
 #'         Sys.sleep(2)
 #'         sample(1:100, 1)
-#'       },
-#'       seed = TRUE
+#'       }
 #'     )
 #'   })
 #'
@@ -76,8 +77,21 @@
 #'   bind_task_button(rand_task, "recalculate")
 #'
 #'   observeEvent(input$recalculate, {
-#'     # Invoke the extended in an observer
+#'     # Invoke the task in an observer.
 #'     rand_task$invoke()
+#'   })
+#'
+#'   observeEvent(input$cancel, {
+#'     # For cancelling the task if required.
+#'     rand_task$cancel()
+#'   })
+#'
+#'   observe({
+#'     # Disable cancel button when task is not running.
+#'     updateActionButton(
+#'       inputId = "cancel",
+#'       disabled = rand_task$status() != "running"
+#'     )
 #'   })
 #'
 #'   output$result <- renderText({

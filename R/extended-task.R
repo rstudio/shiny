e8b7c08a192bf35f99d43256ba961dde52dc834f
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2025-06-16 15:42:21 +0100

    Adds mirai to documentation (#4230)
    * Update docs to mention mirai
    
    * Update example to use mirai
    
    * Fix other roxygen2 render

--- R/extended-task.R
+++ R/extended-task.R
@@ -41,12 +41,15 @@
 #'   is, a function that quickly returns a promise) and allows even that very
 #'   session to immediately unblock and carry on with other user interactions.
 #'
-#' @examplesIf rlang::is_interactive() && rlang::is_installed("future")
-#'
+#' @examplesIf rlang::is_interactive() && rlang::is_installed("mirai")
 #' library(shiny)
 #' library(bslib)
-#' library(future)
-#' plan(multisession)
+#' library(mirai)
+#'
+#' # Set background processes for running tasks
+#' daemons(1)
+#' # Reset when the app is stopped
+#' onStop(function() daemons(0))
 #'
 #' ui <- page_fluid(
 #'   titlePanel("Extended Task Demo"),
@@ -60,13 +63,12 @@
 #'
 #' server <- function(input, output) {
 #'   rand_task <- ExtendedTask$new(function() {
-#'     future(
+#'     mirai(
 #'       {
 #'         # Slow operation goes here
 #'         Sys.sleep(2)
 #'         sample(1:100, 1)
-#'       },
-#'       seed = TRUE
+#'       }
 #'     )
 #'   })
 #'
@@ -100,11 +102,12 @@ ExtendedTask <- R6Class("ExtendedTask", portable = TRUE, cloneable = FALSE,
     #' @param func The long-running operation to execute. This should be an
     #'   asynchronous function, meaning, it should use the
     #'   [\{promises\}](https://rstudio.github.io/promises/) package, most
-    #'   likely in conjuction with the
+    #'   likely in conjunction with the
+    #'   [\{mirai\}](https://mirai.r-lib.org) or
     #'   [\{future\}](https://rstudio.github.io/promises/articles/promises_04_futures.html)
     #'   package. (In short, the return value of `func` should be a
-    #'   [`Future`][future::future()] object, or a `promise`, or something else
-    #'   that [promises::as.promise()] understands.)
+    #'   [`mirai`][mirai::mirai()], [`Future`][future::future()], `promise`,
+    #'   or something else that [promises::as.promise()] understands.)
     #'
     #'   It's also important that this logic does not read from any
     #'   reactive inputs/sources, as inputs may change after the function is

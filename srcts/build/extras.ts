df6e9ffa5e91c0d2ac7f81c9e6b971c486c43c28
Barret Schloerke <barret@posit.co>
2025-08-01 11:39:09 -0400

    Build extras quickly

--- srcts/build/extras.ts
+++ srcts/build/extras.ts
@@ -3,30 +3,12 @@
 // npm run build
 // ```
 
-// - TypeScript -----------------------------------------------------------
-
-import { banner, build, outDir } from "./_build.js";
-
-await build({
-  bundle: true,
-  sourcemap: true,
-  minify: true,
-  plugins: [],
-  banner: banner,
-  entryPoints: [
-    "srcts/extras/shiny-autoreload.ts",
-    "srcts/extras/shiny-showcase.ts",
-    "srcts/extras/shiny-testmode.ts",
-  ],
-  outdir: outDir,
-});
-
-// - Sass -----------------------------------------------------------
-
 import autoprefixer from "autoprefixer";
 import { sassPlugin } from "esbuild-sass-plugin";
 import postcss from "postcss";
 import postcssPresetEnv from "postcss-preset-env";
+
+import { banner, build, outDir } from "./_build.js";
 
 const sassOpts = {
   minify: true,
@@ -45,24 +27,50 @@ const sassOpts = {
   ],
 };
 
-await build({
-  ...sassOpts,
-  entryPoints: ["srcts/extras/shiny-showcase.scss"],
-  outfile: outDir + "shiny-showcase.css",
-});
-await build({
-  ...sassOpts,
-  entryPoints: [
-    // Must keep shiny.scss within `inst` to be able to use as htmldependency
-    outDir + "shiny_scss/shiny.scss",
-  ],
-  outfile: outDir + "shiny.min.css",
-});
-await build({
-  ...sassOpts,
-  entryPoints: ["srcts/extras/busy-indicators/busy-indicators.scss"],
-  outfile: outDir + "busy-indicators/busy-indicators.css",
-  plugins: [sassPlugin()],
-  bundle: false,
-  metafile: true,
+async function main(): Promise<void> {
+  await Promise.all([
+    // - TypeScript -----------------------------------------------------------
+    // TypeScript builds
+    build({
+      bundle: true,
+      sourcemap: true,
+      minify: true,
+      plugins: [],
+      banner: banner,
+      entryPoints: [
+        "srcts/extras/shiny-autoreload.ts",
+        "srcts/extras/shiny-showcase.ts",
+        "srcts/extras/shiny-testmode.ts",
+      ],
+      outdir: outDir,
+    }),
+
+    // Sass builds
+    build({
+      ...sassOpts,
+      entryPoints: ["srcts/extras/shiny-showcase.scss"],
+      outfile: outDir + "shiny-showcase.css",
+    }),
+    build({
+      ...sassOpts,
+      entryPoints: [
+        // Must keep shiny.scss within `inst` to be able to use as htmldependency
+        outDir + "shiny_scss/shiny.scss",
+      ],
+      outfile: outDir + "shiny.min.css",
+    }),
+    build({
+      ...sassOpts,
+      entryPoints: ["srcts/extras/busy-indicators/busy-indicators.scss"],
+      outfile: outDir + "busy-indicators/busy-indicators.css",
+      plugins: [sassPlugin()],
+      bundle: false,
+      metafile: true,
+    }),
+  ]);
+}
+
+main().catch((err) => {
+  console.error("Error:\n" + err);
+  process.exit(1);
 });

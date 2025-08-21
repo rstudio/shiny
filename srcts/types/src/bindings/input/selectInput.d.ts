320445dfaabcba0690763a6195bb76b69c5f6d54
cpsievert <cpsievert@users.noreply.github.com>
2025-08-21 15:39:33 +0000

    `npm run build` (GitHub Actions)

--- srcts/types/src/bindings/input/selectInput.d.ts
+++ srcts/types/src/bindings/input/selectInput.d.ts
@@ -32,6 +32,7 @@ declare class SelectInputBinding extends InputBinding {
     unsubscribe(el: HTMLElement): void;
     initialize(el: SelectHTMLElement): void;
     protected _selectize(el: SelectHTMLElement, update?: boolean): SelectizeInfo | undefined;
+    private _supplyDefaultPlugins;
 }
 export { SelectInputBinding };
 export type { SelectInputReceiveMessageData };

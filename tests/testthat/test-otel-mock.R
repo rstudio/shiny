9eb62a360be5a3aa7b26ea7da66e50e8bd140528
Barret Schloerke <barret@posit.co>
2025-10-17 15:34:45 -0400

    Use otelsdk::with_otel_record and testServer() to test ospan nesting / shape

---
+++ tests/testthat/test-otel-mock.R
@@ -0,0 +1,199 @@
+skip_if_not_installed("otelsdk")
+
+withr::local_options(list(shiny.otel.enabled = "all"))
+
+expect_code_attrs <- function(trace) {
+  testthat::expect_true(!is.null(trace))
+  testthat::expect_true(is.list(trace$attributes))
+  testthat::expect_true(is.character(trace$attributes[["code.filepath"]]))
+  testthat::expect_equal(trace$attributes[["code.filepath"]], "test-otel-mock.R")
+  testthat::expect_true(is.numeric(trace$attributes[["code.lineno"]]))
+  testthat::expect_true(is.numeric(trace$attributes[["code.column"]]))
+
+  invisible(trace)
+}
+MOCK_SESSION_TOKEN <- "test-session-token"
+expect_session_id <- function(trace) {
+  testthat::expect_true(!is.null(trace))
+  testthat::expect_true(is.list(trace$attributes))
+  testthat::expect_true(is.character(trace$attributes[["session.id"]]))
+  testthat::expect_equal(trace$attributes[["session.id"]], MOCK_SESSION_TOKEN)
+
+  invisible(trace)
+}
+
+expect_trace <- function(traces, name, pos = 1) {
+  # Filter to traces with the given name
+  trace_set <- traces[which(names(traces) == name)]
+  testthat::expect_gte(length(trace_set), pos)
+
+  # Get the trace at the given position
+  trace <- trace_set[[pos]]
+  testthat::expect_true(is.list(trace))
+
+  expect_code_attrs(trace)
+  expect_session_id(trace)
+
+  trace
+}
+
+create_mock_session <- function() {
+  session <- MockShinySession$new()
+  session$token <- MOCK_SESSION_TOKEN
+  session
+}
+
+test_server_with_otel <- function(session, server, expr, args = list()) {
+  stopifnot(inherits(session, "MockShinySession"))
+  stopifnot(is.function(server))
+
+  info <- otelsdk::with_otel_record({
+    # rlang quosure magic to capture and pass through `expr`
+    testServer(server, {{ expr }}, args = args, session = session)
+  })
+
+  info$traces
+}
+
+test_that("handles observers", {
+  server <- function(input, output, session) {
+    observe({
+      42
+    })
+
+    my_observe <- observe({
+      43
+    })
+
+    observe({
+      44
+    }, label = "labeled observer")
+  }
+
+  session <- create_mock_session()
+  traces <- test_server_with_otel(session, server, {
+    # probably not needed to do anything here
+    session$flushReact()
+  })
+
+  expect_trace(traces, "observe mock-session:<anonymous>")
+  expect_trace(traces, "observe mock-session:my_observe")
+  expect_trace(traces, "observe mock-session:labeled observer")
+})
+
+test_that("reactiveVal / reactiveValues is supported", {
+  server <- function(input, output, session) {
+    rv <- reactiveVal(0)
+    rv2 <- (function() {reactiveVal(0)})()  # test anonymous reactiveVal
+    rv3 <- reactiveVal(0, "labeled_rv")
+
+    observe({
+      isolate({
+        rv(rv() + 1)
+        rv2(rv2() + 1)
+        rv3(rv3() + 1)
+      })
+    })
+  }
+
+  session <- create_mock_session()
+  traces <- test_server_with_otel(session, server, {
+    session$flushReact()
+    expect_equal(rv(), 1)
+  })
+
+  expect_trace(traces, "observe mock-session:<anonymous>")
+
+  # TODO-future: Add tests to see the `Set reactiveVal mock-session:rv` logs
+  # Requires: https://github.com/r-lib/otelsdk/issues/21
+})
+
+test_that("reactive is supported", {
+  server <- function(input, output, session) {
+    r <- reactive({ 42 })
+    r2 <- (function() {reactive({ r() })})()  # test anonymous reactive
+    r3 <- reactive({ r2() }, label = "labeled_rv")
+
+    observe(label = "obs_r3", {
+      r3()
+    })
+  }
+
+  session <- create_mock_session()
+  traces <- test_server_with_otel(session, server, {
+    session$flushReact()
+    session$flushReact()
+    session$flushReact()
+    expect_equal(r(), 42)
+    expect_equal(r2(), 42)
+    expect_equal(r3(), 42)
+  })
+
+  observe_trace <- expect_trace(traces, "observe mock-session:obs_r3")
+  r_trace <- expect_trace(traces, "reactive mock-session:r")
+  r2_trace <- expect_trace(traces, "reactive mock-session:<anonymous>")
+  r3_trace <- expect_trace(traces, "reactive mock-session:labeled_rv")
+
+  expect_equal(r_trace$parent, r2_trace$span_id)
+  expect_equal(r2_trace$parent, r3_trace$span_id)
+  expect_equal(r3_trace$parent, observe_trace$span_id)
+})
+
+
+test_that("outputs are supported", {
+  server <- function(input, output, session) {
+    output$txt <- renderText({
+      "Hello, world!"
+    })
+  }
+
+  session <- create_mock_session()
+  traces <- test_server_with_otel(session, server, {
+    session$flushReact()
+    session$flushReact()
+    session$flushReact()
+    expect_equal(output$txt, "Hello, world!")
+  })
+
+  expect_trace(traces, "output mock-session:txt")
+})
+
+test_that("extended tasks are supported", {
+  server <- function(input, output, session) {
+    rand_task <- ExtendedTask$new(function() {
+      promise_resolve(42) |> then(function(value) {
+        value
+      })
+    })
+
+    observe(label = "invoke task", {
+      rand_task$invoke()
+    })
+
+    output$result <- renderText({
+      # React to updated results when the task completes
+      number <- rand_task$result()
+      paste0("Your number is ", number, ".")
+    })
+  }
+
+  session <- create_mock_session()
+  traces <- test_server_with_otel(session, server, {
+    session$flushReact()
+
+    while(!later::loop_empty()) {
+      later::run_now()
+      session$flushReact()
+    }
+    session$flushReact()
+
+  })
+
+  invoke_obs <- expect_trace(traces, "observe mock-session:invoke task")
+  render1_trace <- expect_trace(traces, "output mock-session:result")
+  ex_task_trace <- expect_trace(traces, "ExtendedTask mock-session:rand_task")
+
+  render2_trace <- expect_trace(traces, "output mock-session:result", pos = 2)
+
+  expect_equal(invoke_obs$span_id, ex_task_trace$parent)
+})

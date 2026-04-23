3184461c8b8059c911bde28ba4348f0ef1ed81e8
E Nelson <liz.nelson@posit.co>
2026-04-23 19:23:49 -0400

    Update showProgress method signature in DownloadLinkOutputBinding

--- srcts/types/src/bindings/output/downloadlink.d.ts
+++ srcts/types/src/bindings/output/downloadlink.d.ts
@@ -2,6 +2,5 @@ import { OutputBinding } from "./outputBinding";
 declare class DownloadLinkOutputBinding extends OutputBinding {
     find(scope: HTMLElement): JQuery<HTMLElement>;
     renderValue(el: HTMLElement, data: string): void;
-    showProgress(): void;
-}
+    showProgress(el: HTMLElement, show: boolean): void;}
 export { DownloadLinkOutputBinding };

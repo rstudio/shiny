0bc789a310181327adf7641725c0be9ef19fad76
elnelson575 <elnelson575@users.noreply.github.com>
2026-04-23 23:30:52 +0000

    `npm run build` (GitHub Actions)

--- srcts/types/src/bindings/output/downloadlink.d.ts
+++ srcts/types/src/bindings/output/downloadlink.d.ts
@@ -2,6 +2,6 @@ import { OutputBinding } from "./outputBinding";
 declare class DownloadLinkOutputBinding extends OutputBinding {
     find(scope: HTMLElement): JQuery<HTMLElement>;
     renderValue(el: HTMLElement, data: string): void;
-    showProgress(el: HTMLElement, show: boolean): void;
+    showProgress(): void;
 }
 export { DownloadLinkOutputBinding };

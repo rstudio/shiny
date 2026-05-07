a4da2c1eb8cce95c6881db2b828754e4f8abd8e9
E Nelson <liz.nelson@posit.co>
2026-05-06 20:48:00 -0400

    Update helper

--- tests/testthat/helper-shinytest2.R
+++ tests/testthat/helper-shinytest2.R
@@ -5,3 +5,18 @@ skip_if_not_shinytest2 <- function() {
   skip_on_os(c("windows", "linux", "solaris", "emscripten"))
   skip_if_not_installed("shinytest2")
 }
+
+# Launch an inline shinytest2 app and register cleanup in the caller's frame.
+# Returns the AppDriver instance ready for use.
+#
+# Usage:
+#   app <- launch_test_app(function() {
+#     library(shiny)
+#     shinyApp(ui, server)
+#   })
+launch_test_app <- function(app_fn, ..., envir = parent.frame()) {
+  app <- shinytest2::AppDriver$new(app_fn, ...)
+  withr::defer(app$stop(), envir = envir)
+  app$wait_for_idle()
+  app
+}
--- tests/testthat/test-zzz-st2-download.R
+++ tests/testthat/test-zzz-st2-download.R
@@ -3,132 +3,128 @@ library(shinytest2)
 
 # library(shiny) must be called explicitly inside the function so that
 # shinytest2's library shim loads the dev package via pkgload::load_all().
-app <- AppDriver$new(
-  function() {
-    library(shiny)
-    library(shinyjs)
+app <- launch_test_app(function() {
+  library(shiny)
+  library(shinyjs)
 
-    handler <- function() {
-      downloadHandler(
-        filename = "test.txt",
-        content = function(file) writeLines("test", file)
-      )
-    }
-
-    ui <- fluidPage(
-      useShinyjs(),
-
-      h3("downloadButton"),
-
-      uiOutput("btn_auto_ui"),
-      checkboxInput("toggle_btn_auto", "Enabled", value = TRUE),
-
-      uiOutput("btn_off_ui"),
-      checkboxInput("toggle_btn_off", "Enabled", value = FALSE),
-
-      uiOutput("btn_on_ui"),
-      checkboxInput("toggle_btn_on", "Enabled", value = TRUE),
-
-      uiOutput("btn_shinyjs_ui"),
-      checkboxInput("toggle_btn_shinyjs", "Enabled", value = FALSE),
-
-      h3("downloadLink"),
-
-      uiOutput("lnk_auto_ui"),
-      checkboxInput("toggle_lnk_auto", "Enabled", value = TRUE),
-
-      uiOutput("lnk_off_ui"),
-      checkboxInput("toggle_lnk_off", "Enabled", value = FALSE),
-
-      uiOutput("lnk_on_ui"),
-      checkboxInput("toggle_lnk_on", "Enabled", value = TRUE),
-
-      uiOutput("lnk_shinyjs_ui"),
-      checkboxInput("toggle_lnk_shinyjs", "Enabled", value = FALSE)
+  handler <- function() {
+    downloadHandler(
+      filename = "test.txt",
+      content = function(file) writeLines("test", file)
     )
-
-    server <- function(input, output, session) {
-      output$btn_auto <- handler()
-      output$btn_off <- handler()
-      output$btn_on <- handler()
-      output$btn_shinyjs <- handler()
-      output$lnk_auto <- handler()
-      output$lnk_off <- handler()
-      output$lnk_on <- handler()
-      output$lnk_shinyjs <- handler()
-
-      output$btn_auto_ui <- renderUI({
-        if (isTRUE(input$toggle_btn_auto)) {
-          downloadButton("btn_auto", "Auto (default)")
-        } else {
-          downloadButton("btn_auto", "Auto (default)", enabled = FALSE)
-        }
-      })
-
-      output$btn_off_ui <- renderUI({
-        if (isTRUE(input$toggle_btn_off)) {
-          downloadButton("btn_off", "Disabled", enabled = TRUE)
-        } else {
-          downloadButton("btn_off", "Disabled", enabled = FALSE)
-        }
-      })
-
-      output$btn_on_ui <- renderUI({
-        if (isTRUE(input$toggle_btn_on)) {
-          downloadButton("btn_on", "Pre-enabled", enabled = TRUE)
-        } else {
-          downloadButton("btn_on", "Pre-enabled", enabled = FALSE)
-        }
-      })
-
-      output$btn_shinyjs_ui <- renderUI({
-        btn <- downloadButton("btn_shinyjs", "shinyjs-disabled")
-        if (!isTRUE(input$toggle_btn_shinyjs)) {
-          shinyjs::disabled(btn)
-        } else {
-          btn
-        }
-      })
-
-      output$lnk_auto_ui <- renderUI({
-        if (isTRUE(input$toggle_lnk_auto)) {
-          downloadLink("lnk_auto", "Auto (default)")
-        } else {
-          downloadLink("lnk_auto", "Auto (default)", enabled = FALSE)
-        }
-      })
-
-      output$lnk_off_ui <- renderUI({
-        if (isTRUE(input$toggle_lnk_off)) {
-          downloadLink("lnk_off", "Disabled", enabled = TRUE)
-        } else {
-          downloadLink("lnk_off", "Disabled", enabled = FALSE)
-        }
-      })
-
-      output$lnk_on_ui <- renderUI({
-        if (isTRUE(input$toggle_lnk_on)) {
-          downloadLink("lnk_on", "Pre-enabled", enabled = TRUE)
-        } else {
-          downloadLink("lnk_on", "Pre-enabled", enabled = FALSE)
-        }
-      })
-
-      output$lnk_shinyjs_ui <- renderUI({
-        lnk <- downloadLink("lnk_shinyjs", "shinyjs-disabled")
-        if (!isTRUE(input$toggle_lnk_shinyjs)) {
-          shinyjs::disabled(lnk)
-        } else {
-          lnk
-        }
-      })
-    }
-
-    shinyApp(ui, server)
   }
-)
-withr::defer({ app$stop() })
-app$wait_for_idle()
+
+  ui <- fluidPage(
+    useShinyjs(),
+
+    h3("downloadButton"),
+
+    uiOutput("btn_auto_ui"),
+    checkboxInput("toggle_btn_auto", "Enabled", value = TRUE),
+
+    uiOutput("btn_off_ui"),
+    checkboxInput("toggle_btn_off", "Enabled", value = FALSE),
+
+    uiOutput("btn_on_ui"),
+    checkboxInput("toggle_btn_on", "Enabled", value = TRUE),
+
+    uiOutput("btn_shinyjs_ui"),
+    checkboxInput("toggle_btn_shinyjs", "Enabled", value = FALSE),
+
+    h3("downloadLink"),
+
+    uiOutput("lnk_auto_ui"),
+    checkboxInput("toggle_lnk_auto", "Enabled", value = TRUE),
+
+    uiOutput("lnk_off_ui"),
+    checkboxInput("toggle_lnk_off", "Enabled", value = FALSE),
+
+    uiOutput("lnk_on_ui"),
+    checkboxInput("toggle_lnk_on", "Enabled", value = TRUE),
+
+    uiOutput("lnk_shinyjs_ui"),
+    checkboxInput("toggle_lnk_shinyjs", "Enabled", value = FALSE)
+  )
+
+  server <- function(input, output, session) {
+    output$btn_auto <- handler()
+    output$btn_off <- handler()
+    output$btn_on <- handler()
+    output$btn_shinyjs <- handler()
+    output$lnk_auto <- handler()
+    output$lnk_off <- handler()
+    output$lnk_on <- handler()
+    output$lnk_shinyjs <- handler()
+
+    output$btn_auto_ui <- renderUI({
+      if (isTRUE(input$toggle_btn_auto)) {
+        downloadButton("btn_auto", "Auto (default)")
+      } else {
+        downloadButton("btn_auto", "Auto (default)", enabled = FALSE)
+      }
+    })
+
+    output$btn_off_ui <- renderUI({
+      if (isTRUE(input$toggle_btn_off)) {
+        downloadButton("btn_off", "Disabled", enabled = TRUE)
+      } else {
+        downloadButton("btn_off", "Disabled", enabled = FALSE)
+      }
+    })
+
+    output$btn_on_ui <- renderUI({
+      if (isTRUE(input$toggle_btn_on)) {
+        downloadButton("btn_on", "Pre-enabled", enabled = TRUE)
+      } else {
+        downloadButton("btn_on", "Pre-enabled", enabled = FALSE)
+      }
+    })
+
+    output$btn_shinyjs_ui <- renderUI({
+      btn <- downloadButton("btn_shinyjs", "shinyjs-disabled")
+      if (!isTRUE(input$toggle_btn_shinyjs)) {
+        shinyjs::disabled(btn)
+      } else {
+        btn
+      }
+    })
+
+    output$lnk_auto_ui <- renderUI({
+      if (isTRUE(input$toggle_lnk_auto)) {
+        downloadLink("lnk_auto", "Auto (default)")
+      } else {
+        downloadLink("lnk_auto", "Auto (default)", enabled = FALSE)
+      }
+    })
+
+    output$lnk_off_ui <- renderUI({
+      if (isTRUE(input$toggle_lnk_off)) {
+        downloadLink("lnk_off", "Disabled", enabled = TRUE)
+      } else {
+        downloadLink("lnk_off", "Disabled", enabled = FALSE)
+      }
+    })
+
+    output$lnk_on_ui <- renderUI({
+      if (isTRUE(input$toggle_lnk_on)) {
+        downloadLink("lnk_on", "Pre-enabled", enabled = TRUE)
+      } else {
+        downloadLink("lnk_on", "Pre-enabled", enabled = FALSE)
+      }
+    })
+
+    output$lnk_shinyjs_ui <- renderUI({
+      lnk <- downloadLink("lnk_shinyjs", "shinyjs-disabled")
+      if (!isTRUE(input$toggle_lnk_shinyjs)) {
+        shinyjs::disabled(lnk)
+      } else {
+        lnk
+      }
+    })
+  }
+
+  shinyApp(ui, server)
+})
 
 is_disabled <- function(id) {
   app$get_js(sprintf(

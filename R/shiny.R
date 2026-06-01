4fb6b0afc2e249052ea4253767c4afbcbd8d69b2
Carson Sievert <cpsievert1@gmail.com>
2026-06-01 15:47:30 -0500

    Apply suggestion from @cpsievert

--- R/shiny.R
+++ R/shiny.R
@@ -1090,7 +1090,7 @@ ShinySession <- R6Class(
             dots <- eval(substitute(alist(...)))
           }
 
-          if (any_unnamed(dots)) 
+          if (any_unnamed(dots))
             stop("exportTestValues: all arguments must be named.")
 
           names(dots) <- ns(names(dots))
